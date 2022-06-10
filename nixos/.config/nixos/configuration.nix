4f3a826e1894474245aebf8ac14df04f25b4e262
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-10 07:33:06 -0500

    feat: tinygo removed and c compiler tools installed

--- nixos/.config/nixos/configuration.nix
+++ nixos/.config/nixos/configuration.nix
@@ -25,7 +25,7 @@
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 80 443 8000 8080 ];
+      allowedTCPPorts = [ 80 443 8000 8080  ];
     };
 
     enableIPv6 = true;
@@ -89,7 +89,7 @@
   environment.systemPackages = with pkgs; [
     # golangci-lint
     gcc
-    tinygo
+    g++
 
     python310
     virtualenv
@@ -176,8 +176,7 @@
     description = "Set the battery charge threshold";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = ''
-        ${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+      ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
       ExecStop = ''${pkgs.bash}/bin/bash -c "exit 0"'';
     };
     wantedBy = [ "multi-user.target" ];
