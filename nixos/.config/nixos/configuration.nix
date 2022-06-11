94bdef1045d4b968cbda222b43b1b56dcff994fc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-10 23:02:55 -0500

    feat: g++ as string, bat and sass

--- nixos/.config/nixos/configuration.nix
+++ nixos/.config/nixos/configuration.nix
@@ -25,7 +25,7 @@
   networking = {
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 80 443 8000 8080  ];
+      allowedTCPPorts = [ 80 443 8000 8080 ];
     };
 
     enableIPv6 = true;
@@ -89,7 +89,7 @@
   environment.systemPackages = with pkgs; [
     # golangci-lint
     gcc
-    g++
+    "g++"
 
     python310
     virtualenv
@@ -117,11 +117,13 @@
     openjdk
     openssh
     unzip
+    sass
     wget
     dpkg
     tree
     stow
     tmux
+    bat
     # cron
     zip
     zsh
@@ -176,7 +178,8 @@
     description = "Set the battery charge threshold";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
       ExecStop = ''${pkgs.bash}/bin/bash -c "exit 0"'';
     };
     wantedBy = [ "multi-user.target" ];
