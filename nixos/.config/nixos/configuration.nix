fd70b889322db20581b1322e00a1977eec210d80
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-24 23:13:36 -0500

    feat: vgo2nix for just go mod

--- nixos/.config/nixos/configuration.nix
+++ nixos/.config/nixos/configuration.nix
@@ -124,7 +124,7 @@
           jetbrains.pycharm-community
         ];
 
-        go = with pkgs; [ go gopls gcc go2nix ];
+        go = with pkgs; [ go gopls gcc vgo2nix ];
 
         node = with pkgs; [ nodejs-18_x ];
 
