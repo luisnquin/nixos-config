10313d31cea3a2f1f1a76147f515de9fef8f1e3e
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-30 20:57:14 -0500

    refactor: alejandra and more organization

--- nixos/.config/nixos/configuration.nix
+++ nixos/.config/nixos/configuration.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, ... }:
-
 {
-  imports = [ ./hardware-configuration.nix ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = false;
+    android_sdk.accept_license = true;
+  };
 
   time.timeZone = "America/Lima";
 
-  fonts.fonts = with pkgs; [ cascadia-code jetbrains-mono nerdfonts ];
+  fonts.fonts = with pkgs; [cascadia-code jetbrains-mono nerdfonts];
 
   boot = {
     loader = {
@@ -24,13 +30,13 @@
       };
     };
 
-    supportedFilesystems = [ "ntfs" ];
+    supportedFilesystems = ["ntfs"];
   };
-  
-  services.gnome.gnome-keyring.enable = true;
-  services.openssh.enable = true;
+
   # xdg.portal.wlr.enable = true;
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker.enable = true;
+  };
 
   system.autoUpgrade = {
     enable = true;
@@ -63,9 +69,9 @@
 
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 20 80 443 8088 ];
+      allowedTCPPorts = [20 80 443 8088];
       allowPing = false;
-      trustedInterfaces = [ "docker0" ];
+      trustedInterfaces = ["docker0"];
     };
   };
 
@@ -75,27 +81,18 @@
     keyMap = "es";
   };
 
-  services.xserver = {
-    enable = true;
-
-    layout = "latam";
-
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
-
   sound.enable = true;
 
   hardware = {
     pulseaudio.enable = true;
     opengl.enable = true;
-  };
 
-  # hardware.nvidia = {
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  #   powerManagement.enable = true;
-  #   modesetting.enable = true;
-  # };
+    # nvidia = {
+    #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+    #   powerManagement.enable = true;
+    #   modesetting.enable = true;
+    # };
+  };
 
   users.users.luisnquin = {
     isNormalUser = true;
@@ -103,58 +100,72 @@
     description = "Luis Quiñones";
     shell = pkgs.zsh;
 
-    extraGroups = [ "wheel" "docker" "adbusers" ];
+    extraGroups = ["wheel" "docker" "adbusers"];
   };
 
-  services.xserver.displayManager.startx.enable = true;
-  services.xserver.desktopManager.xterm.enable = true;
+  services = {
+    gnome.gnome-keyring.enable = true;
+    openssh.enable = true;
+    xserver = {
+      videoDrivers = ["intel" "modesetting"];
+      libinput.enable = true;
+      layout = "latam";
+      autorun = true;
+      enable = true;
 
-  services.xserver = {
-    autorun = true;
-    videoDrivers = [ "intel" "modesetting" ];
-    libinput.enable = true;
-  };
-
-  environment.systemPackages = with pkgs;
-    let
-      set = {
-        python = with pkgs; [
-          python310
-          virtualenv
-          jetbrains.pycharm-community
-        ];
-
-        go = with pkgs; [ go gopls gcc vgo2nix ];
-
-        node = with pkgs; [ nodejs-18_x ];
-
-        android = with pkgs; [ android-tools flutter dart ];
-
-        db = with pkgs; [ postgresql ];
-
-        docker = with pkgs; [ docker docker-compose ];
-
-        apps = with pkgs; [ spotify discord vscode slack ];
-
-        otherApps = with pkgs; [ fragments ];
-
-        utils = with pkgs; [
-          pre-commit
-          redoc-cli
-          openjdk
-          nixfmt
-          sass
-          stow
-          tmux
-          git
-          zsh
-        ];
+      displayManager = {
+        sddm.enable = true;
+        startx.enable = true;
       };
 
-    in [
+      desktopManager = {
+        plasma5.enable = true;
+        xterm.enable = true;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; let
+    set = {
+      python = with pkgs; [
+        python310
+        virtualenv
+        jetbrains.pycharm-community
+      ];
+
+      go = with pkgs; [go gopls gcc vgo2nix];
+
+      node = with pkgs; [nodejs-18_x];
+
+      android = with pkgs; [android-tools flutter dart];
+
+      db = with pkgs; [postgresql];
+
+      docker = with pkgs; [docker docker-compose];
+
+      apps = with pkgs; [spotify discord vscode slack fragments];
+
+      nixtools = with pkgs; [nixpkgs-fmt vscode-extensions.jnoortheen.nix-ide];
+
+      utils = with pkgs; [
+        pre-commit
+        redoc-cli
+        openjdk
+        nixfmt
+        sass
+        stow
+        tmux
+        git
+        zsh
+      ];
+    };
+  in
+    [
+      exfat-utils
       binutils
       gnumake
       openssh
+      ntfs3g
       unzip
       wget
       dpkg
@@ -162,10 +173,16 @@
       bat
       zip
       jq
-      ntfs3g
-      exfat-utils
-    ] ++ set.python ++ set.go ++ set.node ++ set.android ++ set.db ++ set.docker
-    ++ set.apps ++ set.otherApps ++ set.utils;
+    ]
+    ++ set.python
+    ++ set.go
+    ++ set.node
+    ++ set.android
+    ++ set.db
+    ++ set.docker
+    ++ set.nixtools
+    ++ set.apps
+    ++ set.utils;
 
   programs = {
     adb.enable = true;
@@ -188,15 +205,8 @@
       autosuggestions.enable = true;
       enableCompletion = true;
       syntaxHighlighting.enable = true;
-      # sway.enable = true;
-      # dotDir = ".config/zsh";
-      # plugins = [
-      #   {
-      #     name = "zsh-you-should-use";
-      #     src = pkgs.zsh-you-should-use;
-      #    }
-      # ];
     };
+    # sway.enable = true;
   };
 
   environment = {
@@ -229,11 +239,10 @@
       description = "Set the battery charge threshold";
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = ''
-          ${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+        ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
         ExecStop = ''${pkgs.bash}/bin/bash -c "exit 0"'';
       };
-      wantedBy = [ "multi-user.target" ];
+      wantedBy = ["multi-user.target"];
     };
 
     numLockDisabled = {
@@ -241,11 +250,10 @@
       description = "Disable numlock by default";
       serviceConfig = {
         Type = "oneshot";
-        ExecStart =
-          ''${pkgs.bash}/bin/bash -c "${pkgs.numlockx}/bin/numlockx on"'';
+        ExecStart = ''${pkgs.bash}/bin/bash -c "${pkgs.numlockx}/bin/numlockx on"'';
         ExecStop = ''${pkgs.bash}/bin/bash -c "exit 0"'';
       };
-      wantedBy = [ "multi-user.target" ];
+      wantedBy = ["multi-user.target"];
     };
   };
 
