6067646e089df83969b66711a585ae0384b11522
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-08-08 22:02:26 -0500

    feat: vscode settings + shfmt to shell scripts

--- update.sh
+++ update.sh
@@ -2,20 +2,22 @@
 
 main() {
     printf "Welcome, \033[1;34m%s\033[0m! ❄️❄️❄️\n\n" "$USER"
-    
+
     configuration_devdirectory="$HOME/.dotfiles/"
     configuration_devfile="${configuration_devdirectory}configuration.nix"
-    
-    stat "$configuration_devfile" > /dev/null
+
+    stat "$configuration_devfile" >/dev/null
     sudo nixos-rebuild boot --upgrade --show-trace
-    
-    
-    printf "\n\033[1;34mSuccessfully updated!\033[0m\n\nPress enter to continue"; read -r
+
+    printf "\n\033[1;34mSuccessfully updated!\033[0m\n\nPress enter to continue"
+    read -r
     clear
-    printf "Do you want to reboot?\033[1;33m(y/N)\033[0m "; read -r answer
-    
-    if [ "$answer" = "y" ] || [ "$answer" = "Y" ];
-    then echo "Rebooting, you probably won't see this ❄️❄️❄️"; reboot
+    printf "Do you want to reboot?\033[1;33m(y/N)\033[0m "
+    read -r answer
+
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+        echo "Rebooting, you probably won't see this ❄️❄️❄️"
+        reboot
     else
         echo "Bye! ❄️❄️❄️"
     fi
