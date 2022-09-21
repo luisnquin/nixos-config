0b46ad53059d9c450fdb991f126f4f0d5796147d
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-09-21 06:23:08 -0500

    fix: fixed misspelled git command + new 'ds' alias

--- configuration.nix
+++ configuration.nix
@@ -296,7 +296,7 @@
 
       ## Git shortcuts
       gl1 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-      gl2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      gl2 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
       gsp = "git stash pop";
       gs = "git stash";
       gl = "gl1";
@@ -307,6 +307,7 @@
       ## ???
       runds = "rm -rf compose/nginx/env.json && make compose-up && make build && make run";
       v3 = "cd ~/go/src/gitlab.wiserskills.net/wiserskills/v3/";
+      ds = "v3 && cd dataserver/";
 
       playground = "cd ~/workspace/playground/";
       pypl = "cd ~/workspace/playground/python/";
