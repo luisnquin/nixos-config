3fc65a20255fa95fe53edf5e7ff5c650a37de7c5
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-11-20 15:39:37 -0500

    feat(scripts): added ls command to nyx manager

--- .scripts/main.sh
+++ .scripts/main.sh
@@ -10,7 +10,7 @@ turnoff=0
 subcommand=0
 
 template=$(
-    printf "\033[0;35m%s\033[0m [command] [flags]\n\nAvailable commands:\n  update\tUpdates the machine\n  inspect\tVerifies if the configuration.nix file has been changed and not saved to a git repository\n  style\t\tApplies alejandra style to all .nix files\n  clean\t\tCleans with the old generations\n  voir\t\tChecks if a new NixOS update is available\n\nGlobal flags:\n-r, --reboot\tReboots the machine after the update\n-t, --turnoff\tPower off the machine after the update\n-h, --help\tHelp for the script\n" "$0"
+    printf "\033[0;35mnyx\033[0m [command] [flags]\n\nAvailable commands:\n  update\tUpdates the machine\n  inspect\tVerifies if the configuration.nix file has been changed and not saved to a git repository\n  style\t\tApplies alejandra style to all .nix files\n  ls\t\tList elements in dotfiles directory\n  clean\t\tCleans with the old generations\n  voir\t\tChecks if a new NixOS update is available\n\nGlobal flags:\n-r, --reboot\tReboots the machine after the update\n-t, --turnoff\tPower off the machine after the update\n-h, --help\tHelp for the script\n"
 )
 
 main() {
@@ -27,7 +27,8 @@ main() {
         sudo nixos-rebuild boot --upgrade --show-trace
         printf "\n\033[1;34mSuccessfully updated! ❄️❄️❄️\033[0m\n"
         ;;
-    2) # Clean up
+    2)
+        # Clean up
         greet
 
         nb_of_removed_gens=$(($(nix-env --list-generations | wc -l) - 1))
@@ -53,9 +54,11 @@ main() {
             printf "\n%s generations has been removed\n" "$nb_of_removed_gens"
         fi
         ;;
-    3) # TODO: Voir
+    3)
+        # TODO: Voir
         ;;
-    4) # Inspect
+    4)
+        # Inspect
         stat "$dotfiles_dir/.git/" >/dev/null
 
         nix_files=$(find "$dotfiles_dir" -type f -name "*.nix")
@@ -75,12 +78,17 @@ main() {
             done
         )
         ;;
-    5) # Style
+    5)
+        # Style
         (
             cd "$dotfiles_dir"
             alejandra ./* | head
         )
         ;;
+    6) # ls
+        ls --color=tty "$dotfiles_dir"
+        ;;
+
     esac
 
     if [ "$reboot" = 1 ]; then
@@ -121,6 +129,7 @@ while [ "$#" -gt 0 ]; do
     voir) subcommand=3 shift 1 ;;
     inspect) subcommand=4 shift 1 ;;
     style) subcommand=5 shift 1 ;;
+    ls) subcommand=6 shift 1 ;;
     -h | --h | -help | --help)
         echo "$template"
         shift 1
