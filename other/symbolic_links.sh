af47a372881eefcaa30cd138e7222a4bf0b4d51b
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-11 11:32:10 -0500

    feat: added more k9s configs

--- other/symbolic_links.sh
+++ other/symbolic_links.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir -p ~/.config/spotify-tui/ && ln -P other/.config/spotify-ui/config.yml ~/.config/spotify-tui
-mkdir -p ~/.k9s/ && ln -P other/.k9s/skin.yml ~/.k9s
+mkdir -p ~/.config/spotify-tui/ && ln -Pf other/.config/spotify-ui/config.yml ~/.config/spotify-tui
+mkdir -p ~/.k9s/ && ln -Pf other/.k9s/* ~/.k9s
