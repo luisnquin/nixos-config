2dd3cd3828221062a5a4f319f52de9baca51c0f9
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-13 14:09:08 -0500

    feat(k9s): updated columns displayed in pods table

--- other/symbolic_links.sh
+++ other/symbolic_links.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir -p ~/.config/spotify-tui/ && ln -Pf other/.config/spotify-ui/config.yml ~/.config/spotify-tui
-mkdir -p ~/.config/k9s/ && ln -Pf other/.config/k9s/* ~/.config/k9s
+mkdir -p ~/.config/spotify-tui/ && ln -Pf ./other/.config/spotify-tui/config.yml ~/.config/spotify-tui
+mkdir -p ~/.config/k9s/ && ln -Pf ./other/.config/k9s/* ~/.config/k9s
