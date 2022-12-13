4ce3ca619f13121e3172d00b22861af00b870ad7
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-13 16:58:00 -0500

    feat(k9s): updated config file

--- other/symbolic_links.sh
+++ other/symbolic_links.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 mkdir -p ~/.config/spotify-tui/ && ln -Pf ./other/.config/spotify-tui/config.yml ~/.config/spotify-tui
-mkdir -p ~/.config/k9s/ && ln -Pf ./other/.config/k9s/* ~/.config/k9s
+mkdir -p ~/.config/k9s/ && ln -Pf ./other/.config/k9s/views.yml ~/.config/k9s
+mkdir -p ~/.config/k9s/ && ln -Pf ./other/.config/k9s/skin.yml ~/.config/k9s
