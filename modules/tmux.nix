fce2323d17e67def97e7e46c099c1aebd98d4e05
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-19 08:01:59 -0500

    fix(tmux): fixed tmux stuff

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -16,10 +16,9 @@
       tmuxPlugins.onedark-theme
       tmuxPlugins.fzf-tmux-url
       # tmuxPlugins.continuum
+      tmuxPlugins.tmux-fzf
       tmuxPlugins.sidebar
       tmuxPlugins.sysstat
-      # gitmux
-      fzf
     ];
 
     extraConfig = ''
@@ -34,7 +33,10 @@
       # set -g status-bg black
       # set -g status-fg magenta
 
-      set -g status-right "#{sysstat_cpu} | #{sysstat_mem} | #{sysstat_swap} | #{sysstat_loadavg} | #[fg=cyan]#(echo $USER)#[default]@#H"
+      # TODO: add client env
+      # set -g status-right "#{sysstat_cpu} | #{sysstat_mem} | #{sysstat_swap} | #{sysstat_loadavg} | #[fg=cyan]#(echo $USER)#[default]@#H"
+
+      set -g status-right '#(${pkgs.gitmux}/bin/gitmux "#{pane_current_path}")'
     '';
   };
 }
