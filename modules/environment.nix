164664e4bf9a8eafa3daee31067044777bbfc17d
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-24 20:37:21 -0500

    feat(env): multihand git aliases are now waterproof + comments on non pretty obvious aliases

--- modules/environment.nix
+++ modules/environment.nix
@@ -184,26 +184,36 @@ in {
       g = "git";
       ga = "git add";
       gaa = "git add --all";
+      # Git branch to git branch, ha
       gb = "git branch";
+      # Git commit and verbose 🤨
       gc = "git commit -v";
-      gca = "git commit --amend";
       gck = "git checkout";
       gd = "git diff";
       gds = "git diff --staged";
       gl = "git log --oneline";
-      ggc = "git gc --aggressive";
-      gmgc = "command ls | xargs -i git -C {} gc --aggressive";
       gls = "git log --oneline | head -n 10";
       gl1 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       gl2 = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      # Git garbage collector
+      ggc = "git gc --aggressive";
+      # Git garbage collector over all subdirectories, skipping non-directories and non-git repositories
+      gmgc = "find . -maxdepth 1 -type d | xargs -I {} bash -c 'if git -C {} rev-parse --git-dir > /dev/null 2>&1; then git -C {} gc --aggressive; fi'";
+      # Pull current branch
       ggpull = "git pull origin $(git branch --show-current)";
+      # Pull the specified branch
       gpull = "git pull origin";
-      # TODO: check .git
-      gmb = ''command ls | xargs -i sh -c 'printf " ~ \033[0;94m{}\033[0m:" && git -C {} branch --show-current' '';
-      gmpull = "command ls | xargs -i git -C {} pull";
-      gmpush = "command ls | xargs -i git -C {} push";
+      # Show current branch in all subdirectories, skipping non-directories and non-git repositories
+      gmb = ''find . -maxdepth 1 -type d | xargs -I {} bash -c 'if git -C {} rev-parse --git-dir > /dev/null 2>&1; then printf " ~ \033[0;94m{}\033[0m:"; git -C {} branch --show-current; fi' '';
+      # Pull the current branch of all subdirectories, skipping non-directories and non-git repositories
+      gmpull = "find . -maxdepth 1 -type d | xargs -I {} bash -c 'if git -C {} rev-parse --git-dir > /dev/null 2>&1; then git -C {} pull; fi'";
+      # Push the current branch of all subdirectories, skipping non-directories and non-git repositories
+      gmpush = "find . -maxdepth 1 -type d | xargs -I {} bash -c 'if git -C {} rev-parse --git-dir > /dev/null 2>&1; then git -C {} push; fi'";
+      # Push current branch
       ggpush = "git push origin $(git branch --show-current)";
+      # Push the specified branch
       gpush = "git push origin";
+      # Git reset but it doesn't output anything
       gr = "git reset -q";
       grb = "git rebase";
       gs = "git stash";
