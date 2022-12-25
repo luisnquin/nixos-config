63c2db92ec8433a8de48302ce709363258d63f7a
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-12-25 15:10:56 -0500

    fix(modules): fixed modules imports in default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,14 +1,9 @@
-{
-  config,
-  username,
-  ...
-}: {
+{config, ...}: {
   imports = [
     "/etc/nixos/modules/environment.nix"
-    "/etc/nixos/modules/spotifyd.nix"
+    "/etc/nixos/modules/spotify.nix"
     "/etc/nixos/modules/nvidia.nix"
     "/etc/nixos/modules/tmux.nix"
-    "/etc/nixos/modules/rust.nix"
     "/etc/nixos/modules/git.nix"
   ];
 }
