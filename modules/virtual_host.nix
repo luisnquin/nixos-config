79f7fa4277ff4e43b7a4ff4bc1bc62b0a6f018c4
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-05 22:04:24 -0500

    fix(env): fixed typo in virtual host declaration 'virtualization' -> 's'

--- modules/virtual_host.nix
+++ modules/virtual_host.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # ref: https://search.nixos.org/options?channel=22.11&from=0&size=50&sort=relevance&type=packages&query=virtualization.libvirtd
-  virtualization.libvirtd = {
+  virtualisation.libvirtd = {
     enable = true;
     onBoot = "start"; # Other value: ignore
     onShutdown = "suspend"; # Other value: shutdown
