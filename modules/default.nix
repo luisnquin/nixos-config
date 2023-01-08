9fdff158ede23f20f0c620386dd812fd5a6ad1dc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-08 01:45:56 -0500

    chore(modules): kubernetes.nix -> k8s.nix

--- modules/default.nix
+++ modules/default.nix
@@ -2,11 +2,11 @@
   imports = [
     "/etc/nixos/modules/virtual_host.nix"
     "/etc/nixos/modules/environment.nix"
-    "/etc/nixos/modules/kubernetes.nix"
     "/etc/nixos/modules/spotify.nix"
     "/etc/nixos/modules/docker.nix"
     "/etc/nixos/modules/nvidia.nix"
     "/etc/nixos/modules/tmux.nix"
     "/etc/nixos/modules/git.nix"
+    "/etc/nixos/modules/k8s.nix"
   ];
 }
