6495c99330a42dc73c8dccc2b2c0e1f9677b11b0
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-11 19:53:39 -0500

    feat(modules): added read of spotify-tui configuration file

--- modules/spotify.nix
+++ modules/spotify.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  #  lib,
   ...
 }
 : let
@@ -47,91 +46,8 @@ in {
     spotifyd
     spotify
   ];
-}
-/*
-home-manager.users."${owner.username}" = {
-  xdg.configFile."spotify-tui/config.yml" = {
-    source = lib.yamlGenerate "spotify-tui-config" {
-      theme = let
-        color = {
-          skyBlue = "72, 186, 224";
-          white = "216, 219, 226"; # pretty white
-          hotPink = "222, 22, 82";
-          deadRed = "130, 78, 84";
-          friendlyRed = "219, 39, 60";
-          drunkYellow = "222, 200, 31";
-          pinkishWhite = "232, 218, 222";
-          stiffYellow = "237, 177, 26";
-          deadSkyBlue = "88, 164, 176";
-          bluishWhite = "169, 188, 208";
-          bluishGray = "89, 98, 117";
-        };
-      in {
-        active = "${color.skyBlue}";
-        inactive = "${color.white}";
-        banner = "${color.hotPink}";
 
-        error_border = "${color.deadRed}";
-        error_text = "${color.friendlyRed}";
-        hint = "${color.drunkYellow}";
-
-        playbar_background = "${color.pinkishWhite}";
-        playbar_progress = "${color.hotPink}";
-        playbar_progress_text = "${color.pinkishWhite}";
-        playbar_text = "${color.pinkishWhite}";
-
-        hovered = "${color.stiffYellow}";
-        selected = "${color.deadSkyBlue}";
-
-        text = "${color.bluishWhite}";
-        header = "${color.bluishGray}";
-      };
-
-      behavior = {
-        seek_milliseconds = 5000;
-        volume_increment = 10;
-
-        tick_rate_milliseconds = 350;
-        enable_text_emphasis = true;
-        show_loading_indicator = true;
-        enforce_wide_search_bar = false;
-
-        liked_icon = "♥";
-        shuffle_icon = "咽";
-        repeat_track_icon = "綾";
-        repeat_context_icon = "凌";
-        playing_icon = "▶";
-        paused_icon = "⏸";
-
-        set_window_title = true;
-      };
-
-      keybindings = {
-        back = "ctrl-q";
-        jump_to_album = "a";
-        jump_to_artist_album = "A";
-
-        manage_devices = "d";
-        decrease_volume = "-";
-        increase_volume = "+";
-        toggle_playback = " ";
-        seek_backwards = "<";
-        seek_forwards = ">";
-        next_track = "n";
-        previous_track = "p";
-        copy_song_url = "c";
-        copy_album_url = "C";
-        help = "?";
-        shuffle = "ctrl-s";
-        repeat = "r";
-        search = "/";
-        audio_analysis = "v";
-        jump_to_context = "o";
-        basic_view = "B";
-        add_item_to_queue = "z";
-      };
-    };
+  home-manager.users."${owner.username}" = {
+    xdg.configFile."spotify-tui/config.yml".text = builtins.readFile ../dots/home/spotify-tui/config.yml;
   };
-};
-*/
-
+}
