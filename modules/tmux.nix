a956f9490e63a12f00b1e926f6c9ef5c5dce5e2f
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-15 00:01:11 -0500

    feat(tmux): adde behavior to auto copy selected text + commented extra config have been moved out

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -22,18 +22,10 @@
 
     extraConfig = ''
       setw -g mouse on
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip"
+
       set -g window-status-separator ""
-
       set-option -ga terminal-overrides ",*256col*:Tc:RGB"
-
-      # set -g automatic-rename off
-      # set -g status-left-length 40
-      # set -g status-left "#S #[fg=white]#[fg=yellow]#I #[fg=cyan]#P"
-      # set -g status-bg black
-      # set -g status-fg magenta
-
-      # TODO: add client env
-      # set -g status-right "#{sysstat_cpu} | #{sysstat_mem} | #{sysstat_swap} | #{sysstat_loadavg} | #[fg=cyan]#(echo $USER)#[default]@#H"
 
       set -g status-right '#(${pkgs.gitmux}/bin/gitmux "#{pane_current_path}")'
     '';
@@ -45,3 +37,10 @@
     etc."gitmux.conf".text = builtins.readFile ../dots/etc/gitmux.conf;
   };
 }
+# set -g automatic-rename off
+# set -g status-left-length 40
+# set -g status-left "#S #[fg=white]#[fg=yellow]#I #[fg=cyan]#P"
+# set -g status-bg black
+# set -g status-fg magenta
+# set -g status-right "#{sysstat_cpu} | #{sysstat_mem} | #{sysstat_swap} | #{sysstat_loadavg} | #[fg=cyan]#(echo $USER)#[default]@#H"
+
