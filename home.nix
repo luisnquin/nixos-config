5f32ee31902c3924e63ebbe56ecd226ba7bb03f4
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-15 01:41:00 -0500

    chore(home): removed useless comments

--- home.nix
+++ home.nix
@@ -14,16 +14,4 @@ in {
 
     xdg.enable = true;
   };
-
-  #programs.git = {
-  #  enable = true;
-  #  userName = "luisnquin";
-  #  userEmail = "lpaandres2020@gmail.com";
-  #};
-  #
-  #programs.bash = {
-  #  enable = true;
-  #};
-  #
-  #programs.go.enable = true;
 }
