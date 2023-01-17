9a89c463882d07dc69180bdc6200e3294593ab09
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-17 00:02:31 -0500

    feat(nix): added more personal settings

--- modules/nix.nix
+++ modules/nix.nix
@@ -21,12 +21,21 @@
       dates = ["13:00"];
     };
 
+    # Ref: https://nixos.org/manual/nix/stable/command-ref/conf-file.html
     settings = {
+      # Nix automatically detects files in the store that have identical contents, and replaces them with hard links to a single copy.
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
+      keep-outputs = true;
+      download-attempts = 3;
       # Required by cachix
       trusted-users = ["root" "luisnquin"];
+      # Defines the maximum number of jobs that Nix will try to build in parallel.
       max-jobs = 4;
+      # When free disk space in /nix/store drops below min-free during a build, Nix performs a garbage-collection.
+      min-free = 10000000000; # 10GB
+      # Number of seconds between checking free disk space.
+      min-free-check-interval = 30;
     };
   };
 }
