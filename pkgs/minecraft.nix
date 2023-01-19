586434faf07ad9edd2bef11be9056f9101870b8a
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-18 21:39:48 -0500

    chore(pkgs): added make desktop item draft

--- pkgs/minecraft.nix
+++ pkgs/minecraft.nix
@@ -1,49 +1,63 @@
 {
-  # jre,
+  lib,
   stdenv,
   openjdk,
+  fetchurl,
   makeWrapper,
-  makeDesktopItem,
+  #  makeDesktopItem,
+  #  copyDesktopItems,
   ...
-}: let
-  #minecraft = makeDesktopItem rec {
-  #  name = "Minecraft";
-  #  icon = "OpenJK_Icon_128";
-  #  exec = "${openjdk}/bin/java -jar ${file}";
-  #  comment = "Unofficial English patch for LauncherFenix";
-  #  desktopName = "Minecraft";
-  #  genericName = "Minecraft";
-  #  categories = ["Game"];
-  #};
-in
-  stdenv.mkDerivation rec {
-    name = "minecraft";
-    src = builtins.fetchurl {
-      url = "https://github.com/ztgasdf/fenix-english/releases/download/v0.2/LauncherFenix-Minecraft-v7-patched.jar";
-      sha256 = "118l794ljyc43rn3px67kb5ryw4s8dcz239w487wmj2jf14sbnc1";
-    };
+}:
+# How to build ref: https://git.marvid.fr/scolobb/nix-GINsim/src/branch/master/ginsim.nix
+stdenv.mkDerivation rec {
+  name = "minecraft";
+  src = fetchurl {
+    url = "https://github.com/ztgasdf/fenix-english/releases/download/v0.2/LauncherFenix-Minecraft-v7-patched.jar";
+    sha256 = "118l794ljyc43rn3px67kb5ryw4s8dcz239w487wmj2jf14sbnc1";
+  };
 
-    dontUnpack = true;
+  dontUnpack = true;
 
-    nativeBuildInputs = [makeWrapper];
+  nativeBuildInputs = [
+    # copyDesktopItems
+    makeWrapper
+  ];
 
-    installPhase = ''
-      mkdir -pv $out/share/java $out/bin
-      cp ${src} $out/share/java/minecraft.jar
+  propagatedBuildInputs = [
+    openjdk
+  ];
 
-      makeWrapper ${openjdk}/bin/java $out/bin/minecraft \
-        --add-flags "-jar $out/share/java/minecraft.jar" \
-        --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
-        --set _JAVA_AWT_WM_NONREPARENTING 1
-    '';
+  installPhase = ''
+    mkdir -pv $out/share/java $out/bin
+    cp ${src} $out/share/java/minecraft.jar
 
-    #postInstall = ''
-    #  ls ${minecraft}
-    #  mkdir -p $out/share/applications
-    #  ln -s ${minecraft}/share/applications/* $out/share/applications
-    #'';
-  }
-#installPhase = ''
-#  cp $src $out/bin
-#'';
+    makeWrapper ${openjdk}/bin/java $out/bin/minecraft \
+      --add-flags "-jar $out/share/java/minecraft.jar" \
+      --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on' \
+      --set _JAVA_AWT_WM_NONREPARENTING 1
+  '';
 
+  # postInstall = ''
+  #   mkdir -p $out/share/applications
+  #   ln -s ${minecraft}/share/applications/* $out/share/applications
+  # '';
+
+  #desktopItems = [
+  #  (makeDesktopItem {
+  #    name = "minecraft";
+  #    exec = "minecraft";
+  #    desktopName = "Minecraft";
+  #    genericName = "Minecraft";
+  #    icon = "/etc/nixos/pkgs/icons/minecraft.png";
+  #    comment = "Join people all over the world playing Minecraft, one of the most popular videogames around!";
+  #    categories = ["Game"];
+  #    type = "Application";
+  #  })
+  #];
+
+  meta = with lib; {
+    description = "Unofficial English patch for LauncherFenix";
+    homepage = "https://github.com/ztgasdf/fenix-english";
+    maintainers = with maintainers; ["${owner}"];
+  };
+}
