67276956979fcf4235e9d51720d75fed8f47d93a
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-18 19:35:49 -0500

    refactor(modules): some environment components moved to shell file

--- modules/environment.nix
+++ modules/environment.nix
@@ -5,218 +5,143 @@
 }: let
   owner = import "/etc/nixos/owner.nix";
 in {
-  environment = {
-    systemPackages = with pkgs; let
-      gg = {
-        apps = [
-          obs-studio
-          discord
-          vivaldi
-          etcher
-          brave
-          slack
-          gimp
-        ];
+  environment.systemPackages = with pkgs; let
+    gg = {
+      apps = [
+        obs-studio
+        discord
+        vivaldi
+        etcher
+        brave
+        slack
+        gimp
+      ];
 
-        dev = [
-          # Go-related
-          air
-          delve
-          gcc
-          go_1_20
-          go-protobuf
-          gofumpt
-          golangci-lint
-          gopls
-          gotools
-          govulncheck
-          grpc-tools
+      dev = [
+        # Go-related
+        air
+        delve
+        gcc
+        go_1_20
+        go-protobuf
+        gofumpt
+        golangci-lint
+        gopls
+        gotools
+        govulncheck
+        grpc-tools
 
-          # Rust-related
-          cargo
-          clippy
-          rust-analyzer
-          rustc
-          rustfmt
-          rustup
-          vscode-extensions.matklad.rust-analyzer
+        # Rust-related
+        cargo
+        clippy
+        rust-analyzer
+        rustc
+        rustfmt
+        rustup
+        vscode-extensions.matklad.rust-analyzer
 
-          # JavaScript-related
-          nodePackages.typescript
-          nodePackages.pnpm
-          nodejs-18_x
-          deno
-          bun
+        # JavaScript-related
+        nodePackages.typescript
+        nodePackages.pnpm
+        nodejs-18_x
+        deno
+        bun
 
-          # Nix-related
-          alejandra
-          nixos-option
-          rnix-lsp
-          vscode-extensions.jnoortheen.nix-ide
-          nix-prefetch-git # Tool to get information from remote repository like sha256
+        # Nix-related
+        alejandra
+        nixos-option
+        rnix-lsp
+        vscode-extensions.jnoortheen.nix-ide
+        nix-prefetch-git # Tool to get information from remote repository like sha256
 
-          # Python-related
-          pyright
-          python311
-          python310Packages.pipx
-          virtualenv
+        # Python-related
+        pyright
+        python311
+        python310Packages.pipx
+        virtualenv
 
-          # Other
-          nodePackages.firebase-tools
-          license-generator
-          onlyoffice-bin
-          redoc-cli
-          awscli2
-          vscode
-          clang
+        # Other
+        nodePackages.firebase-tools
+        license-generator
+        onlyoffice-bin
+        redoc-cli
+        awscli2
+        vscode
+        clang
 
-          # websocat
-          # dbeaver
-          # gobang
+        # websocat
+        # dbeaver
+        # gobang
 
-          minify # HTML, CSS, and JavaScript minifier
-          shfmt # Shell code formatter
-          zathura # PDF viewer
-          sqlc # SQL generator
-          # Processors
-          csvkit
-          htmlq
-          dsq
-          jq
-          yq
-          # HTTP
-          postman
-          ngrok
-        ];
+        minify # HTML, CSS, and JavaScript minifier
+        shfmt # Shell code formatter
+        zathura # PDF viewer
+        sqlc # SQL generator
+        # Processors
+        csvkit
+        htmlq
+        dsq
+        jq
+        yq
+        # HTTP
+        postman
+        ngrok
+      ];
 
-        osint = [
-          exiftool
-          maigret
-          whois
-        ];
+      osint = [
+        exiftool
+        maigret
+        whois
+      ];
 
-        preferences = [
-          rclone # For management in cloud storages
-          freshfetch # neofetch replacement
-          xclip # Clipboard
-          tldr # Alternative to man
+      preferences = [
+        rclone # For management in cloud storages
+        freshfetch # neofetch replacement
+        xclip # Clipboard
+        tldr # Alternative to man
 
-          # Fufu stuff
-          octofetch
-          nyancat
-          genact
-          tree
-        ];
+        # Fufu stuff
+        octofetch
+        nyancat
+        genact
+        tree
+      ];
 
-        core = [
-          stdenv_32bit
-          coreutils
-          libsecret
-          binutils
-          openssh
-          openssl
-          gnumake
-          openjdk
-          neovim
-          wget
-          vim
-          bat
-          exa # ls command replacement
-          vlc
+      core = [
+        stdenv_32bit
+        coreutils
+        libsecret
+        binutils
+        openssh
+        openssl
+        gnumake
+        openjdk
+        neovim
+        wget
+        vim
+        bat
+        exa # ls command replacement
+        vlc
 
-          # NTFS
-          ntfs3g
-          exfat
+        # NTFS
+        ntfs3g
+        exfat
 
-          gnome.seahorse # Keyring
+        gnome.seahorse # Keyring
 
-          # System monitoring tools
-          gotop
-          btop
-          htop
+        # System monitoring tools
+        gotop
+        btop
+        htop
 
-          # Compressed files
-          p7zip
-          unzip
-          unar
-          zip
-        ];
-      };
-    in (builtins.concatLists (builtins.attrValues gg));
-
-    shellAliases = {
-      # My own
-      nyx = "sh ~/.dotfiles/.scripts/main.sh";
-      gest = "go clean -testcache && go test -v";
-      setup = "tmux rename-window \"setup 🦭\" \\; split-window -h \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; send-keys -t 2 \"spt\" ENTER \\; send-keys -t 1 \"gotop --nvidia --color=vice\" ENTER \\; send-keys -t 3 \"k9s  --readonly\" ENTER; clear";
-
-      # It's not like you're not needed
-      runds = "(ds; rm -rf compose/nginx/env.json && make compose-up && make build && make run)";
-      v3 = "cd ~/go/src/gitlab.wiserskills.net/wiserskills/v3/";
-      ds = "v3 && cd dataserver/";
-
-      # Instant tp to some directories
-      dot = "cd ~/.dotfiles/";
-      down = "cd ~/Downloads/";
-      etc = "cd ~/.etc/";
-      gopl = "cd ~/Workspace/playground/go/";
-      pl = "cd ~/Workspace/playground/";
-      pr = "cd ~/Workspace/projects/";
-      tests = "cd ~/Tests/";
-      tmp = "cd /tmp/";
-      workspace = "cd ~/Workspace/";
-
-      # Overwriten program calls
-      cp = "cp --interactive";
-      rm = "rm --interactive";
-      mv = "mv --interactive";
-      du = "du --human-readable";
-      xclip = "xclip -selection c";
-      ls = "exa --icons";
-      ll = "exa -l";
-      la = "exa -a";
-      cat = "bat -p";
-
-      # Those who are lazy to write definitely go here
-      open = "xdg-open";
-      rc = "rclone";
-      cls = "clear";
-      poff = "poweroff";
-      actl = "act --list";
-      neofetch = "freshfetch";
-      whoseport = "netstat -tulpln 2> /dev/null | grep :";
-      nyancat = "nyancat --no-counter";
-      ale = "alejandra --quiet";
-      dud = "du --human-readable --summarize";
-      man = "tldr";
-      transg = "transgression-tui";
-      ns = "nix-shell";
-      # Abstraction
-      listen = "ngrok http";
-      py = "python3";
-      share = "ngrok http";
-      top = "gotop --nvidia --color=vice";
-      unrar = "unar";
+        # Compressed files
+        p7zip
+        unzip
+        unar
+        zip
+      ];
     };
-
-    # sessionVariables = {};
-
-    variables = {
-      # The other related config only apply to the build
-      NIXPKGS_ALLOW_UNFREE = "1";
-      PATH = "$PATH:$GORROT:$GOPATH/bin";
-      GOPATH = "/home/$USER/go";
-      EDITOR = "nano";
-    };
-
-    # Google search, zsh history search, highlighter for conventional
-    # branches and tmux startup in non-vscode editors
-    #
-    # Shell script code called during interactive shell initialisation.
-    # This code is assumed to be shell-independent, which means you
-    # should stick to pure sh without sh word split.
-    interactiveShellInit = builtins.readFile ../dots/.shrc;
-  };
+  in (builtins.concatLists (builtins.attrValues gg));
 
   # Configuration files
   environment.etc = {
--- modules/shell.nix
+++ modules/shell.nix
@@ -5,8 +5,6 @@
 }: let
   owner = import "/etc/nixos/owner.nix";
 in {
-  environment.shells = [pkgs.zsh];
-
   home-manager.users."${owner.username}" = {
     programs.zsh = {
       enable = true;
@@ -156,5 +154,83 @@ in {
         };
       };
     };
+  };
+
+  environment = {
+    # Google search, zsh history search, highlighter for conventional
+    # branches and tmux startup in non-vscode editors
+    #
+    # Shell script code called during interactive shell initialisation.
+    # This code is assumed to be shell-independent, which means you
+    # should stick to pure sh without sh word split.
+    interactiveShellInit = builtins.readFile ../dots/.shrc;
+
+    # sessionVariables = {};
+
+    variables = {
+      # The other related config only apply to the build
+      NIXPKGS_ALLOW_UNFREE = "1";
+      PATH = "$PATH:$GORROT:$GOPATH/bin";
+      GOPATH = "/home/$USER/go";
+      EDITOR = "nano";
+    };
+
+    shellAliases = {
+      # My own
+      nyx = "sh ~/.dotfiles/.scripts/main.sh";
+      gest = "go clean -testcache && go test -v";
+      setup = "tmux rename-window \"setup 🦭\" \\; split-window -h \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; send-keys -t 2 \"spt\" ENTER \\; send-keys -t 1 \"gotop --nvidia --color=vice\" ENTER \\; send-keys -t 3 \"k9s  --readonly\" ENTER; clear";
+
+      # It's not like you're not needed
+      runds = "(ds; rm -rf compose/nginx/env.json && make compose-up && make build && make run)";
+      v3 = "cd ~/go/src/gitlab.wiserskills.net/wiserskills/v3/";
+      ds = "v3 && cd dataserver/";
+
+      # Instant tp to some directories
+      dot = "cd ~/.dotfiles/";
+      down = "cd ~/Downloads/";
+      etc = "cd ~/.etc/";
+      gopl = "cd ~/Workspace/playground/go/";
+      pl = "cd ~/Workspace/playground/";
+      pr = "cd ~/Workspace/projects/";
+      tests = "cd ~/Tests/";
+      tmp = "cd /tmp/";
+      workspace = "cd ~/Workspace/";
+
+      # Overwriten program calls
+      cp = "cp --interactive";
+      rm = "rm --interactive";
+      mv = "mv --interactive";
+      du = "du --human-readable";
+      xclip = "xclip -selection c";
+      ls = "exa --icons";
+      ll = "exa -l";
+      la = "exa -a";
+      cat = "bat -p";
+
+      # Those who are lazy to write definitely go here
+      "~" = "cd /home/$USER/";
+      open = "xdg-open";
+      rc = "rclone";
+      cls = "clear";
+      poff = "poweroff";
+      actl = "act --list";
+      neofetch = "freshfetch";
+      whoseport = "netstat -tulpln 2> /dev/null | grep :";
+      nyancat = "nyancat --no-counter";
+      ale = "alejandra --quiet";
+      dud = "du --human-readable --summarize";
+      man = "tldr";
+      transg = "transgression-tui";
+      ns = "nix-shell";
+      # Abstraction
+      listen = "ngrok http";
+      py = "python3";
+      share = "ngrok http";
+      top = "gotop --nvidia --color=vice";
+      unrar = "unar";
+    };
+
+    shells = [pkgs.zsh];
   };
 }
