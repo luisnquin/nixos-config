7745ee5e8f3069a95fe96821dcbe7d9d82249f35
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-22 10:23:08 -0500

    feat(pkgs): added nyx pkg metadata + updated rev

--- pkgs/nyx.nix
+++ pkgs/nyx.nix
@@ -1,17 +1,27 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.stdenv.mkDerivation {
-  name = "nyx";
-  src = pkgs.fetchFromGitHub {
-    owner = "luisnquin";
-    repo = "nixos-config";
-    rev = "81693bd179a390ccbc1432b475e4f3d7247bde9a";
-    sha256 = "1pfasi9asfb8s0aqyz9vnp94v4pm2xr41wkfcikm0vpd2vjfzr8g";
-  };
+{pkgs ? import <nixpkgs> {}}: let
+  owner = "luisnquin";
+  repo = "nixos-config";
+in
+  pkgs.stdenv.mkDerivation {
+    name = "nyx";
+    src = pkgs.fetchFromGitHub {
+      owner = owner;
+      repo = repo;
+      rev = "9a6cb5f53a6c85fc6c8d67bea99d56b8bdee25a5";
+      sha256 = "1irwvir4jqxipd3a8z3klk771h6nywsplf9659xqd19dw6961n4m";
+    };
 
-  installPhase = ''
-    mkdir -p $out/bin/
-    cp $src/.scripts/main.sh $out/bin/
-    mv $out/bin/main.sh $out/bin/nyx
-    chmod +x $out/bin/nyx
-  '';
-}
+    installPhase = ''
+      mkdir -p $out/bin/
+      cp $src/.scripts/main.sh $out/bin/
+      mv $out/bin/main.sh $out/bin/nyx
+      chmod +x $out/bin/nyx
+    '';
+
+    meta = with pkgs.lib; {
+      description = "A CLI tool to manage NixOS computers";
+      homepage = "https://github.com/${owner}/${pname}";
+      license = licenses.mit;
+      maintainers = with maintainers; ["${owner}"];
+    };
+  }
