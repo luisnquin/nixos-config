dd23a5a7d23af2c8fb3a43d59efceaeca17843c9
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-22 00:00:57 -0500

    feat(shell): added alacritty + config

--- modules/shell.nix
+++ modules/shell.nix
@@ -56,6 +56,13 @@ in {
         }
       ];
     };
+
+    programs.alacritty = {
+      enable = true;
+      package = pkgs.alacritty;
+    };
+
+    xdg.configFile."alacritty.yml".text = builtins.readFile ../dots/home/alacritty.yml;
   };
 
   programs = {
