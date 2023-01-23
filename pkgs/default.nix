8b0d311dcfabcd4ea3e25fe622521fb8a19e87a8
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-22 20:44:20 -0500

    feat(pkgs): added chatgpt desktop but cannot be used at runtime(by now)

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
+    # (callPackage ./chatgpt-desktop.nix {})
     (callPackage ./transg-tui.nix {})
     (callPackage ./minecraft.nix {})
     (callPackage ./tomato.nix {})
