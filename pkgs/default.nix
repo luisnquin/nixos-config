9cfc7ad094047069b45d9deadfce7634700eb963
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-28 08:20:10 -0500

    feat(pkgs): added npkill

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,10 +5,12 @@
 }: {
   environment.systemPackages = with pkgs; [
     # (callPackage ./chatgpt-desktop.nix {})
+    # (callPackage ./terminalizer.nix {})
     (callPackage ./transg-tui.nix {})
     (callPackage ./minecraft.nix {})
-    (callPackage ./tomato.nix {})
     (callPackage ./ai-cli.nix {})
+    (callPackage ./npkill.nix {})
+    (callPackage ./tomato.nix {})
     (callPackage ./nyx.nix {})
     (callPackage ./no.nix {})
     # (callPackage ./nao.nix {})
---
+++ pkgs/npkill.nix
@@ -0,0 +1,22 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.buildNpmPackage rec {
+  pname = "npkill";
+  version = "0.10.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "voidcosmos";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-oi/mZ4mLHhfXiLpoXCZ/8UNGzvUpjMMqX9bYRXXhYMY=";
+  };
+
+  npmDepsHash = "sha256-NM5Iuq0ys2lDoajfjetxqziZzEeVRNb748vt67oYj3w=";
+  npmPackFlags = ["--ignore-scripts"];
+
+  meta = with pkgs.lib; {
+    description = "List any node_modules package dir in your system and how heavy they are. You can then select which ones you want to erase to free up space";
+    homepage = "https://npkill.js.org";
+    license = licenses.mit;
+    maintainers = with maintainers; [luisnquin];
+  };
+}
