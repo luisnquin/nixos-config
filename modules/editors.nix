fe55f75cb4c0f58c777e00f8837333eff75fb689
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-01-30 23:07:19 -0500

    fix(editor): removed vscode extensions(thanks home manager)

--- modules/editors.nix
+++ modules/editors.nix
@@ -11,10 +11,10 @@ in {
       enableExtensionUpdateCheck = true;
       enableUpdateCheck = true;
       package = vscode;
-      extensions = with vscode-extensions; [
-        matklad.rust-analyzer
-        jnoortheen.nix-ide
-      ];
+      # extensions = with vscode-extensions; [
+      #   matklad.rust-analyzer
+      #   jnoortheen.nix-ide
+      # ];
       mutableExtensionsDir = true;
     };
   };
