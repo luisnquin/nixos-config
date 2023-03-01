ae9fdfd38ce1b74bb9a84d51bd986d9e57bcf434
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-02-28 19:35:47 -0500

    feat(spotify): removed spotifyd and spotify-tui

--- modules/spotify.nix
+++ modules/spotify.nix
@@ -6,48 +6,7 @@
 : let
   owner = import "/etc/nixos/owner.nix";
 in {
-  services.spotifyd = {
-    enable = true;
-
-    # Ref: http://spotifyd.github.io/spotifyd/config/File.html
-    settings.global = {
-      device = "hw:0,0"; # 'aplay -l' or 'aplay -L'
-      control = "hw:0,0"; # Like the sugar
-      device_name = config.networking.hostName;
-      cache_path = ''/home/${owner.username}/.cache/spotifyd'';
-      max_cache_size = 1000000000;
-      volume_normalisation = false;
-      device_type = "speaker";
-      no_audio_cache = false;
-      initial_volume = "80";
-      dbus_type = "session"; # this scope should be enough
-      use_mpris = true;
-      backend = "alsa"; # I'm using pipewire, so
-      autoplay = true;
-      bitrate = 320;
-
-      username = owner.spotifyUsername;
-      password = owner.spotifyPassword;
-    };
-  };
-
-  systemd.services.spotifyd = {
-    after = ["network-online.target" "sound.target"];
-    serviceConfig = {
-      SupplementaryGroups = ["audio"];
-      DynamicUser = true;
-      Restart = "always";
-    };
-  };
-
   environment.systemPackages = with pkgs; [
-    # spicetify-cli
-    spotify-tui
-    spotifyd
     spotify
   ];
-
-  home-manager.users."${owner.username}" = {
-    xdg.configFile."spotify-tui/config.yml".text = builtins.readFile ../dots/home/spotify-tui/config.yml;
-  };
 }
--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -39,7 +39,7 @@
 
     shellAliases = {
       gest = "go clean -testcache && richgo test -v";
-      setup = "tmux rename-window \"setup 🦭\" \\; split-window -h \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; send-keys -t 2 \"spt\" ENTER \\; send-keys -t 1 \"gotop --nvidia --color=vice\" ENTER \\; send-keys -t 3 \"k9s  --readonly\" ENTER; clear";
+      setup = "tmux rename-window \"setup 🦭\" \\; split-window -h \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; split-window -v \\; resize-pane -D 3 \\; selectp -t 0 \\; send-keys -t 1 \"gotop --nvidia --color=vice\" ENTER \\; send-keys -t 3 \"k9s  --readonly\" ENTER; clear";
 
       # It's not like you're not needed
       runds = "(ds; rm -rf compose/nginx/env.json && make compose-up && make build && make run)";
