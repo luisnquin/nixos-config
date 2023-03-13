3876e9ba71e0a5cdcd5c8508788a736e2cd26cfb
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-12 21:25:36 -0500

    chore(pkgs): removed ai-cli and terminalizer

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,10 +4,8 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    # (callPackage ./terminalizer.nix {})
     (callPackage ./transg-tui.nix {})
     (callPackage ./minecraft.nix {})
-    (callPackage ./ai-cli.nix {})
     (callPackage ./npkill.nix {})
     (callPackage ./tomato.nix {})
     (callPackage ./nyx.nix {})
