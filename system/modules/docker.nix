4c122c30a22a5eb4b3c161f72fd1eb0cf9a4ccf3
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-31 20:59:08 -0500

    chore(docker): added --force flag to dra alias

--- system/modules/docker.nix
+++ system/modules/docker.nix
@@ -25,7 +25,7 @@
 
     shellAliases = {
       dka = "docker kill $(docker ps -qa) 2> /dev/null";
-      dra = "docker rm $(docker ps -qa) 2> /dev/null";
+      dra = "docker rm -f $(docker ps -qa) 2> /dev/null";
       dkra = "dka && dra";
       dria = "docker rmi -f $(docker image ls -qa)";
       dils = "docker image ls";
