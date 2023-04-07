7c070e3632ee6e4a7d7793b7853f379bd96dbd23
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-04-06 23:34:12 -0500

    feat(system): script to globally toggle microphone between muted and unmuted states

---
+++ system/dots/scripts/mic-toggle-notify.sh
@@ -0,0 +1,8 @@
+microphone_state=''
+
+if [[ $(output=$(amixer set Capture toggle); echo "$output" | grep -m 1 --fixed-strings --only-matching '[on]') ]]; then
+  microphone_state=unmuted;
+else
+  microphone_state=muted; fi
+
+notify-send "Global microphone state: $microphone_state" "Triggered by keybinding (Ctrl + Alt + M)" --app-name=System --expire-time=2500 --urgency=normal
