739caa2007f50b85e1b0cd77de1736573f9870ca
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-04-08 14:28:17 -0500

    feat(home): added btop++ config

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,12 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   imports = [
     ./terminal/default.nix
     ./editors.nix
     ./spotify.nix
+    ./btop.nix
     ./k8s.nix
     ./git.nix
   ];
--- home/modules/k8s.nix
+++ home/modules/k8s.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   xdg.configFile = {
     "k9s/views.yml".text = builtins.readFile ../dots/k9s/views.yml;
     "k9s/skin.yml".text = builtins.readFile ../dots/k9s/skin.yml;
