853f8824ed1769f8b83c23fe6fa82b18fe547c86
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-09 17:38:23 -0600

    feat(home): added `lazydocker` config file

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -3,6 +3,7 @@
     ./terminal/default.nix
     ./editors.nix
     ./spotify.nix
+    ./docker.nix
     ./btop.nix
     ./k8s.nix
     ./git.nix
---
+++ home/modules/docker.nix
@@ -0,0 +1,5 @@
+{config, ...}: {
+  xdg.configFile = {
+    "lazydocker/config.yml".text = builtins.readFile ../dots/lazydocker/config.yml;
+  };
+}
