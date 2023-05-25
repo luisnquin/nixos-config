80d5b02ee8c50df3ca357685d7b8a4a883c4ebfc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-24 19:36:07 -0600

    feat(systemctl): added unit service to ensure home fs

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   imports = [
     ./successful-ping-to-google.nix
+    ./ensure-home-fs.nix
     ./battery-limit.nix
     ./tldr-update.nix
   ];
---
+++ system/services/ensure-home-fs.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  username = (import ../../owner.nix).username;
+in {
+  systemd.services.ensure-home-fs = {
+    enable = true;
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash \
+               -c 'mkdir -p /home/${username}/Workspace/playground/ \
+                            /home/${username}/Workspace/projects/ \
+                            /home/${username}/Saves/ \
+                            /home/${username}/Work/ \
+                            /home/${username}/Work/ \
+                            /home/${username}/Temp/ \
+                            /home/${username}/.etc/
+      '';
+    };
+  };
+}
