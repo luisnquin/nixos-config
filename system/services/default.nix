d7da2443a08d1d719493764d95e3895b5918a3bc
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-06-10 02:51:23 -0600

    system(services): added 'discord-skip-host-update'

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   imports = [
     ./successful-ping-to-google.nix
+    ./discord-skip-host-update.nix
     ./ensure-home-fs.nix
     ./battery-limit.nix
     ./tldr-update.nix
---
+++ system/services/discord-skip-host-update.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  username = (import ../../owner.nix).username;
+in {
+  systemd.services.discord-skip-host-update = {
+    enable = true;
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash -c "DISCORD_DIR=\"/home/\${username}/.config/discord\"; \
+              DISCORD_SETTINGS_FILE=\"\$DISCORD_DIR/settings.json\"; \
+              if ! test -f \"\$DISCORD_SETTINGS_FILE\"; then \
+                  mkdir -p \"\$DISCORD_DIR\" && echo '{\"SKIP_HOST_UPDATE\": true}' > \"\$DISCORD_SETTINGS_FILE\"; \
+              else \
+                  ${pkgs.jq}/bin/jq -s '.[0] + {\"SKIP_HOST_UPDATE\": true}' \"\$DISCORD_SETTINGS_FILE\" > \"\$DISCORD_SETTINGS_FILE\"; \
+              fi"
+      '';
+    };
+  };
+}
