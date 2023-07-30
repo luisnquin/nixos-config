cff5d06dd242bae15917e042d7743e3ca6484ed5
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-07-30 13:24:24 -0600

    feat(k8s): added kw and k aliases for pretty obvious typing reasons

--- system/modules/k8s.nix
+++ system/modules/k8s.nix
@@ -12,6 +12,8 @@
       # The main config.yml is generated "in runtime" and there's no other
       # way without overdo with `secrets.nix`
       k9s = "k9s --crumbsless --logoless --refresh=2";
+      kw = "k9s";
+      k = "kubectl";
     };
   };
 }
