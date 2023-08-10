34f9ce3c09e4b4e758c682a1dba86b620b664011
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-08-09 20:40:21 -0600

    feat: updated mime apps

--- home/modules/mime-apps.nix
+++ home/modules/mime-apps.nix
@@ -1,18 +1,28 @@
 {host, ...}: {
-  xdg.mimeApps = {
+  xdg.mimeApps = let
+    dolphinDesktop = "org.kde.dolphin.desktop";
+    vivaldiDesktop = "vivaldi-stable.desktop";
+    braveDesktop = "brave.desktop";
+  in {
     enable = true;
+
+    associations.added = {
+      "inode/directory" = dolphinDesktop;
+      "x-scheme-handler/geo" = "wheelmap-geo-handler.desktop";
+      "x-scheme-handler/http" = braveDesktop;
+      "x-scheme-handler/https" = braveDesktop;
+      "x-scheme-handler/mailto" = vivaldiDesktop;
+    };
 
     defaultApplications =
       {
-        "x-scheme-handler/mailto" = "vivaldi-stable.desktop";
+        "x-scheme-handler/mailto" = vivaldiDesktop;
         "x-scheme-handler/slack" = "slack.desktop";
-        "inode/directory" = "org.kde.dolphin.desktop";
+        "inode/directory" = dolphinDesktop;
       }
       // (
         if host.browser == "brave"
-        then let
-          braveDesktop = "brave.desktop";
-        in {
+        then {
           "x-scheme-handler/unknown" = braveDesktop;
           "x-scheme-handler/https" = braveDesktop;
           "x-scheme-handler/about" = braveDesktop;
