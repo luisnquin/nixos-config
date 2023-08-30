7f8a2e75fd98310925fdc25e71c3322295f222e0
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-08-30 00:20:01 -0600

    feat(dunst): added customizable notification daemon

--- home/modules/desktop.nix
+++ home/modules/desktop.nix
@@ -9,6 +9,35 @@
   # https://github.com/i3-wsman/i3-wsman
   programs.rofi.enable = true;
 
+  services.dunst = {
+    enable = true;
+    package = pkgs.dunst;
+
+    iconTheme = with pkgs; {
+      name = "Adwaita";
+      package = gnome.adwaita-icon-theme;
+      size = "16x16";
+    };
+
+    settings = {
+      global = {
+        width = 300;
+        height = 300;
+        offset = "30x50";
+        origin = "top-right";
+        transparency = 10;
+        frame_color = "#eceff1";
+        font = "Cascadia Code 9";
+      };
+
+      urgency_normal = {
+        background = "#37474f";
+        foreground = "#eceff1";
+        timeout = 10;
+      };
+    };
+  };
+
   xsession.windowManager.i3 = {
     enable = true;
     # package = pkgs.i3;
