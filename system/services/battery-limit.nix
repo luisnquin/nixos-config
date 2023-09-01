943eb8e0b333f905446b03c2327e8ca384fca22d
luisnquin <luisnquin@users.noreply.github.com>
2023-09-01 15:51:08 +0000

    chore(deadnix): removed dead code

--- system/services/battery-limit.nix
+++ system/services/battery-limit.nix
@@ -1,15 +1,11 @@
 {
   pkgs,
-  host,
   ...
 }: {
   systemd.services.battery-charge-threshold = {
     enable = true;
     description = "Set the battery charge threshold";
-    serviceConfig = let
-      # threshold = pkgs.lib.strings.toInt host.batteryThreshold;
-      # offset = 1;
-    in {
+    serviceConfig = {
       Type = "oneshot";
       # ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${host.batteryThreshold + offset} > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
       ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
