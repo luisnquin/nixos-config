ddd6349a943504d7e6a365c7125125dc1e94c3cf
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-09-01 09:49:57 -0600

    fix(battery-limit): temporary hardcoded due to 'cannot coerce an integer to a string'

--- system/services/battery-limit.nix
+++ system/services/battery-limit.nix
@@ -7,10 +7,12 @@
     enable = true;
     description = "Set the battery charge threshold";
     serviceConfig = let
-      offset = 1;
+      # threshold = pkgs.lib.strings.toInt host.batteryThreshold;
+      # offset = 1;
     in {
       Type = "oneshot";
-      ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${host.batteryThreshold + offset} > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+      # ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${host.batteryThreshold + offset} > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+      ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
     };
     wantedBy = ["multi-user.target"];
   };
