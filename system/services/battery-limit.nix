c74eee9f664c50dddaa06ce550e973d131f05bad
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-09-02 07:12:36 -0600

    feat(battery-limit): configurable threshold working

--- system/services/battery-limit.nix
+++ system/services/battery-limit.nix
@@ -1,11 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  host,
+  ...
+}: {
   systemd.services.battery-charge-threshold = {
     enable = true;
     description = "Set the battery charge threshold";
-    serviceConfig = {
+    serviceConfig = let
+      offset = 1;
+
+      batteryThreshold = toString (host.batteryThreshold + offset);
+      batteryChargeThresholdPath = "/sys/class/power_supply/BAT1/charge_control_end_threshold";
+    in {
       Type = "oneshot";
-      # ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${host.batteryThreshold + offset} > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
-      ExecStart = ''${pkgs.bash}/bin/bash -c "echo 61 > /sys/class/power_supply/BAT1/charge_control_end_threshold"'';
+      ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${batteryThreshold} > ${batteryChargeThresholdPath}"'';
     };
     wantedBy = ["multi-user.target"];
   };
