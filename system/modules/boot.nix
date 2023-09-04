516b3f756f8d6c7086c813e95976779a402bffe1
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-09-03 22:07:40 -0600

    feat(grub): black screen as grub splash image

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -10,18 +10,21 @@
       timeout = 15;
 
       grub = {
-        gfxmodeBios = host.resolution;
-        gfxmodeEfi = host.resolution;
-        theme = fallout-grub-theme;
-
         enable = true;
         device = "nodev";
-        useOSProber = true;
-        efiSupport = true;
+
         forceInstall = false;
+        useOSProber = true;
 
         # For a better future: https://github.com/NixOS/nixpkgs/issues/23926
         configurationLimit = 42;
+        efiSupport = true;
+
+        gfxmodeBios = host.resolution;
+        gfxmodeEfi = host.resolution;
+
+        splashImage = ./../dots/boot/grub/splash-image.png;
+        theme = fallout-grub-theme;
       };
     };
 
