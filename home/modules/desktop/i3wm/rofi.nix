0d54fc405baaefe0c51ef27421e557f504e64f2f
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-09-06 22:22:56 -0600

    feat(rofi): directly struggling with config files, nixos options for this limit me

--- home/modules/desktop/i3wm/rofi.nix
+++ home/modules/desktop/i3wm/rofi.nix
@@ -1,13 +1,10 @@
 {pkgs, ...}: {
   programs.rofi = {
     enable = true;
-
     plugins = with pkgs; [
       rofi-calc
     ];
-
-    theme = "Arc-Dark";
-    location = "center";
-    terminal = "${pkgs.alacritty}/bin/alacritty";
   };
+
+  xdg.configFile."rofi/config.rasi".text = builtins.readFile ./../../../dots/rofi/config.rasi;
 }
