1e1c8ad88dc6071b7c98f263885f74f7d8d37eef
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-09 00:35:58 -0500

    feat(tmux): improved packaging

--- pkgs/tpm.nix
+++ pkgs/tpm.nix
@@ -14,16 +14,30 @@ stdenvNoCC.mkDerivation rec {
   };
 
   preConfigure = ''
-    substituteInPlace ./tpm \
-      --replace 'CURRENT_DIR=' 'CURRENT_DIR="${placeholder "out"}/share" #'
+    substituteInPlace {./tpm,./bindings/*,./scripts/*.sh} \
+      --replace 'CURRENT_DIR=' 'CURRENT_DIR="${placeholder "out"}/share" # '
+
+    substituteInPlace {./bin/*,./bindings/*} \
+     --replace 'SCRIPTS_DIR=' 'SCRIPTS_DIR="${placeholder "out"}/share/scripts" # '
+
+    substituteInPlace {./scripts/helpers/tmux_utils.sh,./scripts/*.sh} \
+     --replace 'HELPERS_DIR=' 'HELPERS_DIR="${placeholder "out"}/share/scripts/helpers" # '
   '';
 
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/{share,bin}
-    cp -r ./{bin,bindings,scripts} $out/share
-    cp -r ./tpm $out/bin
+
+    cp -r ./{bindings,scripts} $out/share
+
+    for p in ./bin/*_plugins ; do
+      f=$(basename $p)
+      mv "./bin/$f" "./bin/tpm_$f";
+    done
+
+    cp {./bin/*,./tpm} $out/bin
+
     chmod +x $out/bin/*
 
     runHook postInstall
