c45971734955f14b89cfc263001e5ed7a351f319
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-12 22:55:10 -0500

    feat(wayland): systemd unit for xwaylandvideobridge in home manager

--- home/services/default.nix
+++ home/services/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     ./keep-track-of-tpm-plugins.nix
+    ./xwaylandvideobridge.nix
     ./battery-notifier.nix
     ./bluetooth-mpris.nix
   ];
---
+++ home/services/xwaylandvideobridge.nix
@@ -0,0 +1,26 @@
+{
+  pkgs,
+  host,
+  lib,
+  ...
+}: let
+  isWayland = builtins.elem host.desktop ["hyprland"]; # TODO: improve
+in {
+  systemd.user.services = lib.mkIf isWayland {
+    xwaylandvideobridge = {
+      Unit = {
+        Description = "Tool to make it easy to stream wayland windows and screens to existing applications running under Xwayland";
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.xwaylandvideobridge}/bin/xwaylandvideobridge";
+        Restart = "on-failure";
+      };
+
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
+  };
+}
