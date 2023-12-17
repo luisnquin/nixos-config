64f36b570933fc5c5fe1de724a5cc250108aff5b
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-17 17:51:00 -0500

    feat(spotify): ziro theme

--- home/modules/spotify.nix
+++ home/modules/spotify.nix
@@ -10,10 +10,10 @@ in {
 
   programs.spicetify = with spicePkgs; {
     enable = true;
-    theme = themes."Sleek";
-    colorScheme = "cherry";
-    # theme = themes.Ziro;
-    # colorScheme = "ylx-ui";
+    # theme = themes."Comfy";
+
+    theme = themes.Ziro;
+    colorScheme = "ylx-ui";
 
     enabledExtensions = with extensions; [
       historyShortcut
