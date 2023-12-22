e500def4db44e701a286ad880271f7df29abb6a1
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-22 14:02:32 -0500

    feat(rust): default rustup toolchain setup

--- home/modules/dev/rust.nix
+++ home/modules/dev/rust.nix
@@ -1,7 +1,17 @@
-{pkgs, ...}: {
-  home.file.".cargo/config.toml".source = (pkgs.formats.toml {}).generate "cargo-config" {
-    net = {
-      git-fetch-with-cli = true;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  home = {
+    file.".cargo/config.toml".source = (pkgs.formats.toml {}).generate "cargo-config" {
+      net = {
+        git-fetch-with-cli = true;
+      };
     };
+
+    activation.rustupDefault = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      ${pkgs.rustup}/bin/rustup default stable
+    '';
   };
 }
