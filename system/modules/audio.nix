95c5dae34a4e46bf609f619dde75f6940bfb80a1
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-23 18:34:44 -0500

    feat(audio): pulseaudio package

--- system/modules/audio.nix
+++ system/modules/audio.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   # pulseaudio doesn't give a good support for some programs
   services.pipewire = {
     enable = true;
@@ -7,6 +7,10 @@
     pulse.enable = true;
     jack.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    pulseaudio
+  ];
 
   programs.noisetorch.enable = true;
   sound.enable = true;
