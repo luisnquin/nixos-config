c38af607bed68aba8fdd50863d886897168b1256
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-27 20:01:54 -0500

    feat(rofi-radio): some settings written in nix:D

--- home/modules/desktop/rofi.nix
+++ home/modules/desktop/rofi.nix
@@ -3,11 +3,33 @@
   pkgs,
   ...
 }: {
-  programs.rofi = {
-    enable = true;
-    plugins = with pkgs; [
-      rofi-calc
-    ];
+  programs = {
+    rofi = {
+      enable = true;
+      plugins = with pkgs; [
+        rofi-calc
+      ];
+    };
+
+    rofi-radio = {
+      enable = true;
+      broadcasters = [
+        {
+          name = "More than ever";
+          url = "https://youtube.com/playlist?list=PL_qr1c1sjwPmIC5OLHmIeq1D6lB_xUPny";
+          shuffle = true;
+        }
+        {
+          name = "Night City Radio";
+          url = "https://youtube.com/playlist?list=PLUh-lR3snT_rcET-QS9tUJaCzEcjuXyyD";
+          shuffle = true;
+        }
+        {
+          name = "Box Lofi";
+          url = "http://stream.zeno.fm/f3wvbbqmdg8uv";
+        }
+      ];
+    };
   };
 
   home.packages = [
