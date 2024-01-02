d5af01ad589c7789ac885ca4e57f54a414f67559
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-02 14:57:36 -0500

    feat(ocaml): ensuring opam stuff with systemd

--- home/modules/dev/ocaml.nix
+++ home/modules/dev/ocaml.nix
@@ -1,39 +1,15 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }: {
-  home = {
-    packages = with pkgs; [
-      ocamlPackages.utop
-      dune-release
-      ocamlformat
-      ocaml
-    ];
-
-    activation.opamInit = let
-      opam = pkgs.opam.overrideAttrs (
-        p: {
-          postInstall = let
-            wrapperLine = ''
-              wrapProgram ${placeholder "out"}/bin/opam \
-                --prefix PATH : ${lib.makeBinPath (with pkgs; [gnupatch gnutar gnumake gzip gcc])}
-            '';
-          in
-            p.postInstall
-            + wrapperLine;
-        }
-      );
-
-      packagesToInstall = lib.strings.concatMapStrings (p: p + " ") [
-        "ocaml-lsp-server"
-      ];
-    in
-      lib.hm.dag.entryAfter ["writeBoundary"] ''
-        ${opam}/bin/opam init --no-setup --reinit
-        ${opam}/bin/opam install ${packagesToInstall}
-      '';
-  };
+  home.packages = with pkgs; [
+    ocamlPackages.utop
+    dune-release
+    ocamlformat
+    ocaml
+  ];
 
   programs = {
     opam = {
@@ -45,5 +21,43 @@
     zsh.initExtra = ''
       [[ ! -r /home/"$USER"/.opam/opam-init/init.zsh ]] || source /home/"$USER"/.opam/opam-init/init.zsh >/dev/null 2>/dev/null
     '';
+  };
+
+  systemd.user.services = lib.mkIf config.programs.opam.enable {
+    opam-update = {
+      Unit.Description = "Automatically update and keep installed opam packages";
+
+      Service = let
+        opam = pkgs.opam.overrideAttrs (
+          p: {
+            postInstall = let
+              wrapperLine = ''
+                wrapProgram ${placeholder "out"}/bin/opam \
+                  --prefix PATH : ${lib.makeBinPath (with pkgs; [gnupatch gnutar gnumake gzip gcc])}
+              '';
+            in
+              p.postInstall
+              + wrapperLine;
+          }
+        );
+
+        packagesToInstall = lib.strings.concatMapStrings (p: p + " ") [
+          "ocaml-lsp-server"
+        ];
+
+        opam-init = pkgs.writeShellScriptBin "opam-init" ''
+          ${opam}/bin/opam init --no-setup --reinit
+          ${opam}/bin/opam install ${packagesToInstall}
+        '';
+      in {
+        Type = "oneshot";
+        ExecStart = "${opam-init}/bin/opam-init";
+        Restart = "on-failure";
+      };
+
+      Install = {
+        WantedBy = ["default.target"];
+      };
+    };
   };
 }
