bc25a1bceb223474c40b757c03ded6eb4e6dc2f0
Lucy Diniz <lucydiniz.rs@gmail.com>
2024-01-05 05:40:39 +0000

    chore(deadnix): removed dead code

--- lib/default.nix
+++ lib/default.nix
@@ -2,7 +2,7 @@
   pkgs,
   lib,
 }:
-builtins.mapAttrs (name: path: import path {inherit pkgs lib;}) {
+builtins.mapAttrs (_name: path: import path {inherit pkgs lib;}) {
   base64 = ./base64.nix;
   fs = ./fs.nix;
 }
