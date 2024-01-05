509cd04bad06485510e10f9e31f94ab4c53b3ca4
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-05 11:58:12 -0500

    feat(rust): using systemd service to set default toolchain

--- home/modules/dev/rust.nix
+++ home/modules/dev/rust.nix
@@ -3,15 +3,25 @@
   lib,
   ...
 }: {
-  home = {
-    file.".cargo/config.toml".source = (pkgs.formats.toml {}).generate "cargo-config" {
-      net = {
-        git-fetch-with-cli = true;
-      };
+  home.file.".cargo/config.toml".source = (pkgs.formats.toml {}).generate "cargo-config" {
+    net = {
+      git-fetch-with-cli = true;
+    };
+  };
+
+  systemd.user.services.rustup-default = {
+    Unit = {
+      Description = "Set the default Rust toolchain";
     };
 
-    activation.rustupDefault = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      ${pkgs.rustup}/bin/rustup default stable
-    '';
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.rustup}/bin/rustup default stable";
+      Restart = "on-failure";
+    };
+
+    Install = {
+      WantedBy = ["default.target"];
+    };
   };
 }
