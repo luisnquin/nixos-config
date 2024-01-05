585df5614de46519a64e984e4cbdc0b6868a245c
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-05 01:24:36 -0500

    feat(browser): chromium extensions

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -4,8 +4,22 @@
   ...
 }: {
   home.packages = [
-    pkgs."${host.browser}"
     pkgs.mullvad-browser
     pkgs.vivaldi # Always available
   ];
+
+  programs.chromium = {
+    enable = true;
+    package = pkgs."${host.browser}";
+    extensions = let
+      ids = [
+        "iaiomicjabeggjcfkbimgmglanimpnae" # Tab manager
+        "idgpnmonknjnojddfkpgkljpfnnfcklj" # ModHeader - Modify HTTP headers
+        "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
+        "fdpohaocaechififmbbbbbknoalclacl" # GoFullPage - Full Page Screen Capture
+        "gppongmhjkpfnbhagpmjfkannfbllamg" # Wappalyzer - Technology profiler
+      ];
+    in
+      builtins.map (id: {inherit id;}) ids;
+  };
 }
