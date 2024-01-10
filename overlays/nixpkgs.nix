9649e04acdab1238fffaa95aa61fdb8ccdfc879c
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-10 13:45:13 -0500

    refactor(overlays): improved indentation + using self.fetchurl in brave overlay

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -1,42 +1,48 @@
 [
-  (self: super: {
-    ranger =
-      super.ranger.overrideAttrs
-      (r: {
-        preConfigure =
-          r.preConfigure
-          + ''
-            substituteInPlace ranger/ext/img_display.py \
-              --replace "Popen(['ueberzug'" \
-                        "Popen(['${self.ueberzugpp}/bin/ueberzugpp'"
+  (
+    self: super: {
+      ranger =
+        super.ranger.overrideAttrs
+        (r: {
+          preConfigure =
+            r.preConfigure
+            + ''
+              substituteInPlace ranger/ext/img_display.py \
+                --replace "Popen(['ueberzug'" \
+                          "Popen(['${self.ueberzugpp}/bin/ueberzugpp'"
 
-            substituteInPlace ranger/config/rc.conf \
-              --replace 'set preview_images_method w3m' \
-                        'set preview_images_method ueberzug'
-          '';
-      });
-  })
-  (self: super: {
-    xwaylandvideobridge =
-      super.xwaylandvideobridge.overrideAttrs
-      (_old: {
-        version = "unstable";
-        src = self.fetchFromGitHub {
-          owner = "KDE";
-          repo = "xwaylandvideobridge";
-          rev = "6589a2c5f9eb952fb501635c0d9870ec1abebb10";
-          hash = "sha256-cOxmLE5LJItUaqQCJaDBzNjyRGmyFO0NFTV/CpzmYco=";
+              substituteInPlace ranger/config/rc.conf \
+                --replace 'set preview_images_method w3m' \
+                          'set preview_images_method ueberzug'
+            '';
+        });
+    }
+  )
+  (
+    self: super: {
+      xwaylandvideobridge =
+        super.xwaylandvideobridge.overrideAttrs
+        (_old: {
+          version = "unstable";
+          src = self.fetchFromGitHub {
+            owner = "KDE";
+            repo = "xwaylandvideobridge";
+            rev = "6589a2c5f9eb952fb501635c0d9870ec1abebb10";
+            hash = "sha256-cOxmLE5LJItUaqQCJaDBzNjyRGmyFO0NFTV/CpzmYco=";
+          };
+        });
+    }
+  )
+  (
+    self: super: {
+      brave = super.brave.overrideAttrs (_old: rec {
+        version = "1.61.114";
+
+        src = self.fetchurl {
+          url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
+          hash = "sha256-AVL08Npg1nuvFJrd3rC2rCZeoLnPuQsgpvf2R623c6Y=";
         };
       });
-  })
-  (_self: super: {
-    brave = super.brave.overrideAttrs (_old: rec {
-      version = "1.61.114";
-
-      src = super.fetchurl {
-        url = "https://github.com/brave/brave-browser/releases/download/v${version}/brave-browser_${version}_amd64.deb";
-        hash = "sha256-AVL08Npg1nuvFJrd3rC2rCZeoLnPuQsgpvf2R623c6Y=";
-      };
-    });
-  })
+    }
+  )
 ]
