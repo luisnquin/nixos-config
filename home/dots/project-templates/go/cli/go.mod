bb9597bd729b7e261905b178c5f822046d5f3aff
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-11 08:20:13 -0500

    feat: project-templates + vendored

--- home/dots/project-templates/go/cli/go.mod
+++ home/dots/project-templates/go/cli/go.mod
@@ -1,3 +1,5 @@
 module go.dev
 
-go 1.20
+go 1.21
+
+toolchain go1.21.3
---
+++ home/dots/project-templates/python/main.py
@@ -0,0 +1,11 @@
+"""---"""
+
+
+def main() -> None:
+    """---"""
+
+    print('Hi ^^')
+
+
+if __name__ == '__main__':
+    main()
