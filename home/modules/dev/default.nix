a9791288c035a84ec2739f9dfa7c292a75143afa
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-11 14:58:43 -0500

    fix(dev): updated path to templates module

--- home/modules/dev/default.nix
+++ home/modules/dev/default.nix
@@ -3,13 +3,13 @@
     ./git
     ./k8s
 
+    ./templates.nix
     ./editors.nix
     ./python.nix
     ./audit.nix
     ./ocaml.nix
     ./rust.nix
     ./node.nix
-    ./tplr.nix
     ./etc.nix
     ./go.nix
   ];
