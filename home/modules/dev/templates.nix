6a35ecaf2ed349f4ae921be5d38068e62bf9bf13
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-11 22:24:37 -0500

    feat(dev-template): added go rest template

--- home/modules/dev/templates.nix
+++ home/modules/dev/templates.nix
@@ -1,12 +1,16 @@
-{
+{pkgs, ...}: let
+  inherit (pkgs) fetchFromGitHub;
+in {
   tools.tplr = {
     enable = true;
     templates = {
       "go" = {
-        # "rest" = builtins.path {
-        #   name = "go-rest-template";
-        #   path = ../../dots/project-templates/go/rest;
-        # };
+        "rest" = fetchFromGitHub {
+          owner = "luisnquin";
+          repo = "go-rest-template";
+          rev = "v0.1.0";
+          sha256 = "0r09c22zpsiiy0rxdpkgg4ciq87x2hzq1ixqsjcfiqmmd6cx2c2n";
+        };
         "cli" = builtins.path {
           name = "go-cli-template";
           path = ../../dots/project-templates/go/cli;
