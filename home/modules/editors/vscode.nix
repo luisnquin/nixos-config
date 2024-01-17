5c51b07c20c40a432a0e6a7c109c8d83f792767b
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-17 17:30:42 -0500

    feat(vscode): enabled + some extensions

--- home/modules/editors/vscode.nix
+++ home/modules/editors/vscode.nix
@@ -1,13 +1,53 @@
 {pkgs, ...}: {
   programs.vscode = {
-    enable = false;
+    enable = true;
     enableExtensionUpdateCheck = true;
+    mutableExtensionsDir = true;
     enableUpdateCheck = true;
     package = pkgs.vscode;
-    # extensions = with vscode-extensions; [
-    #   matklad.rust-analyzer
-    #   jnoortheen.nix-ide
-    # ];
-    mutableExtensionsDir = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      # Go
+      golang.go
+
+      # Rust
+      rust-lang.rust-analyzer
+
+      # Nix
+      arrterian.nix-env-selector
+      brettm12345.nixfmt-vscode
+      kamadorueda.alejandra
+      jnoortheen.nix-ide
+      bbenoist.nix
+
+      # OCaml
+      ocamllabs.ocaml-platform
+
+      # Python
+      ms-python.python
+      ms-python.vscode-pylance
+      # ms-python.pylint
+      # zeshuaro.vscode-python-poetry
+
+      # JS ecosystem
+      # rvest.vs-code-prettier-eslint
+      # YoavBls.pretty-ts-errors
+
+      # Git
+      eamodio.gitlens
+
+      # Formats
+      redhat.vscode-yaml
+      # dlasagno.rasi
+
+      # Theme
+      zhuangtongfa.material-theme
+
+      # Misc
+      christian-kohler.path-intellisense
+      mechatroner.rainbow-csv
+      usernamehw.errorlens
+      ritwickdey.liveserver
+    ];
   };
 }
