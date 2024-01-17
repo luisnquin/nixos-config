10493d19249ae73b785aaf3e671b6815eb5a4c5a
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-17 17:14:35 -0500

    fix(js-tools): renamed ncu -> npm-check-updates

--- home/modules/dev/js-tools.nix
+++ home/modules/dev/js-tools.nix
@@ -6,7 +6,7 @@
   home.packages = with pkgs; [
     nodePackages.pnpm
     pkgsx.npkill # remove node_modules from child directories
-    ncu #  Find newer versions of package dependencies and check outdated npm packages locally or globally.
+    npm-check-updates # (ncu) Find newer versions of package dependencies and check outdated npm packages locally or globally.
     nodejs_21
     bun
   ];
