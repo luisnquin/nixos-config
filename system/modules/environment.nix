4815a631f53dd45ffd34a6faa490f154b94f51a5
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-18 15:41:16 -0500

    chore: commented maigret package since it's broken in build time

--- system/modules/environment.nix
+++ system/modules/environment.nix
@@ -56,7 +56,7 @@
 
         osint = [
           exiftool
-          maigret
+          # maigret # Broken -> ValueError: Hash algorithm not known for ed448 - use .cms_hash_algorithm for CMS purposes. More info at https://github.com/wbond/asn1crypto/pull/230.
           whois
         ];
       });
