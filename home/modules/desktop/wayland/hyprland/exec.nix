e7a46d973342ded485f5e2f7449636878327d72c
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-18 16:39:48 -0500

    refactor(hyprland): exec rules in a separeted submodule

---
+++ home/modules/desktop/wayland/hyprland/exec.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  always = [
+    "pkill waybar & sleep 0.1 && waybar"
+  ];
+
+  once = [
+    "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store -max-items 200"
+    "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+  ];
+}
