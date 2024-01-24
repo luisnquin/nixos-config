12c25c4f90916f25bf4d6bcf7e21a50b07e97d1d
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-24 15:23:59 -0500

    feat(mako): enabled + settings

---
+++ home/modules/desktop/wayland/components/mako.nix
@@ -0,0 +1,41 @@
+{
+  services.mako = {
+    enable = true;
+
+    font = "Cascadia Code 9";
+    backgroundColor = "#1b1924de";
+    textColor = "#cbcacf";
+
+    layer = "overlay";
+    anchor = "top-right";
+    width = 320;
+    height = 130;
+    padding = "20";
+    margin = "5";
+
+    borderColor = "#1b1924de";
+    borderRadius = 3;
+    borderSize = 2;
+
+    maxIconSize = 25;
+    icons = true;
+    sort = "-time";
+    defaultTimeout = 3000;
+
+    extraConfig = ''
+      [urgency=low]
+      border-color=#444152
+
+      [urgency=normal]
+      border-color=#1b1924de
+
+      [urgency=high]
+      border-color=#ff5555
+      default-timeout=0
+
+      [category=mpd]
+      default-timeout=2000
+      group-by=category
+    '';
+  };
+}
