685962613d8eaf104c06d4257b7065f3d81a9979
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-24 14:59:36 -0500

    feat(dunst): settings v2

--- home/modules/desktop/dunst.nix
+++ home/modules/desktop/dunst.nix
@@ -9,40 +9,79 @@
       size = "16x16";
     };
 
-    settings = {
-      global = {
-        font = "Cascadia Code 9";
-        frame_color = "#8d31de";
-        width = 320;
-        height = 300;
-        padding = 13;
-        offset = "30x40";
-        origin = "top-right";
-        transparency = 10; # X11 only
-        corner_radius = 3;
-        notification_limit = 20;
-        scale = 2;
+    settings = let
+      _v1 = {
+        global = {
+          font = "Cascadia Code 9";
+          frame_color = "#8d31de";
+          width = 320;
+          height = 300;
+          padding = 13;
+          offset = "30x40";
+          origin = "top-right";
+          transparency = 10; # X11 only
+          corner_radius = 3;
+          notification_limit = 20;
+          scale = 2;
+        };
+
+        urgency_low = {
+          background = "#262121";
+          foreground = "#888888";
+          timeout = 10;
+        };
+
+        urgency_normal = {
+          background = "#371f4d";
+          foreground = "#eceff1";
+          timeout = 10;
+        };
+
+        urgency_critical = {
+          background = "#900000";
+          foreground = "#ffffff";
+          frame_color = "#ff0000";
+          timeout = 20;
+          override_pause_level = 20;
+        };
       };
 
-      urgency_low = {
-        background = "#262121";
-        foreground = "#888888";
-        timeout = 10;
-      };
+      v2 = {
+        global = {
+          font = "Cascadia Code 9";
+          frame_color = "#1e1b29";
+          width = 330;
+          height = 300;
+          padding = 20;
+          offset = "40x50";
+          origin = "top-right";
+          transparency = 10; # X11 only
+          corner_radius = 3;
+          notification_limit = 20;
+          scale = 3;
+        };
 
-      urgency_normal = {
-        background = "#371f4d";
-        foreground = "#eceff1";
-        timeout = 10;
-      };
+        urgency_low = {
+          background = "#1b1925";
+          foreground = "#cbcacf";
+          timeout = 10;
+        };
 
-      urgency_critical = {
-        background = "#900000";
-        foreground = "#ffffff";
-        frame_color = "#ff0000";
-        timeout = 20;
-        override_pause_level = 20;
+        urgency_normal = {
+          background = "#1b1924de";
+          foreground = "#cbcacf";
+          timeout = 10;
+        };
+
+        urgency_critical = {
+          background = "#900000";
+          foreground = "#ffffff";
+          frame_color = "#ff0000";
+          timeout = 20;
+          override_pause_level = 20;
+        };
       };
-    };
+    in
+      v2;
   };
 }
