efe8d1bbf04ffdbc95f0893efaad9924445fb9b7
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-24 16:45:34 -0500

    feat(color-picker): .desktop file via overrides with the generic name of "Color Picker"

--- home/modules/color-picker.nix
+++ home/modules/color-picker.nix
@@ -4,16 +4,44 @@
   lib,
   ...
 }: let
-  package =
-    if isWayland
-    then pkgs.wl-color-picker
-    else pkgs.xcolor;
+  package = let
+    color-picker =
+      if isWayland
+      then {
+        package = pkgs.wl-color-picker;
+        extraArgs = "";
+      }
+      else {
+        package = pkgs.xcolor;
+        extraArgs = "-s";
+      };
+  in
+    color-picker.package.overrideAttrs (
+      old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.copyDesktopItems];
+
+        desktopItems = [
+          (pkgs.makeDesktopItem rec {
+            name = "Color Picker";
+            exec = "${lib.getExe color-picker.package} ${color-picker.extraArgs}";
+            icon = name;
+            desktopName = name;
+            genericName = let
+              pname =
+                if builtins.isString old.pname
+                then old.pname
+                else old.name;
+            in "${pname} - ${old.meta.description}";
+          })
+        ];
+      }
+    );
 in {
   home = {
     packages = [package];
 
     shellAliases = {
-      color-picker = "${lib.getExe package}";
+      "color-picker" = "${lib.getExe package}";
     };
   };
 }
