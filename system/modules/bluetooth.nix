71662c9187855605c38616332f09dd6f40c8c5c8
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-27 09:12:33 -0500

    feat(bluetooth): added bluetuith package

--- system/modules/bluetooth.nix
+++ system/modules/bluetooth.nix
@@ -1,12 +1,17 @@
 {
   isTiling,
   config,
+  pkgs,
   ...
 }: {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
+
+  environment.systemPackages = [
+    pkgs.bluetuith
+  ];
 
   services.blueman.enable = config.hardware.bluetooth.enable && isTiling;
 }
