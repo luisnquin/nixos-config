de2ce74a78cc4b2915603a47c0f1d5573458500e
Lucy Diniz <lucydiniz.rs@gmail.com>
2024-01-27 01:16:12 +0000

    deadnix: removed dead code

--- home/modules/desktop/dunst.nix
+++ home/modules/desktop/dunst.nix
@@ -10,41 +10,6 @@
     };
 
     settings = let
-      _v1 = {
-        global = {
-          font = "Cascadia Code 9";
-          frame_color = "#8d31de";
-          width = 320;
-          height = 300;
-          padding = 13;
-          offset = "30x40";
-          origin = "top-right";
-          transparency = 10; # X11 only
-          corner_radius = 3;
-          notification_limit = 20;
-          scale = 2;
-        };
-
-        urgency_low = {
-          background = "#262121";
-          foreground = "#888888";
-          timeout = 10;
-        };
-
-        urgency_normal = {
-          background = "#371f4d";
-          foreground = "#eceff1";
-          timeout = 10;
-        };
-
-        urgency_critical = {
-          background = "#900000";
-          foreground = "#ffffff";
-          frame_color = "#ff0000";
-          timeout = 20;
-          override_pause_level = 20;
-        };
-      };
 
       v2 = {
         global = {
