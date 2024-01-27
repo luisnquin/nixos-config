68f1f5d789c5a43bf8fbaf0febc648271a0aa0f1
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-26 22:03:39 -0500

    feat: updated battery-notifier settings + flake

--- home/modules/battery.nix
+++ home/modules/battery.nix
@@ -3,9 +3,9 @@
     enable = true;
     settings = {
       interval_ms = 700;
-      reminder_threshold = 30;
-      threat_threshold = 5;
-      warn_threshold = 15;
+      reminder = {threshold = 30;};
+      threat = {threshold = 5;};
+      warn = {threshold = 15;};
     };
   };
 }
