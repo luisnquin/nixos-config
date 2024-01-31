5b217e7899e7c38bd08d8834cc70dd616129d5ae
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-30 23:04:41 -0500

    refactor(battery): using dots to specify toml values

--- home/modules/battery.nix
+++ home/modules/battery.nix
@@ -3,9 +3,9 @@
     enable = true;
     settings = {
       interval_ms = 700;
-      reminder = {threshold = 30;};
-      threat = {threshold = 5;};
-      warn = {threshold = 15;};
+      reminder.threshold = 30;
+      threat.threshold = 5;
+      warn.threshold = 15;
     };
   };
 }
