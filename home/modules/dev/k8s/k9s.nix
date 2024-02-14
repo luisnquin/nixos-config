194c38a5bf46086c8ef21a090b11beb4c2f1e8f4
Luis Quiñones <lpaandres2020@gmail.com>
2024-02-14 09:59:23 -0500

    fix(k8s): `programs.k9s.skin` has been renamed to `programs.k9s.skins.skin` :(

--- home/modules/dev/k8s/k9s.nix
+++ home/modules/dev/k8s/k9s.nix
@@ -1,7 +1,7 @@
 {libx, ...}: {
   programs.k9s = {
     enable = true;
-    skin = libx.formats.fromYAML ../../../dots/k9s/skin.yml;
+    skins.skin = libx.formats.fromYAML ../../../dots/k9s/skin.yml;
     views = libx.formats.fromYAML ../../../dots/k9s/views.yml;
   };
 }
