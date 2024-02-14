ce5dffaaa17dbb0f4bcdd2acd86338b06e764f59
Luis Quiñones <lpaandres2020@gmail.com>
2024-02-14 07:34:19 -0500

    refactor(battery): battery-charge-threshold service in same module

--- system/modules/battery.nix
+++ system/modules/battery.nix
@@ -1,3 +1,23 @@
-{host, ...}: {
+{
+  pkgs,
+  host,
+  lib,
+  ...
+}: {
   services.thermald.enable = host.isLaptop;
+
+  systemd.services.battery-charge-threshold = {
+    enable = host.isLaptop;
+    description = "Set the battery charge threshold";
+    serviceConfig = let
+      offset = 1;
+
+      batteryThreshold = builtins.toString (host.batteryThreshold + offset);
+      batteryChargeThresholdPath = "/sys/class/power_supply/BAT1/charge_control_end_threshold";
+    in {
+      Type = "oneshot";
+      ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${batteryThreshold} > ${batteryChargeThresholdPath}"'';
+    };
+    wantedBy = ["multi-user.target"];
+  };
 }
--- system/services/default.nix
+++ system/services/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./successful-ping-to-google.nix
-    ./battery-threshold.nix
     ./clean-trash-bin.nix
     ./ensure-home-fs.nix
     ./tldr-update.nix
