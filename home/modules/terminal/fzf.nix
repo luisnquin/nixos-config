f0680dc3da538fd2e7afe3527773d629e612a93b
Luis Quiñones <lpaandres2020@gmail.com>
2024-02-20 09:48:40 -0500

    feat(fzf): commented colors and default options settings because they're a little buggy now

--- home/modules/terminal/fzf.nix
+++ home/modules/terminal/fzf.nix
@@ -3,5 +3,35 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    # # https://vitormv.github.io/fzf-themes#eyJib3JkZXJTdHlsZSI6InJvdW5kZWQiLCJib3JkZXJMYWJlbCI6IiIsImJvcmRlckxhYmVsUG9zaXRpb24iOjAsInByZXZpZXdCb3JkZXJTdHlsZSI6InJvdW5kZWQiLCJwYWRkaW5nIjoiMCIsIm1hcmdpbiI6IjAiLCJwcm9tcHQiOiI+ICIsIm1hcmtlciI6Ij4iLCJwb2ludGVyIjoi4peGIiwic2VwYXJhdG9yIjoi4pSAIiwic2Nyb2xsYmFyIjoi4pSCIiwibGF5b3V0IjoiZGVmYXVsdCIsImluZm8iOiJkZWZhdWx0IiwiY29sb3JzIjoiZmc6I2U4ZDRmMyxmZys6I2QwZDBkMCxiZys6IzIyMTMyNixobDojNGViMGQwLGhsKzojNGViOGRjLGluZm86I2QyZDI4NixtYXJrZXI6Izc4MmNjZSxwcm9tcHQ6IzNkODQ4YixzcGlubmVyOiNhZjVmZmYscG9pbnRlcjojYWY1ZmZmLGhlYWRlcjojODdhZmFmLGJvcmRlcjojMjYyNjI2LGxhYmVsOiNhZWFlYWUscXVlcnk6I2Q5ZDlkOSJ9
+    # colors = {
+    #   fg = "#e8d4f3";
+    #   "fg+" = "#d0d0d0";
+    #   bg = "-1";
+    #   "bg+" = "#221326";
+    #   hl = "#4eb0d0";
+    #   "hl+" = "#4eb8dc";
+    #   info = "#d2d286";
+    #   marker = "#782cce";
+    #   prompt = "#3d848b";
+    #   spinner = "#af5fff";
+    #   pointer = "#af5fff";
+    #   header = "#87afaf";
+    #   border = "#262626";
+    #   label = "#aeaeae";
+    #   query = "#d9d9d9";
+    # };
+
+    # defaultOptions = [
+    #   "--preview-window border-rounded"
+    #   "--border rounded"
+    #   "--border-label ''"
+    #   "--separator '─'"
+    #   "--scrollbar '│'"
+    #   "--pointer '◆'"
+    #   "--prompt '> '"
+    #   "--marker '>'rm "
+    # ];
   };
 }
