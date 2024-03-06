53047867e985a820a1ccdd1d510497d71b41d3b1
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-05 20:06:00 -0500

    feat(dev): added '@angular/cli' package

--- home/modules/dev/node/etc.nix
+++ home/modules/dev/node/etc.nix
@@ -6,5 +6,6 @@
   home.packages = with pkgs; [
     npm-check-updates # (ncu) Find newer versions of package dependencies and check outdated npm packages locally or globally.
     pkgsx.npkill # remove node_modules from child directories
+    pkgs.nodePackages."@angular/cli"
   ];
 }
