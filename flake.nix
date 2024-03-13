5104999d55643a82f83b3f68e98ef45e418401c2
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-12 21:54:32 -0500

    refactor(flake): every input in two main blocks

--- flake.nix
+++ flake.nix
@@ -9,20 +9,18 @@
     nixpkgs-latest.url = "nixpkgs/nixos-unstable";
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
+    nixpkgs_mysql_57.url = "github:NixOS/nixpkgs?rev=06c9198cbf48559191bf6c9b76c0f370f96b8c33";
     rofi-network-manager.url = "github:luisnquin/rofi-network-manager";
     battery-notifier.url = "github:luisnquin/battery-notifier";
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
     grub-themes.url = "github:luisnquin/grub-themes";
+    hyprland-contrib.url = "github:hyprwm/contrib";
     hyprstfu.url = "github:luisnquin/hyprstfu";
     scripts.url = "github:luisnquin/scripts";
+    hyprland.url = "github:hyprwm/Hyprland";
     tplr.url = "github:luisnquin/tplr";
     senv.url = "github:luisnquin/senv";
     nao.url = "github:luisnquin/nao";
-
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
-    hyprland-contrib.url = "github:hyprwm/contrib";
-    hyprland.url = "github:hyprwm/Hyprland";
-
-    nixpkgs_mysql_57.url = "github:NixOS/nixpkgs?rev=06c9198cbf48559191bf6c9b76c0f370f96b8c33";
   };
 
   outputs = inputs:
