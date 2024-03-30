be256624bc567c0206ba426f3bf0f186b40d4434
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-30 14:12:06 -0500

    feat(network): nginx config for rustypaste and inner-static + localhost domain

--- system/modules/network/acme.nix
+++ system/modules/network/acme.nix
@@ -11,6 +11,14 @@
     };
 
     certs = {
+      # https://shami.blog/2021/07/howto-letsencrypt-certificates-for-localhost/
+      # localhost
+      "localhost.luisquinones.me" = {
+        dnsProvider = "cloudflare";
+        credentialsFile = config.age.secrets.acme-env-luisquinones-me.path;
+        extraDomainNames = ["*.localhost.luisquinones.me"];
+      };
+
       "luisquinones.me" = {
         dnsProvider = "cloudflare";
         credentialsFile = config.age.secrets.acme-env-luisquinones-me.path;
--- system/modules/network/nginx.nix
+++ system/modules/network/nginx.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   services.nginx = {
@@ -16,11 +17,26 @@
     httpConfig = let
       rustypaste = {
         inherit (config.services.rustypaste.settings.server) address;
+        domain = "rp.localhost";
+      };
+
+      static-web-server = {
+        address = let
+          port = lib.strings.removePrefix "[::]:" config.services.static-web-server.listen;
+        in "0.0.0.0:${port}";
+
         domain = "localhost";
       };
 
+      localhost = let
+        domainsDir = "${config.security.acme.certs."localhost.luisquinones.me".directory}";
+      in {
+        crt = "${domainsDir}/cert.pem";
+        key = "${domainsDir}/key.pem";
+      };
+
       luisquinones-me = let
-        domainsDir = "/var/lib/acme/luisquinones.me";
+        domainsDir = "${config.security.acme.certs."luisquinones.me".directory}";
       in {
         crt = "${domainsDir}/cert.pem";
         key = "${domainsDir}/key.pem";
@@ -35,8 +51,24 @@
 
       server {
           listen 80 default_server;
-          server_name ${rustypaste.domain};
+          server_name ${static-web-server.domain};
           return 301 https://$host$request_uri;
+      }
+
+      server {
+        listen 443 ssl;
+        server_name ${static-web-server.domain};
+
+        ssl_certificate ${localhost.crt};
+        ssl_certificate_key ${localhost.key};
+
+        location / {
+            proxy_pass http://${static-web-server.address};
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+        }
       }
 
       server {
