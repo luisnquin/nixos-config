def2a89abc5a0f13569adba652621e4ba9716e8e
Luis Quiñones <lpaandres2020@gmail.com>
2024-03-30 08:06:22 -0500

    fix(chromium): removed unused extensions

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -13,20 +13,15 @@
     package = pkgs."${host.browser}";
     extensions = let
       ids = [
-        "iaiomicjabeggjcfkbimgmglanimpnae" # Tab manager
         "dmghijelimhndkbmpgbldicpogfkceaj" # "Dark mode"
-        "nigigpmchbpkjjgncmpiggfnikllldlh" # Forever pinned
         "emjjmkkcdllendgbldliiphlbpnomnjk" # target="_blank"-toggler
         "gppongmhjkpfnbhagpmjfkannfbllamg" # Wappalyzer - Technology profiler
-        "nikomkkhhpfoeamojhhgpfkpkdlfhfii" # TabsPlus
-        # "fdpohaocaechififmbbbbbknoalclacl" # GoFullPage - Full Page Screen Capture
 
         "dnhpnfgdlenaccegplpojghhmaamnnfp" # Augmented Steam
         "anlikcnbgdeidpacdbdljnabclhahhmd" # Enhanced GitHub
         "ficfmibkjjnpogdcfhfokmihanoldbfe" # File Icons for GitHub and GitLab
         "obnjfbgikjcdfnbnmdamffacjfpankih" # Excalisave
 
-        "idgpnmonknjnojddfkpgkljpfnnfcklj" # ModHeader - Modify HTTP headers
         "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
         "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
         "pfefekfhnmbfcofpjojnpmhdakcadeil" # AstroSpect
