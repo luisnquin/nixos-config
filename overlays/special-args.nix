fd1f2e1f054709a0c48d0d021fd721317cc09f1c
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-02 21:56:52 -0500

    refactor(overlays): removed unused rec statement

--- overlays/special-args.nix
+++ overlays/special-args.nix
@@ -1,5 +1,5 @@
 {grimblast, ...}: {
-  grimblast = grimblast.overrideAttrs (_oldAttrs: rec {
+  grimblast = grimblast.overrideAttrs (_oldAttrs: {
     prePatch = ''
       substituteInPlace ./grimblast --replace '-t 3000' '-t 3000 -i ${./../home/dots/icons/crop.512.png}'
     '';
