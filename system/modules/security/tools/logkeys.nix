3c0865d8da81ab403f79be0bb2ca05a52f23f0c3
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-03 22:00:01 -0500

    security(logkeys): packages from nixpkgs are root-weak for 'pain-control' group

--- system/modules/security/tools/logkeys.nix
+++ system/modules/security/tools/logkeys.nix
@@ -14,8 +14,11 @@
           options = ["NOPASSWD" "SETENV"];
         }) [
           "/run/current-system/sw/bin/logkeys"
-          "/run/current-system/sw/bin/llk"
           "/run/current-system/sw/bin/llkk"
+          "/run/current-system/sw/bin/llk"
+          "${pkgs.logkeys}/bin/logkeys"
+          "${pkgs.logkeys}/bin/llkk"
+          "${pkgs.logkeys}/bin/llk"
         ];
     }
   ];
