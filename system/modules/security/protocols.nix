353e096acb49adc80b939a540db1bbbcae222062
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-05 22:09:26 -0500

    security(openssh): added identify files from home directory + default ones

--- system/modules/security/protocols.nix
+++ system/modules/security/protocols.nix
@@ -1,4 +1,8 @@
-{libx, ...}: {
+{
+  libx,
+  user,
+  ...
+}: {
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -7,6 +11,26 @@
   services.openssh = {
     enable = true;
     banner = libx.base64.decode "SXQncyB0cnVlLCB5b3UgY2FuIG5ldmVyIGVhdCBhIHBldCB5b3UgbmFtZQ==";
+
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/home/${user.alias}/.ssh/id_ed25519";
+        type = "ed25519";
+      }
+      {
+        path = "/home/${user.alias}/.ssh/id_ed25520";
+        type = "ed25519";
+      }
+    ];
 
     # https://github.com/NixOS/nixpkgs/issues/234683
     settings = {
