cd182caf0b795bd486e22200b980082be692f024
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-06 12:49:08 -0500

    feat: updated battery-notifier input + options

--- home/modules/monitoring.nix
+++ home/modules/monitoring.nix
@@ -1,15 +1,15 @@
 {host, ...}: {
-  programs.battery-notifier = {
-    enable = host.isLaptop;
-    settings = rec {
-      interval_ms = 700;
-      reminder.threshold = host.batteryThreshold / 2;
-      warn.threshold = reminder.threshold / 2;
-      threat.threshold = warn.threshold / 3;
-    };
-  };
-
   services = {
+    battery-notifier = {
+      enable = host.isLaptop;
+      settings = rec {
+        interval_ms = 700;
+        reminder.threshold = host.batteryThreshold / 2;
+        warn.threshold = reminder.threshold / 2;
+        threat.threshold = warn.threshold / 3;
+      };
+    };
+
     playerctld.enable = true;
 
     udiskie = {
