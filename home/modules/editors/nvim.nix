04028d87e009b67cc7ca3486e9019f6671e9415c
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-07 19:44:32 -0500

    feat(nvim): support for more languages

--- home/modules/editors/nvim.nix
+++ home/modules/editors/nvim.nix
@@ -21,16 +21,20 @@
       languages = {
         enableLSP = true;
 
+        bash.enable = true;
+        clang.enable = true;
+        zig.enable = true;
         nix.enable = true;
         rust.enable = true;
+        go.enable = true;
+        ts.enable = true;
         sql = {
           enable = true;
           lsp.enable = false; # (sqls is deprecated, use sqlls instead.)
         };
+        terraform.enable = true;
         markdown.enable = true;
-        ts.enable = true;
         python.enable = true;
-        go.enable = true;
       };
     };
   };
