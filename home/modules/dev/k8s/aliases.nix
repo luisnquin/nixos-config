96c3441926d22bc779f70770df1c669c510f211b
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-18 15:19:18 -0500

    feat(k8s): added 'mrk' alias

--- home/modules/dev/k8s/aliases.nix
+++ home/modules/dev/k8s/aliases.nix
@@ -1,7 +1,8 @@
 {
-  home.shellAliases = {
+  home.shellAliases = rec {
     k9s = "k9s --crumbsless --logoless --refresh=2";
     kw = "k9s";
     k = "kubectl";
+    mrk = k; # sometimes I want to call it like this
   };
 }
