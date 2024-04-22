2acd3b31cc6ba811ff1d5400be2911c76c45b5ad
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-22 15:47:56 -0500

    feat(nginx): manually creating nginx group and user

--- system/modules/security/groups.nix
+++ system/modules/security/groups.nix
@@ -1,10 +1,19 @@
-{user, ...}: {
+{
+  config,
+  user,
+  lib,
+  ...
+}: {
   users.groups = {
     vboxusers.members = [user.alias];
 
     control = {
       name = "pain-control";
       members = [user.alias];
+    };
+
+    nginx = lib.mkForce {
+      gid = config.ids.gids.nginx;
     };
   };
 }
--- system/modules/security/user.nix
+++ system/modules/security/user.nix
@@ -1,27 +1,37 @@
 {
+  config,
   user,
   pkgs,
   libx,
+  lib,
   ...
 }: {
   users = {
     motd = libx.base64.decode "Q29uZnJvbnQgW2hpXXN0b3J5";
     defaultUserShell = pkgs.zsh;
 
-    users.${user.alias} = {
-      description = ''Dorian 🍂'';
+    users = {
+      ${user.alias} = {
+        description = ''Dorian 🍂'';
 
-      home = ''/home/${user.alias}/'';
-      hashedPassword = null;
-      isNormalUser = true;
+        home = ''/home/${user.alias}/'';
+        hashedPassword = null;
+        isNormalUser = true;
 
-      extraGroups = [
-        "networkmanager"
-        "docker"
-        "wheel"
-      ];
+        extraGroups = [
+          "networkmanager"
+          "docker"
+          "wheel"
+        ];
 
-      shell = pkgs.zsh;
+        shell = pkgs.zsh;
+      };
+
+      nginx = lib.mkForce {
+        group = "nginx";
+        isSystemUser = true;
+        uid = config.ids.uids.nginx;
+      };
     };
   };
 }
