a7181510c4b673ecddfb114cbcc1943495976fa6
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-22 16:26:44 -0500

    feat(network): added netport function

--- system/modules/network/shell.sh
+++ system/modules/network/shell.sh
@@ -3,3 +3,7 @@
 netscan() {
     nmcli radio wifi on && nmcli --fields SSID,SECURITY,BARS device wifi list ifname "$(nmcli device | awk '$2 == "wifi" {print $1}')" --rescan yes
 }
+
+netport() {
+    sudo ss -lptn "sport = :$1"
+}
