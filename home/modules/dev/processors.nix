cb07afab9dfffe32a60e85594aeed11b6bc2a2ea
Luis Quiñones <lpaandres2020@gmail.com>
2024-04-24 10:30:04 -0500

    feat(dev): using programs.jq options

--- home/modules/dev/processors.nix
+++ home/modules/dev/processors.nix
@@ -2,6 +2,18 @@
   home.packages = with pkgs; [
     htmlq
     yq-go
-    jq
   ];
+
+  programs.jq = {
+    enable = true;
+    colors = {
+      arrays = "1;37";
+      false = "0;37";
+      null = "1;30";
+      numbers = "0;37";
+      objects = "1;37";
+      strings = "0;32";
+      true = "0;37";
+    };
+  };
 }
