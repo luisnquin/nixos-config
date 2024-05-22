e83e02709cc683a5b7da6673c83625fad4a0c6e4
Luis Quiñones <lpaandres2020@gmail.com>
2024-05-21 20:32:16 -0500

    feat: xplr with wl-clipboard plugin

--- home/modules/file-manager.nix
+++ home/modules/file-manager.nix
@@ -3,4 +3,25 @@
     gnome.nautilus
     ranger
   ];
+
+  programs.xplr = {
+    enable = true;
+    package = pkgs.xplr;
+    plugins = {
+      wl-clipboard = pkgs.fetchFromGitHub {
+        owner = "sayanarijit";
+        repo = "wl-clipboard.xplr";
+        rev = "a3ffc87460c5c7f560bffea689487ae14b36d9c3";
+        hash = "sha256-I4rh5Zks9hiXozBiPDuRdHwW5I7ppzEpQNtirY0Lcks=";
+      };
+    };
+
+    extraConfig = ''
+      require("wl-clipboard").setup {
+        copy_command = "wl-copy -t text/uri-list",
+        paste_command = "wl-paste",
+        keep_selection = true,
+      }
+    '';
+  };
 }
