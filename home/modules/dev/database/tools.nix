f2dba6e65181d5bf4f99e413c1e5c235bf0adf14
Luis Quiñones <lpaandres2020@gmail.com>
2024-06-17 14:40:50 -0500

    feat(zsh): added turso binary directory in session $PATH

--- home/modules/dev/database/tools.nix
+++ home/modules/dev/database/tools.nix
@@ -1,8 +1,19 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     litecli
     pgcli
     mycli
     sqlc
   ];
+
+  programs.zsh.initExtra = let
+    # TODO: try to directly add to the path
+    inherit (config.home) homeDirectory;
+  in ''
+    export PATH="${homeDirectory}/.turso:$PATH"
+  '';
 }
