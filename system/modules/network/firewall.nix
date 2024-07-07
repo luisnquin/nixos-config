0b5e5e63bc8baccd5bd48fded14af220d0425a47
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-07 16:24:31 -0500

    network(firewall): removed old config comments

--- system/modules/network/firewall.nix
+++ system/modules/network/firewall.nix
@@ -1,17 +1,5 @@
 {
   networking = {
-    # nftables = {
-    #   enable = true;
-    #   ruleset = ''
-    #     table ip nat {
-    #       chain PREROUTING {
-    #         type nat hook prerouting priority dstnat; policy accept;
-    #         iifname "wlo1" tcp dport { 80, 443, 357 } dnat to 10.100.0.3
-    #       }
-    #     }
-    #   '';
-    # };
-
     firewall = {
       enable = true;
       allowedTCPPorts = [357 5900];
@@ -22,18 +10,5 @@
         ];
       };
     };
-
-    #   nat = {
-    #     enable = false;
-    #     internalInterfaces = ["ens3"];
-    #     externalInterface = "wlo1";
-    #     forwardPorts =
-    #       builtins.map (port: {
-    #         sourcePort = port;
-    #         proto = "tcp";
-    #         destination = "10.100.0.3:${builtins.toString port}";
-    #       })
-    #       firewall.allowedTCPPorts;
-    #   };
   };
 }
