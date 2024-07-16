9ae51c8648efa5afb5da3006cca11f1f48abacc0
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-16 11:33:16 -0500

    feat(mobile): added ANDROID_HOME environment variable

--- home/modules/dev/mobile.nix
+++ home/modules/dev/mobile.nix
@@ -1,9 +1,19 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    android-studio
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    packages = with pkgs; [
+      android-studio
 
-    kotlin-language-server
-    kotlin-native
-    kotlin
-  ];
+      kotlin-language-server
+      kotlin-native
+      kotlin
+    ];
+
+    sessionVariables = {
+      ANDROID_HOME = "${config.home.homeDirectory}/.android";
+    };
+  };
 }
