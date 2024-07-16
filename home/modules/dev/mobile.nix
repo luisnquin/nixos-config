91e575ff674caa20c3159e2032e4937e5e93387a
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-16 11:51:05 -0500

    feat(mobile): android tools in android home directory + shell path locations

--- home/modules/dev/mobile.nix
+++ home/modules/dev/mobile.nix
@@ -3,17 +3,30 @@
   pkgs,
   ...
 }: {
-  home = {
+  home = let
+    ANDROID_HOME = "${config.home.homeDirectory}/.android";
+  in {
     packages = with pkgs; [
       android-studio
 
       kotlin-language-server
       kotlin-native
       kotlin
+
+      android-tools
+    ];
+
+    file."${ANDROID_HOME}/platform-tools" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${pkgs.android-tools}/bin";
+    };
+
+    sessionPath = [
+      "${ANDROID_HOME}/platform-tools"
+      "${ANDROID_HOME}/emulator"
     ];
 
     sessionVariables = {
-      ANDROID_HOME = "${config.home.homeDirectory}/.android";
+      inherit ANDROID_HOME;
     };
   };
 }
