550097a1c2af7bba13dcb9c76174267673c44b97
Luis Quiñones <lpaandres2020@gmail.com>
2024-08-19 02:41:11 -0600

    chore(overlays): custom image backend for ranger temporary disabled because ueberzugpp has build errors

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -1,23 +1,23 @@
 [
-  (
-    self: super: {
-      ranger =
-        super.ranger.overrideAttrs
-        (r: {
-          preConfigure =
-            r.preConfigure
-            + ''
-              substituteInPlace ranger/ext/img_display.py \
-                --replace "Popen(['ueberzug'" \
-                          "Popen(['${self.ueberzugpp}/bin/ueberzugpp'"
+  # (
+  #   self: super: {
+  #     ranger =
+  #       super.ranger.overrideAttrs
+  #       (r: {
+  #         preConfigure =
+  #           r.preConfigure
+  #           + ''
+  #             substituteInPlace ranger/ext/img_display.py \
+  #               --replace "Popen(['ueberzug'" \
+  #                         "Popen(['${self.ueberzugpp}/bin/ueberzugpp'"
 
-              substituteInPlace ranger/config/rc.conf \
-                --replace 'set preview_images_method w3m' \
-                          'set preview_images_method ueberzug'
-            '';
-        });
-    }
-  )
+  #             substituteInPlace ranger/config/rc.conf \
+  #               --replace 'set preview_images_method w3m' \
+  #                         'set preview_images_method ueberzug'
+  #           '';
+  #       });
+  #   }
+  # )
   (
     self: super: {
       xwaylandvideobridge =
