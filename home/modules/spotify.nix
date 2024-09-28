2d4660e99c18b6a9066b1180f554ff771ca3eec2
Luis Quiñones <lpaandres2020@gmail.com>
2024-09-27 22:21:11 -0600

    chore(spotify): temporarily removed spotifyd service

--- home/modules/spotify.nix
+++ home/modules/spotify.nix
@@ -1,7 +1,5 @@
 {
-  nixosConfig,
   spicetify,
-  host,
   pkgs,
   ...
 }: {
@@ -42,30 +40,30 @@
     spt = lib.getExe pkgs.spotify-player;
   };
 
-  services.spotifyd = {
-    enable = true;
-    settings.global = rec {
-      username = pkgs.libx.base64.decode "eWVzZWxvbnk=";
-      password_cmd = "cat ${nixosConfig.age.secrets.spotify-access-secret.path}";
-      use_keyring = false;
-      use_mpris = true;
-      dbus_type = "session";
-      backend = "pulseaudio";
-      device = "pipewire";
-      control = device;
-      audio_format = "S16";
-      mixer = "PCM";
-      volume_controller = "none";
-      device_name = "${host.name}-systemd";
-      bitrate = 160;
-      # cache_path = "/etc/spotifyd";
-      # max_cache_size = let GB = 1000000000; in GB;
-      no_audio_cache = true;
-      initial_volume = "90";
-      autoplay = true;
-      volume_normalisation = true;
-      normalisation_pregain = -10;
-      device_type = "computer";
-    };
-  };
+  # services.spotifyd = {
+  #   enable = true;
+  #   settings.global = rec {
+  #     username = pkgs.libx.base64.decode "eWVzZWxvbnk=";
+  #     password_cmd = "cat ${nixosConfig.age.secrets.spotify-access-secret.path}";
+  #     use_keyring = false;
+  #     use_mpris = true;
+  #     dbus_type = "session";
+  #     backend = "pulseaudio";
+  #     device = "pipewire";
+  #     control = device;
+  #     audio_format = "S16";
+  #     mixer = "PCM";
+  #     volume_controller = "none";
+  #     device_name = "${host.name}-systemd";
+  #     bitrate = 160;
+  #     # cache_path = "/etc/spotifyd";
+  #     # max_cache_size = let GB = 1000000000; in GB;
+  #     no_audio_cache = true;
+  #     initial_volume = "90";
+  #     autoplay = true;
+  #     volume_normalisation = true;
+  #     normalisation_pregain = -10;
+  #     device_type = "computer";
+  #   };
+  # };
 }
