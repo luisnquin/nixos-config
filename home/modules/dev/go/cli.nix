226778ac12feb7a78e9d94624a157028ffcd8069
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-04 22:55:05 -0600

    feat(go): added go private entries

--- home/modules/dev/go/cli.nix
+++ home/modules/dev/go/cli.nix
@@ -3,6 +3,11 @@
     enable = true;
     package = pkgs.go_1_23;
 
+    goPrivate = [
+      "github.com/chanchitaapp"
+      "github.com/x0xc000022070"
+    ];
+
     goBin = "go/bin";
     goPath = "go";
   };
