a83a44f070bc50c71a9e1b99f517b273048dcbf4
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-16 22:40:02 -0600

    chore(browser): removed option to setup browser based on chromium

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -1,8 +1,6 @@
 {
   inputs,
   system,
-  pkgs,
-  host,
   ...
 }: {
   home.packages = let
@@ -29,38 +27,5 @@
   in {
     associations.added = associations;
     defaultApplications = associations;
-  };
-
-  programs.chromium = {
-    enable = true;
-    package = pkgs."${host.browser}";
-    extensions = let
-      ids = [
-        "dmghijelimhndkbmpgbldicpogfkceaj" # "Dark mode"
-        "emjjmkkcdllendgbldliiphlbpnomnjk" # target="_blank"-toggler
-        "gppongmhjkpfnbhagpmjfkannfbllamg" # Wappalyzer - Technology profiler
-
-        "dnhpnfgdlenaccegplpojghhmaamnnfp" # Augmented Steam
-        "ficfmibkjjnpogdcfhfokmihanoldbfe" # File Icons for GitHub and GitLab
-        "obnjfbgikjcdfnbnmdamffacjfpankih" # Excalisave
-
-        "oldceeleldhonbafppcapldpdifcinji" # TLDR: Grammar checker
-        "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password
-        "fihnjjcciajhdojfnbdddfaoknhalnja" # I don't care about cookies
-        "njdfdhgcmkocbgbhcioffdbicglldapd" # LocalCDN
-        # The precursors of "privacy" are assholes but still, I have personal reasons to use this
-
-        "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
-        "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-        "pfefekfhnmbfcofpjojnpmhdakcadeil" # AstroSpect
-
-        # GitHub
-        "njkkfaliiinmkcckepjdmgbmjljfdeee" # CodeWing
-        "ldleapnlgbkpkabhbkkeangmnfpikahe" # What's new on GitHub
-        "ialbpcipalajnakfondkflpkagbkdoib" # Lovely forks
-        "anlikcnbgdeidpacdbdljnabclhahhmd" # Enhanced GitHub
-      ];
-    in
-      builtins.map (id: {inherit id;}) ids;
   };
 }
