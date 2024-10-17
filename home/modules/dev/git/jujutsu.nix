e25619a3c7ad1c3e605274ea085eb91d4c2bf66d
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-17 08:40:35 -0600

    feat(jujutsu): user email and username 'cause it cannot take the ones that git repo already has :)

--- home/modules/dev/git/jujutsu.nix
+++ home/modules/dev/git/jujutsu.nix
@@ -1,6 +1,19 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.jujutsu = {
     enable = true;
     package = pkgs.jujutsu;
+
+    # Full options:
+    # https://github.com/martinvonz/jj/blob/main/docs/config.md
+    settings = {
+      user = {
+        email = config.programs.git.userEmail;
+        name = config.programs.git.userName;
+      };
+    };
   };
 }
