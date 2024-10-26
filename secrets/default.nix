d450a1704a5bd392303f58d4bbd79469ce629f47
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-26 15:06:56 -0500

    chore(secrets): removed cloudflared files

--- secrets/default.nix
+++ secrets/default.nix
@@ -6,12 +6,6 @@
     spotify-access-secret = {
       file = ./spotify-access-secret.age;
     };
-
-    "40a98b0e-fca3-42a4-8867-c453a33d45e6" = {
-      file = ./40a98b0e-fca3-42a4-8867-c453a33d45e6.age;
-      owner = "cloudflared";
-      group = "cloudflared";
-    };
   };
 
   environment.systemPackages = [agenix.packages.${system}.default];
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,4 @@ let
   luisnquin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgB5sEmG7PDxQ8D240uM2uSJKUPXmY/zMxtfr2jf7Pl lpaandres2020@gmail.com";
 in {
   "spotify-access-secret.age".publicKeys = [luisnquin];
-  "40a98b0e-fca3-42a4-8867-c453a33d45e6.age".publicKeys = [luisnquin];
 }
