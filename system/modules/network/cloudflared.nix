89d670b7f5812231dc718f8bb5edcb7a03c12d0f
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-26 14:21:56 -0500

    chore(network): removed systemd service to start cloudflared

--- system/modules/network/cloudflared.nix
+++ system/modules/network/cloudflared.nix
@@ -1,23 +1,5 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   environment.systemPackages = [
     pkgs.cloudflared
   ];
-
-  services.cloudflared = {
-    enable = true;
-    package = pkgs.cloudflared;
-
-    # This doesn't seems to be reproducible at the current state.
-    # A new tunnel and configurations will need to be issued by cloudflared's CLI.
-    tunnels = {
-      "40a98b0e-fca3-42a4-8867-c453a33d45e6" = {
-        default = "http://localhost:4000";
-        credentialsFile = config.age.secrets."40a98b0e-fca3-42a4-8867-c453a33d45e6".path;
-      };
-    };
-  };
 }
