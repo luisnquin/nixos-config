cb47c6c6c472d1091d3245b61551526006b6d235
Luis Quiñones <lpaandres2020@gmail.com>
2024-12-25 20:55:22 -0500

    feat(boot/grub): added menu entry for windows boot manager

--- system/modules/boot/grub.nix
+++ system/modules/boot/grub.nix
@@ -15,6 +15,21 @@
     configurationLimit = 42;
     efiSupport = true;
 
+
+  # This is a hack to make the grub menu appear on the boot screen.
+  # I won't needed to do this if I shouldn't deleted the /boot directory before. :D
+    extraEntries = let
+      # sudo blkid /dev/nvme0n1p3
+      windowsBootPartitionUuid = "F2646C4C646C159F";
+    in ''
+      menuentry 'Windows Boot Manager' --class windows --class os {
+        insmod part_gpt
+        insmod fat
+        search --no-floppy --fs-uuid --set=root ${windowsBootPartitionUuid}
+        chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+      }
+    '';
+
     gfxmodeBios = host.resolution;
     gfxmodeEfi = host.resolution;
 
