5aca37830eb12a6e51c9d1437664b8accfb52806
Luis Quiñones <lpaandres2020@gmail.com>
2024-12-29 16:31:27 -0500

    feat(systemd): main user does not require auth to run systemctl and journalctl with sudo

--- system/modules/systemd.nix
+++ system/modules/systemd.nix
@@ -1,3 +1,21 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  user,
+  ...
+}: {
   environment.systemPackages = [pkgs.systemctl-tui];
+
+  security.sudo.extraRules = [
+    {
+      users = [user.alias];
+      commands = let
+        commands = ["systemctl" "journalctl"];
+      in
+        builtins.map (cmd: {
+          command = "/run/current-system/sw/bin/${cmd}";
+          options = ["NOPASSWD"];
+        })
+        commands;
+    }
+  ];
 }
