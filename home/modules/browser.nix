b12d8ca5374ff0d9f4d0e7c24ebb15cd35ecd98f
Luis Quiñones <lpaandres2020@gmail.com>
2024-12-30 23:22:15 -0500

    chore(home/browser): removed unnecessary extensions from chromium browser

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -37,24 +37,11 @@ in {
     extensions = let
       ids = [
         "dmghijelimhndkbmpgbldicpogfkceaj" # "Dark mode"
-        "emjjmkkcdllendgbldliiphlbpnomnjk" # target="_blank"-toggler
         "gppongmhjkpfnbhagpmjfkannfbllamg" # Wappalyzer - Technology profiler
 
         "dnhpnfgdlenaccegplpojghhmaamnnfp" # Augmented Steam
         "ficfmibkjjnpogdcfhfokmihanoldbfe" # File Icons for GitHub and GitLab
-        "obnjfbgikjcdfnbnmdamffacjfpankih" # Excalisave
 
-        "oldceeleldhonbafppcapldpdifcinji" # TLDR: Grammar checker
-        "aeblfdkhhhdcdjpifhhbdiojplfjncoa" # 1Password
-        "fihnjjcciajhdojfnbdddfaoknhalnja" # I don't care about cookies
-        "njdfdhgcmkocbgbhcioffdbicglldapd" # LocalCDN
-        # The precursors of "privacy" are assholes but still, I have personal reasons to use this
-
-        "nhdogjmejiglipccpnnnanhbledajbpd" # Vue.js devtools
-        "fmkadmapgofadopljbjfkapdkoienihi" # React Developer Tools
-        "pfefekfhnmbfcofpjojnpmhdakcadeil" # AstroSpect
-
-        # GitHub
         "njkkfaliiinmkcckepjdmgbmjljfdeee" # CodeWing
         "ldleapnlgbkpkabhbkkeangmnfpikahe" # What's new on GitHub
         "ialbpcipalajnakfondkflpkagbkdoib" # Lovely forks
