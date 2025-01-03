4cda414d7b4de9e8cb80738157d7459a76f44123
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-03 10:05:17 -0500

    feat(browser/browser): zen has the right to have any mimeapp associations as it wants

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -16,11 +16,18 @@ in {
         inherit name;
         value = zen-browser.meta.desktopFile;
       }) [
-        "x-scheme-handler/http"
-        "x-scheme-handler/https"
-        "x-scheme-handler/mailto"
+        "application/x-extension-shtml"
+        "application/x-extension-xhtml"
+        "application/x-extension-html"
+        "application/x-extension-xht"
+        "application/x-extension-htm"
         "x-scheme-handler/unknown"
+        "x-scheme-handler/mailto"
+        "x-scheme-handler/chrome"
         "x-scheme-handler/about"
+        "x-scheme-handler/https"
+        "x-scheme-handler/http"
+        "application/xhtml+xml"
         "application/json"
         "text/plain"
         "text/html"
