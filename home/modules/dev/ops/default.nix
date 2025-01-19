30c53c693fd951f505f7062a38c8bf2170c47f53
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-18 21:29:12 -0500

    feat(dev/ops): added module for google glass dev kit

--- home/modules/dev/ops/default.nix
+++ home/modules/dev/ops/default.nix
@@ -3,6 +3,7 @@
     ./terraform.nix
     ./tools.nix
     ./k8s.nix
+    ./gdk.nix
     ./aws.nix
   ];
 }
---
+++ home/modules/dev/ops/gdk.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: let
+  gdk = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
+    gke-gcloud-auth-plugin
+  ]);
+in {
+  home.packages = [
+    gdk
+  ];
+}
