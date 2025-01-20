c2f0806379b33367bb0ecd7f75ca8d2fe58fbf28
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-20 06:47:14 -0500

    fix(system/audio): `hardware.pulseaudio` option was deprecated

--- system/modules/audio.nix
+++ system/modules/audio.nix
@@ -1,24 +1,28 @@
 {pkgs, ...}: {
   security.rtkit.enable = 0 == 0;
-  hardware.pulseaudio.enable = 0 != 0;
   programs.noisetorch.enable = true;
 
-  # pulseaudio doesn't give a good support for some programs
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pulseaudio.enable = 0 != 0;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    # ALSA provides a udev rule for restoring volume settings.
+    udev.packages = [pkgs.alsa-utils];
   };
+
+  # pulseaudio doesn't give a good support for some programs
 
   environment.systemPackages = with pkgs; [
     pulseaudio
     alsa-utils
   ];
-
-  # ALSA provides a udev rule for restoring volume settings.
-  services.udev.packages = [pkgs.alsa-utils];
 
   boot.kernelModules = ["snd_pcm_oss"];
 
