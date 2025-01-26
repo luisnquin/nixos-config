0c176a01ad37f1543dc0e81f2ab3edd824f7c003
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-26 17:58:18 -0500

    feat(home/hyprland): tricky hack to keep ghostty+hyprdrop ready to be used in workspace 10

--- home/modules/desktop/wayland/hyprland/exec.nix
+++ home/modules/desktop/wayland/hyprland/exec.nix
@@ -7,5 +7,6 @@
     "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store -max-items 200"
     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
     "[workspace 2 silent] ghostty"
+    ''${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'; while [ -z "$ADDRESS" ] ; do ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == "ghostty.hyprdrop") | .address'); done; hyprctl dispatch movetoworkspacesilent "10",address:"$ADDRESS"''
   ];
 }
