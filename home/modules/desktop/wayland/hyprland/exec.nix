ba62d1a96a002eb905203cf76823836bfa2477d5
Luis Quiñones <lpaandres2020@gmail.com>
2025-01-28 09:11:02 -0500

    feat(home/hyprland): toggling hyprdrop window in order to move it to special workspace instead of n10

--- home/modules/desktop/wayland/hyprland/exec.nix
+++ home/modules/desktop/wayland/hyprland/exec.nix
@@ -7,6 +7,9 @@
     "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store -max-items 200"
     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
     "[workspace 2 silent] ghostty"
-    ''${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'; while [ -z "$ADDRESS" ] ; do ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == "ghostty.hyprdrop") | .address'); done; hyprctl dispatch movetoworkspacesilent "10",address:"$ADDRESS"''
+    (let
+      cmd = "${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'";
+      # toggle window to hyprdrop's special workspace
+    in ''${cmd}; while [ -z "$ADDRESS" ] ; do ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == "ghostty.hyprdrop") | .address'); done; ${cmd}'')
   ];
 }
