1ba991493ae1b4aeae05edb4e4628debe2e3af06
Luis Quiñones <lpaandres2020@gmail.com>
2025-03-20 21:40:36 -0500

    Revert "feat(network/hosts): add new 127.0.0.1 entry to make mirrors edge catalyst work"
    This reverts commit d694677ddf324ec5668f13a560f39ee6a85fe267.

--- system/modules/network/hosts.nix
+++ system/modules/network/hosts.nix
@@ -6,7 +6,6 @@
       # "facebook.com"
       "www.tiktok.com" # no fucking way I'm going to use this
       "www.pinterest.es"
-      "winter15.gosredirector.ea.com" # https://www.protondb.com/app/1233570#Gtx61bWJ-3
     ];
   };
 }
