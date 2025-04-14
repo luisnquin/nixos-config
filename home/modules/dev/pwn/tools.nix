94b18dab81bbef809953655823065521861580ea
Luis Quiñones <lpaandres2020@gmail.com>
2025-04-13 22:08:52 -0500

    chore(home/pwn): remove havn package
    This package can't build because cargoHash, cargoVendorDir, cargoDeps, or
    cargoLock must be set in the package definition.

--- home/modules/dev/pwn/tools.nix
+++ home/modules/dev/pwn/tools.nix
@@ -2,7 +2,8 @@
   home.packages = with pkgs; [
     osv-scanner
     aircrack-ng
-    pkgs.extra.havn
+    # error: cargoHash, cargoVendorDir, cargoDeps, or cargoLock must be set
+    # pkgs.extra.havn
     semgrep
     nmap
   ];
