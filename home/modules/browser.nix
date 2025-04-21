7c1d82334c1a0ad752a8adccd7ae614cb3503b33
Luis Quiñones <lpaandres2020@gmail.com>
2025-04-20 22:04:38 -0500

    feat(home/browser): a lot of privacy settings for librewolf

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -53,7 +53,19 @@
   programs.librewolf = {
     enable = true;
     settings = {
+      "beacon.enabled" = false;
+      "device.sensors.enabled" = false;
+      "dom.battery.enabled" = false;
+      "dom.event.clipboardevents.enabled" = false;
+      "geo.enabled" = false;
+      "media.peerconnection.enabled" = false;
+      "privacy.clearHistory.cookiesAndStorage" = false;
+      "privacy.clearHistory.siteSettings" = false;
+      "privacy.firstparty.isolate" = true;
       "privacy.resistFingerprinting" = false;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.socialtracking.enabled" = true;
+      "webgl.disabled" = true; # may be annoying
     };
   };
 
