ecd4f539f0b873c4b4ce03ac2d9312e23adcc64b
Luis Quiñones <lpaandres2020@gmail.com>
2025-05-16 21:54:39 -0500

    refactor: install discord without module options

--- home/modules/discord.nix
+++ home/modules/discord.nix
@@ -1,9 +1,19 @@
-{isWayland, ...}: {
-  # A shitty place to stay
-  programs.discord = {
-    enable = true;
-    withOpenASAR = true;
-    withVencord = true;
-    waylandSupport = isWayland;
-  };
+{pkgs, ...}: {
+  home.packages = [
+    (let
+      # TODO: replace by "discord-canary"
+      discord = pkgs.discord.override {
+        withVencord = true;
+        withOpenASAR = true;
+        nss = pkgs.nss_latest;
+      };
+    in
+      discord.overrideAttrs (old: {
+        libPath = old.libPath + ":${pkgs.libglvnd}/lib";
+        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.makeWrapper];
+        postFixup = ''
+          wrapProgram $out/opt/Discord/Discord --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland}}"
+        '';
+      }))
+  ];
 }
--- home/options/default.nix
+++ home/options/default.nix
@@ -3,7 +3,6 @@
     ./lazydocker.nix
     ./emoji-fzf.nix
     ./ntfy-sh.nix
-    ./discord.nix
     ./github.nix
     ./zig.nix
   ];
