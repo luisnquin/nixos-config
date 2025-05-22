e78c799dc5058e4f96f1a6c8ff1317451f58008c
Luis Quiñones <lpaandres2020@gmail.com>
2025-05-21 20:16:42 -0500

    chore(wayland/mako): migrate all to settings option

--- home/modules/desktop/wayland/components/mako.nix
+++ home/modules/desktop/wayland/components/mako.nix
@@ -1,42 +1,45 @@
 {
   services.mako = {
     enable = true;
+    settings = {
+      font = "Cascadia Code 9";
+      backgroundColor = "#1b1924de";
+      textColor = "#cbcacf";
 
-    font = "Cascadia Code 9";
-    backgroundColor = "#1b1924de";
-    textColor = "#cbcacf";
+      layer = "overlay";
+      anchor = "top-right";
+      width = 320;
+      height = 130;
+      padding = "20";
+      margin = "5";
 
-    layer = "overlay";
-    anchor = "top-right";
-    width = 320;
-    height = 130;
-    padding = "20";
-    margin = "5";
+      borderColor = "#1b1924de";
+      borderRadius = 3;
+      borderSize = 2;
 
-    borderColor = "#1b1924de";
-    borderRadius = 3;
-    borderSize = 2;
+      defaultTimeout = 6000;
+      progressColor = "over #2e1545";
+      maxIconSize = 25;
+      icons = true;
+      sort = "-time";
 
-    defaultTimeout = 6000;
-    progressColor = "over #2e1545";
-    maxIconSize = 25;
-    icons = true;
-    sort = "-time";
+      "urgency=low" = {
+        border-color = "#444152";
+      };
 
-    extraConfig = ''
-      [urgency=low]
-      border-color=#444152
+      "urgency=normal" = {
+        border-color = "#1b1924de";
+      };
 
-      [urgency=normal]
-      border-color=#1b1924de
+      "urgency=high" = {
+        border-color = "#ff5555";
+        default-timeout = 0;
+      };
 
-      [urgency=high]
-      border-color=#ff5555
-      default-timeout=0
-
-      [category=mpd]
-      default-timeout=2000
-      group-by=category
-    '';
+      "category=mpd" = {
+        default-timeout = 2000;
+        group-by = "category";
+      };
+    };
   };
 }
