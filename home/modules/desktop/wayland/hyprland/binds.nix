dcbdc927c6e89a450bde09db5c0f3b5a98d09adb
Luis Quiñones <lpaandres2020@gmail.com>
2025-05-30 22:58:33 -0500

    feat(home/hyprland): migrate everything to hm options

--- home/modules/desktop/wayland/hyprland/binds.nix
+++ home/modules/desktop/wayland/hyprland/binds.nix
@@ -5,179 +5,171 @@
   system,
   pkgs,
   ...
-}: {
+}: let
   mainMod = "SUPER";
 
-  binds = let
-    window = [
-      "$mainMod, left, movefocus, l"
-      "$mainMod, right, movefocus, r"
-      "$mainMod, up, movefocus, u"
-      "$mainMod, down, movefocus, d"
-    ];
-
-    workspaces = [
-      # Go-to
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
-      "$mainMod, 0, workspace, 10"
-
-      # Move window
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
-      "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-      # Resize window
-      "$mainMod SHIFT, h, resizeactive, -40 0"
-      "$mainMod SHIFT, l, resizeactive, 40 0"
-      "$mainMod SHIFT, k, resizeactive, 0 -40"
-      "$mainMod SHIFT, j, resizeactive, 0 40"
-
-      # Scroll
-      "$mainMod, mouse_down, workspace, e+1"
-      "$mainMod, mouse_up, workspace, e-1"
-
-      # Other actions
-      "$mainMod, SPACE, togglefloating"
-      "$mainMod, MINUS, togglesplit"
-      "SUPER_SHIFT, W, killactive"
-      "SUPER_SHIFT, MINUS, pseudo"
-      "$mainMod, F, fullscreen"
-    ];
-
-    custom = let
-      rofi-plugin-call = name: program-to-exec: ''${pkgs.rofi}/bin/rofi -modi "${name}:${program-to-exec}" -show ${name}'';
-
-      spotify-dbus-control = inputs.spotify-dbus-control.defaultPackage.${system};
-      hyprstfu = inputs.hyprstfu.defaultPackage.${system};
-
-      inherit (inputs.nix-scripts.packages.${system}) sys-sound sys-brightness cliphist-rofi;
-
-      grimblast = inputs.hyprland-contrib.packages.${system}.grimblast.overrideAttrs (_oldAttrs: {
-        prePatch = ''
-          substituteInPlace ./grimblast --replace '-t 3000' '-t 3000 -i ${../../../../dots/icons/crop.512.png}'
-        '';
-      });
-
-      inherit (pkgs) lib;
-    in [
-      {
-        "mod+key" = "$mainMod, RETURN";
-        "dispatcher" = "exec, ${pkgs.lib.getExe config.programs.ghostty.package}";
-      }
-      {
-        "mod+key" = ",XF86AudioMicMute";
-        "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --toggle-mic";
-      }
-      {
-        "mod+key" = ",XF86AudioMute";
-        "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --toggle-vol";
-      }
-      {
-        "mod+key" = ",XF86AudioLowerVolume";
-        "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --dec";
-      }
-      {
-        "mod+key" = ",XF86AudioRaiseVolume";
-        "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --inc";
-      }
-      {
-        "mod+key" = "SUPER, XF86AudioRaiseVolume";
-        "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --inc --unleashed";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, Print";
-        "dispatcher" = "exec, ${grimblast}/bin/grimblast --freeze --notify copy area";
-      }
-      {
-        "mod+key" = "$mainMod, Print";
-        "dispatcher" = "exec, ${grimblast}/bin/grimblast --notify copy active";
-      }
-      {
-        "mod+key" = ",Print";
-        "dispatcher" = "exec, ${grimblast}/bin/grimblast --notify copy screen";
-      }
-      {
-        "mod+key" = ",XF86MonBrightnessDown";
-        "dispatcher" = "exec, ${sys-brightness}/bin/sys-brightness --dec";
-      }
-      {
-        "mod+key" = ",XF86MonBrightnessUp";
-        "dispatcher" = "exec, ${sys-brightness}/bin/sys-brightness --inc";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, C";
-        "dispatcher" = "exec, ${rofi-plugin-call "clipboard" "${cliphist-rofi}/bin/cliphist-rofi"}";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, Q";
-        "dispatcher" = "exec, ${pkgs.rofi}/bin/rofi -show window";
-      }
-      {
-        "mod+key" = "$mainMod, Q";
-        "dispatcher" = "exec, ${pkgs.rofi}/bin/rofi -show drun";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, E";
-        "dispatcher" = "exec, ${pkgs.bemoji}/bin/bemoji";
-      }
-      {
-        "mod+key" = "CTRL_SHIFT, braceleft";
-        "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --push-back";
-      }
-      {
-        "mod+key" = "CTRL_SHIFT, braceright";
-        "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --push-forward";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, braceright";
-        "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --next";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, braceleft";
-        "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --prev";
-      }
-      {
-        "mod+key" = "$mainMod, Pause";
-        "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --toggle";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, R";
-        "dispatcher" = "exec, ${pkgs.hyprland}/bin/hyprctl reload";
-      }
-      {
-        "mod+key" = "SUPER_SHIFT, S";
-        "dispatcher" = "exec, ${pkgs.toybox}/bin/pkill glava || ${pkgs.glava}/bin/glava -d";
-      }
-      {
-        "mod+key" = "$mainMod, M";
-        "dispatcher" = "exec, ${hyprstfu}/bin/hyprstfu";
-      }
-      {
-        "mod+key" = "$mainMod, K";
-        "dispatcher" = "exec, ${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'";
-      }
-    ];
-  in
-    window ++ workspaces ++ builtins.map (b: b."mod+key" + ", " + b.dispatcher) custom;
-
-  mouseBinds = [
-    # Move/resize windows with mainMod + LMB/RMB and dragging
-    "$mainMod, mouse:272, movewindow"
-    "$mainMod, mouse:273, resizewindow"
+  window = [
+    "${mainMod}, left, movefocus, l"
+    "${mainMod}, right, movefocus, r"
+    "${mainMod}, up, movefocus, u"
+    "${mainMod}, down, movefocus, d"
   ];
-}
+
+  workspaces = [
+    # Go-to
+    "${mainMod}, 1, workspace, 1"
+    "${mainMod}, 2, workspace, 2"
+    "${mainMod}, 3, workspace, 3"
+    "${mainMod}, 4, workspace, 4"
+    "${mainMod}, 5, workspace, 5"
+    "${mainMod}, 6, workspace, 6"
+    "${mainMod}, 7, workspace, 7"
+    "${mainMod}, 8, workspace, 8"
+    "${mainMod}, 9, workspace, 9"
+    "${mainMod}, 0, workspace, 10"
+
+    # Move window
+    "${mainMod} SHIFT, 1, movetoworkspace, 1"
+    "${mainMod} SHIFT, 2, movetoworkspace, 2"
+    "${mainMod} SHIFT, 3, movetoworkspace, 3"
+    "${mainMod} SHIFT, 4, movetoworkspace, 4"
+    "${mainMod} SHIFT, 5, movetoworkspace, 5"
+    "${mainMod} SHIFT, 6, movetoworkspace, 6"
+    "${mainMod} SHIFT, 7, movetoworkspace, 7"
+    "${mainMod} SHIFT, 8, movetoworkspace, 8"
+    "${mainMod} SHIFT, 9, movetoworkspace, 9"
+    "${mainMod} SHIFT, 0, movetoworkspace, 10"
+
+    # Resize window
+    "${mainMod} SHIFT, h, resizeactive, -40 0"
+    "${mainMod} SHIFT, l, resizeactive, 40 0"
+    "${mainMod} SHIFT, k, resizeactive, 0 -40"
+    "${mainMod} SHIFT, j, resizeactive, 0 40"
+
+    # Scroll
+    "${mainMod}, mouse_down, workspace, e+1"
+    "${mainMod}, mouse_up, workspace, e-1"
+
+    # Other actions
+    "${mainMod}, SPACE, togglefloating"
+    "${mainMod}, MINUS, togglesplit"
+    "SUPER_SHIFT, W, killactive"
+    "SUPER_SHIFT, MINUS, pseudo"
+    "${mainMod}, F, fullscreen"
+  ];
+
+  custom = let
+    rofi-plugin-call = name: program-to-exec: ''${pkgs.rofi}/bin/rofi -modi "${name}:${program-to-exec}" -show ${name}'';
+
+    spotify-dbus-control = inputs.spotify-dbus-control.defaultPackage.${system};
+    hyprstfu = inputs.hyprstfu.defaultPackage.${system};
+
+    inherit (inputs.nix-scripts.packages.${system}) sys-sound sys-brightness cliphist-rofi;
+
+    grimblast = inputs.hyprland-contrib.packages.${system}.grimblast.overrideAttrs (_oldAttrs: {
+      prePatch = ''
+        substituteInPlace ./grimblast --replace '-t 3000' '-t 3000 -i ${../../../../dots/icons/crop.512.png}'
+      '';
+    });
+
+    inherit (pkgs) lib;
+  in [
+    {
+      "mod+key" = "${mainMod}, RETURN";
+      "dispatcher" = "exec, ${pkgs.lib.getExe config.programs.ghostty.package}";
+    }
+    {
+      "mod+key" = ",XF86AudioMicMute";
+      "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --toggle-mic";
+    }
+    {
+      "mod+key" = ",XF86AudioMute";
+      "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --toggle-vol";
+    }
+    {
+      "mod+key" = ",XF86AudioLowerVolume";
+      "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --dec";
+    }
+    {
+      "mod+key" = ",XF86AudioRaiseVolume";
+      "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --inc";
+    }
+    {
+      "mod+key" = "SUPER, XF86AudioRaiseVolume";
+      "dispatcher" = "exec, ${sys-sound}/bin/sys-sound --inc --unleashed";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, Print";
+      "dispatcher" = "exec, ${grimblast}/bin/grimblast --freeze --notify copy area";
+    }
+    {
+      "mod+key" = "${mainMod}, Print";
+      "dispatcher" = "exec, ${grimblast}/bin/grimblast --notify copy active";
+    }
+    {
+      "mod+key" = ",Print";
+      "dispatcher" = "exec, ${grimblast}/bin/grimblast --notify copy screen";
+    }
+    {
+      "mod+key" = ",XF86MonBrightnessDown";
+      "dispatcher" = "exec, ${sys-brightness}/bin/sys-brightness --dec";
+    }
+    {
+      "mod+key" = ",XF86MonBrightnessUp";
+      "dispatcher" = "exec, ${sys-brightness}/bin/sys-brightness --inc";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, C";
+      "dispatcher" = "exec, ${rofi-plugin-call "clipboard" "${cliphist-rofi}/bin/cliphist-rofi"}";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, Q";
+      "dispatcher" = "exec, ${pkgs.rofi}/bin/rofi -show window";
+    }
+    {
+      "mod+key" = "${mainMod}, Q";
+      "dispatcher" = "exec, ${pkgs.rofi}/bin/rofi -show drun";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, E";
+      "dispatcher" = "exec, ${pkgs.bemoji}/bin/bemoji";
+    }
+    {
+      "mod+key" = "CTRL_SHIFT, braceleft";
+      "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --push-back";
+    }
+    {
+      "mod+key" = "CTRL_SHIFT, braceright";
+      "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --push-forward";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, braceright";
+      "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --next";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, braceleft";
+      "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --prev";
+    }
+    {
+      "mod+key" = "${mainMod}, Pause";
+      "dispatcher" = "exec, ${lib.getExe spotify-dbus-control} --toggle";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, R";
+      "dispatcher" = "exec, ${pkgs.hyprland}/bin/hyprctl reload";
+    }
+    {
+      "mod+key" = "SUPER_SHIFT, S";
+      "dispatcher" = "exec, ${pkgs.toybox}/bin/pkill glava || ${pkgs.glava}/bin/glava -d";
+    }
+    {
+      "mod+key" = "${mainMod}, M";
+      "dispatcher" = "exec, ${hyprstfu}/bin/hyprstfu";
+    }
+    {
+      "mod+key" = "${mainMod}, K";
+      "dispatcher" = "exec, ${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'";
+    }
+  ];
+in
+  window ++ workspaces ++ builtins.map (b: b."mod+key" + ", " + b.dispatcher) custom
--- home/modules/desktop/wayland/hyprland/default.nix
+++ home/modules/desktop/wayland/hyprland/default.nix
@@ -1,7 +1,22 @@
-{
-  imports = [
-    ./hyprland.nix
+args @ {
+  pkgs,
+  lib,
+  ...
+}: let
+  mainMod = "SUPER";
 
+  execOnce = [
+    "${pkgs.wl-clipboard}/bin/wl-paste --watch ${pkgs.cliphist}/bin/cliphist store -max-items 200"
+    "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+    "[workspace 2 silent] ghostty"
+    "${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'; while [ -z \"$ADDRESS\" ] ; do ADDRESS=$(hyprctl clients -j | jq -r '.[] | select(.class == \"ghostty.hyprdrop\") | .address'); done; ${pkgs.extra.hyprdrop}/bin/hyprdrop -i ghostty.hyprdrop 'ghostty --class=ghostty.hyprdrop'"
+  ];
+
+  execAlways = [
+    "pkill waybar & sleep 0.1 && waybar"
+  ];
+in {
+  imports = [
     ../components/swww.nix
     ../components/mako.nix
     ../components/waybar
@@ -10,4 +25,138 @@
     ../../rofi.nix
     ../../gtk.nix
   ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = ",preferred,auto,1,mirror,eDP-1";
+      env = "XCURSOR_SIZE,24";
+
+      general = {
+        layout = "dwindle";
+        gaps_in = 4;
+        gaps_out = 20;
+        border_size = 1;
+
+        "col.active_border" = "rgba(595959aa)";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        windowrule = [
+          "opacity 1.0 override 1.0 override,class:Emulator"
+        ];
+
+        windowrulev2 = [
+          "float,center,forceinput, class:(Rofi)$"
+          "float, title:^(Android Emulator -)"
+          "float, title:^(Emulator)$"
+        ];
+      };
+
+      decoration = {
+        rounding = 4;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+        active_opacity = 0.9;
+        inactive_opacity = 1.0;
+        fullscreen_opacity = 1.0;
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
+        };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = [
+          "myBezier, 0.05, 0.9, 0.1, 1.05"
+        ];
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      input = {
+        numlock_by_default = true;
+        sensitivity = 0.2;
+
+        kb_layout = "latam";
+        kb_variant = "";
+        kb_model = "";
+        kb_options = "";
+        kb_rules = "";
+
+        follow_mouse = 1;
+
+        touchpad = {
+          natural_scroll = "no";
+          disable_while_typing = "no";
+        };
+      };
+
+      misc = {
+        disable_autoreload = true;
+        disable_hyprland_logo = true;
+        focus_on_activate = false;
+        new_window_takes_over_fullscreen = 1;
+        initial_workspace_tracking = 2;
+      };
+
+      dwindle = {
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+      master = {
+        new_status = "master";
+      };
+
+      gestures = {
+        workspace_swipe = "off";
+      };
+
+      ecosystem = {
+        no_update_news = true;
+        enforce_permissions = true;
+      };
+
+      device = {
+        name = "epic-mouse-v1";
+        sensitivity = "-0.5";
+      };
+
+      bind = import ./binds.nix args;
+      bindm = [
+        "${mainMod}, mouse:272, movewindow"
+        "${mainMod}, mouse:273, resizewindow"
+      ];
+
+      permission = [
+        "${lib.getExe pkgs.hyprpicker}, screencopy, allow"
+        "${lib.getExe pkgs.grim}, screencopy, allow"
+        "/nix/store/*-zen*/bin/(zen|zen-beta|zen-twilight), screencopy, allow"
+        "${lib.getExe pkgs.brave}, screencopy, allow"
+        "${lib.getExe pkgs.discord-canary}, screencopy, allow"
+        "${lib.getExe pkgs.obs-studio}, screencopy, allow"
+        "${lib.getExe pkgs.xdg-desktop-portal-hyprland}, screencopy, allow"
+      ];
+    };
+    extraConfig = ''
+      $mainMod = ${mainMod}
+
+      ${lib.concatMapStrings (cmd: "exec-once = ${cmd}\n") execOnce}
+      ${lib.concatMapStrings (cmd: "exec = ${cmd}\n") execAlways}
+    '';
+  };
 }
