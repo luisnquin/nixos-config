7fa686bd944378ba9639610404732d03129fb972
Luis Quiñones <lpaandres2020@gmail.com>
2025-06-26 22:53:55 -0500

    feat(home/browser): add my extensions in zen

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -52,6 +52,16 @@
         Cryptomining = true;
         Fingerprinting = true;
       };
+      ExtensionSettings = {
+        "wappalyzer@crunchlabz.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4482384/wappalyzer-6.10.82.xpi";
+          installation_mode = "force_installed";
+        };
+        "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4156831/github_file_icons-1.5.1.xpi";
+          installation_mode = "force_installed";
+        };
+      };
     };
   };
 
