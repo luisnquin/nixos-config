ff34eb004b9167fcd692384b73e7a9222f4b90f9
Luis Quiñones <lpaandres2020@gmail.com>
2025-06-26 22:53:36 -0500

    feat(home/browser): let's say that I've enabled tracking protection in zen

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -46,6 +46,12 @@
       DontCheckDefaultBrowser = true;
       NoDefaultBookmarks = true;
       OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
     };
   };
 
