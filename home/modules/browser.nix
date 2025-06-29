01cd8c355226e673b3f6a0f48cd81fe55d5d69a1
Luis Quiñones <lpaandres2020@gmail.com>
2025-06-29 12:37:16 -0500

    feat(home/browser): always use latest extension version
    I'm not feeling ok.

--- home/modules/browser.nix
+++ home/modules/browser.nix
@@ -54,11 +54,11 @@
       };
       ExtensionSettings = {
         "wappalyzer@crunchlabz.com" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4482384/wappalyzer-6.10.82.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/wappalyzer/latest.xpi";
           installation_mode = "force_installed";
         };
         "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4156831/github_file_icons-1.5.1.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/github-file-icons/latest.xpi";
           installation_mode = "force_installed";
         };
       };
