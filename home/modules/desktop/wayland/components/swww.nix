b529d6cfe752db761d77c656b1cf7b0d82695fc2
Luis Quiñones <lpaandres2020@gmail.com>
2025-07-11 15:45:09 -0500

    chore(wayland/swww): remove exec line to reset background "image"

--- home/modules/desktop/wayland/components/swww.nix
+++ home/modules/desktop/wayland/components/swww.nix
@@ -12,8 +12,6 @@
     inherit (inputs.nix-scripts.packages.${system}) swww-switcher;
   in {
     "hypr/hyprland.conf".text = ''
-      exec = ${pkgs.swww}/bin/swww img ${../../../../dots/background.gif}
-
       bind = $mainMod, L, exec, ${swww-switcher}/bin/cli ${builtins.concatStringsSep " " (wallpaperFiles
         ++ [
           ../../../../dots/background.gif
