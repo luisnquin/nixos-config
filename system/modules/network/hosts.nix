9cf1837bc8e1a5b2f94d86043d8a653ac64478e9
Luis Quiñones <lpaandres2020@gmail.com>
2025-07-23 16:53:33 -0500

    feat(system/network): update list of banned domains

--- system/modules/network/hosts.nix
+++ system/modules/network/hosts.nix
@@ -1,9 +1,10 @@
 {
   networking.hosts = {
     "127.0.0.1" = [
-      # "www.instagram.com"
-      # "web.facebook.com"
-      # "facebook.com"
+      "www.instagram.com"
+      "web.facebook.com"
+      "www.facebook.com"
+      "facebook.com"
       "www.tiktok.com" # no fucking way I'm going to use this
       "www.pinterest.es"
     ];
