8f102d1f8f4bacbfbd46516d785f29e6b596248b
Luis Quiñones <lpaandres2020@gmail.com>
2025-07-23 00:25:41 -0500

    chore(system/network): avoid to leak traffic to 1.1.1.1

--- system/modules/network/cloudflare.nix
+++ system/modules/network/cloudflare.nix
@@ -1,18 +1,14 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   environment.systemPackages = [
     pkgs.cloudflared
   ];
 
-  services.resolved = {
-    enable = true;
-    fallbackDns = builtins.map (name: "${name}#cloudflare-dns.com") config.networking.nameservers;
-    dnsovertls = "true"; # not opportunistic
-    domains = ["~."];
-  };
+  # services.resolved = {
+  #   enable = true;
+  #   fallbackDns = builtins.map (name: "${name}#cloudflare-dns.com") config.networking.nameservers;
+  #   dnsovertls = "true"; # not opportunistic
+  #   domains = ["~."];
+  # };
 
-  networking.nameservers = ["1.1.1.1" "1.0.0.1"];
+  # networking.nameservers = ["1.1.1.1" "1.0.0.1"];
 }
