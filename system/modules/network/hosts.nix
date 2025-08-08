7a3322edb9272126331b8f578c7aab306fc1f3bd
Luis Quiñones <lpaandres2020@gmail.com>
2025-08-08 10:33:21 -0500

    revert: "feat(system/network): temporarily allow facebook io"
    This reverts commit f01104ca196d8cbfe4ab94995a7390b9dc7b745a.

--- system/modules/network/hosts.nix
+++ system/modules/network/hosts.nix
@@ -2,9 +2,9 @@
   networking.hosts = {
     "127.0.0.1" = [
       "www.instagram.com"
-      # "web.facebook.com"
-      # "www.facebook.com"
-      # "facebook.com"
+      "web.facebook.com"
+      "www.facebook.com"
+      "facebook.com"
       "www.tiktok.com" # no fucking way I'm going to use this
       "www.pinterest.es"
     ];
