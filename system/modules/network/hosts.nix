84cd6a52e075328dde4481f02c3e79fee9818759
Luis Quiñones <lpaandres2020@gmail.com>
2025-08-10 11:01:37 -0500

    feat(network/hosts): ban more domains from my system

--- system/modules/network/hosts.nix
+++ system/modules/network/hosts.nix
@@ -2,11 +2,27 @@
   networking.hosts = {
     "127.0.0.1" = [
       "www.instagram.com"
+      "instagram.com"
+
       "web.facebook.com"
       "www.facebook.com"
+      "m.facebook.com"
       "facebook.com"
-      "www.tiktok.com" # no fucking way I'm going to use this
+      "www.fb.com"
+      "fb.com"
+
       "www.pinterest.es"
+
+      "www.threads.net"
+      "threads.net"
+
+      "messenger.com"
+      "www.messenger.com"
+
+      "www.tiktok.com" # no fucking way I'm going to use this
+      "vt.tiktok.com"
+      "m.tiktok.com"
+      "tiktok.com"
     ];
   };
 }
