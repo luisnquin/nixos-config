32d10a70cfc4e3034d886ab05543ffff4b677370
Luis Quiñones <lpaandres2020@gmail.com>
2025-08-17 20:43:04 -0500

    feat(browser/zen): add search engines settings for default profile

--- home/modules/programs/browser/zen.nix
+++ home/modules/programs/browser/zen.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.zen-browser.homeModules.twilight
   ];
@@ -35,6 +39,7 @@
 
   programs.zen-browser = {
     enable = true;
+
     policies = let
       locked = value: {
         Value = value;
@@ -81,6 +86,78 @@
         # Disable swipe gestures (Browser:BackOrBackDuplicate, Browser:ForwardOrForwardDuplicate)
         "browser.gesture.swipe.left" = "";
         "browser.gesture.swipe.right" = "";
+      };
+    };
+
+    profiles.default = {
+      force = true;
+      search = {
+        engines = let
+          nixSnowflakeIcon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+        in {
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "channel";
+                    value = "unstable";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = nixSnowflakeIcon;
+            definedAliases = ["np"];
+          };
+          "Nix Options" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  {
+                    name = "channel";
+                    value = "unstable";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = nixSnowflakeIcon;
+            definedAliases = ["nop"];
+          };
+          "Home Manager Options" = {
+            urls = [
+              {
+                template = "https://home-manager-options.extranix.com/";
+                params = [
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                  {
+                    name = "release";
+                    value = "master"; # unstable
+                  }
+                ];
+              }
+            ];
+            icon = nixSnowflakeIcon;
+            definedAliases = ["hmop"];
+          };
+          bing.metaData.hidden = "true";
+        };
       };
     };
   };
