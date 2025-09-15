9ff38319e1c744b54ce753eba2c1e072952ff64c
Luis Quiñones <lpaandres2020@gmail.com>
2025-09-15 07:54:35 -0400

    feat(editors/vscode): use binary @ v1.104.0

--- home/modules/programs/development/editors/vscode.nix
+++ home/modules/programs/development/editors/vscode.nix
@@ -1,7 +1,32 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  system,
+  ...
+}: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode;
+    package = let
+      version = "1.104.0";
+      throwSystem = throw "Unsupported system: ${system}";
+      plat =
+        {
+          x86_64-linux = "linux-x64";
+          x86_64-darwin = "darwin";
+          aarch64-linux = "linux-arm64";
+          aarch64-darwin = "darwin-arm64";
+          armv7l-linux = "linux-armhf";
+        }
+    .${
+          system
+        } or throwSystem;
+    in
+      pkgs.vscode.overrideAttrs (oldAttrs: {
+        src = builtins.fetchTarball {
+          url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+          sha256 = "1319wjggg77fs6kgw8jfdjxifgg7sdhw51r5spspsldklk1a7qih";
+        };
+        inherit version;
+      });
     mutableExtensionsDir = true;
 
     profiles.default = {
