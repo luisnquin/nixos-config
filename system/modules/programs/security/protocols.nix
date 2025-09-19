9a0a5db5d34b6f4769cfaaf03a4b1c134fbe7c93
Luis Quiñones <lpaandres2020@gmail.com>
2025-09-19 16:29:02 -0500

    feat(security/protocols): adjust ttl params of gnupg agent

--- system/modules/programs/security/protocols.nix
+++ system/modules/programs/security/protocols.nix
@@ -7,6 +7,11 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    settings = {
+      # this is your vector attack
+      default-cache-ttl = 60 * 15;
+      max-cache-ttl = 60 * 20;
+    };
   };
 
   environment = {
