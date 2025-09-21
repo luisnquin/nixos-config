57d09d338421122dcb84f5ca331ced2f1cc54081
Luis Quiñones <lpaandres2020@gmail.com>
2025-09-21 13:36:38 -0500

    feat(peripherals/printing): add settings to ensure a printer

--- system/modules/programs/peripherals/printing.nix
+++ system/modules/programs/peripherals/printing.nix
@@ -3,4 +3,18 @@
     enable = true;
     drivers = [pkgs.epson-escpr];
   };
+
+  hardware.printers.ensurePrinters = [
+    {
+      name = "EPSON_L3110_Series";
+      description = "That shitty printer";
+      location = "Outsource";
+      deviceUri = "usb://EPSON/L3110%20Series?serial=583634353439353376&interface=1";
+      model = "epson-inkjet-printer-escpr/Epson-L3110_Series-epson-escpr-en.ppd";
+      ppdOptions = {
+        PageSize = "A4";
+        ColorModel = "RGB";
+      };
+    }
+  ];
 }
