99224c172c70b3bf04b661f877a47f30f59243fc
Luis Quiñones <lpaandres2020@gmail.com>
2025-09-23 16:28:03 -0500

    feat(shell/man): use hm option to enable tealdeer + replace man @ home lvl

--- home/modules/programs/shell/man.nix
+++ home/modules/programs/shell/man.nix
@@ -1,5 +1,13 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    tealdeer
-  ];
+  programs.tealdeer = {
+    enable = true;
+    settings.updates = {
+      auto_update = true;
+      auto_update_interval_hours = 48; # every 2d
+    };
+  };
+
+  home.shellAliases = {
+    man = "tdlr";
+  };
 }
