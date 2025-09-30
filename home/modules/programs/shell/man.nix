2d07ae557d6f33ac2261e0c41e54ec1fd157dbd9
Luis Quiñones <lpaandres2020@gmail.com>
2025-09-30 00:37:23 -0500

    fix(shell/man): remove man alias at hm level
    I already had a lot of problems because of that decision.

--- home/modules/programs/shell/man.nix
+++ home/modules/programs/shell/man.nix
@@ -1,13 +1,9 @@
-{ ...}: {
+{
   programs.tealdeer = {
     enable = true;
     settings.updates = {
       auto_update = true;
       auto_update_interval_hours = 48; # every 2d
     };
-  };
-
-  home.shellAliases = {
-    man = "tdlr";
   };
 }
