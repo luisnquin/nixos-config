501ae6e0ee89d3ebeebc7aedf48e8d9b7de7c6b1
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-04 15:10:53 -0500

    feat(hardware/battery): override batlimit's safe charget limit + refactor

--- system/modules/hardware/battery.nix
+++ system/modules/hardware/battery.nix
@@ -7,11 +7,16 @@
   ...
 }: let
   powerSupplyBatteryPath = "/sys/class/power_supply/BAT1";
+
+  offset = 1;
+  safeChargeLimit = builtins.toString (host.batteryThreshold + offset);
 in {
   environment.systemPackages = let
     inherit (inputs.nix-scripts.packages.${system}) batlimit;
   in [
-    (batlimit.override {inherit powerSupplyBatteryPath;})
+    (batlimit.override {
+      inherit powerSupplyBatteryPath safeChargeLimit;
+    })
   ];
 
   security.sudo.extraRules = [
@@ -38,13 +43,10 @@ in {
     enable = host.isLaptop;
     description = "Set the battery charge threshold";
     serviceConfig = let
-      offset = 1;
-
-      batteryThreshold = builtins.toString (host.batteryThreshold + offset);
       batteryChargeThresholdPath = "${powerSupplyBatteryPath}/charge_control_end_threshold";
     in {
       Type = "oneshot";
-      ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${batteryThreshold} > ${batteryChargeThresholdPath}"'';
+      ExecStart = ''${pkgs.bash}/bin/bash -c "echo ${safeChargeLimit} > ${batteryChargeThresholdPath}"'';
     };
     wantedBy = ["multi-user.target"];
   };
