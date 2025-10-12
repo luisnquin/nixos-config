ab0e27f3b9f00b4833667551020f1a180bf8d95b
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-11 20:39:01 -0500

    feat(dev/uav): enable express lrs configurator via flatpak

--- home/modules/programs/development/uav.nix
+++ home/modules/programs/development/uav.nix
@@ -2,4 +2,11 @@
   home.packages = [
     pkgs.betaflight-configurator
   ];
+
+  services.flatpak.packages = [
+    {
+      appId = "org.expresslrs.ExpressLRSConfigurator";
+      origin = "flathub";
+    }
+  ];
 }
