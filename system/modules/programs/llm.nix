1af69235795f8c3550c01acf137d3fbb2b0f6277
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-19 18:34:46 -0500

    feat(llm): f this, ollama @ v0.12.3 + enable open webui service

--- system/modules/programs/llm.nix
+++ system/modules/programs/llm.nix
@@ -1,3 +1,20 @@
-{
-  services.ollama.enable = true;
+{pkgs, ...}: {
+  services = {
+    ollama = {
+      enable = true;
+      package = pkgs.ollama.overrideAttrs (old: rec {
+        # Use the latest version from nixpkgs
+        version = "0.12.3";
+        src = pkgs.fetchFromGitHub {
+          owner = "ollama";
+          repo = "ollama";
+          tag = version;
+          hash = "sha256-ooDGwTklGJ/wzDlAY3uJiqpZUxT1cCsqVNJKU8BAPbQ=";
+        };
+
+        vendorHash = "sha256-SlaDsu001TUW+t9WRp7LqxUSQSGDF1Lqu9M1bgILoX4=";
+      });
+    };
+    open-webui.enable = true;
+  };
 }
