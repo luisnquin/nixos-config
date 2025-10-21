a277852e228d50c8f9cf92cb99cc8781bea5a3ce
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-20 21:19:01 -0500

    chore(git/cli): remove old entry
    nosir

--- home/modules/programs/development/git/cli.nix
+++ home/modules/programs/development/git/cli.nix
@@ -24,15 +24,6 @@
       "**/result-*"
     ];
 
-    includes = [
-      (let
-        folder = "~/Projects/github.com/INVERSIONESIO";
-      in {
-        condition = "gitdir:${folder}/";
-        path = "${folder}/.gitconfig_include";
-      })
-    ];
-
     extraConfig = {
       core = {
         editor = "nano -w";
