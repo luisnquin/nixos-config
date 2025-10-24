58a731e7ff05fd32d747247f83a7c3aa67c1fdd3
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-23 20:57:33 -0500

    fix(system/kernel): use stable provider
    Latest git is currently broken and my computer can't boot.

--- system/modules/core/kernel.nix
+++ system/modules/core/kernel.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   boot = {
     kernelParams = ["i8042.reset=1"];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages; # stablevariant?
     extraModprobeConfig = ''
       options snd-intel-dspcfg dsp_driver=1
     '';
