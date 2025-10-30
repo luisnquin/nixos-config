d4771014a896e58c9b366051e27046b953e26162
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-29 21:46:30 -0500

    feat(dev/uav): still draft but use beta configurator from web app
    the package is broken and will be deprecated soon??? I read that long ago

--- home/modules/programs/development/uav.nix
+++ home/modules/programs/development/uav.nix
@@ -1,9 +1,46 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
+{pkgs, ...}: let
+  betaflight-configurator = pkgs.stdenv.mkDerivation rec {
+    pname = "betaflight-configurator-app";
+    version = "unreleased";
+
+    dontUnpack = true;
+    nativeBuildInputs = [pkgs.copyDesktopItems];
+
+    desktopItems = [
+      (pkgs.makeDesktopItem {
+        name = pname;
+        desktopName = "Betaflight Configurator";
+        comment = "Launch Betaflight Configurator in Chromium";
+        exec = pname;
+        icon = "chromium"; # TODO: use official icon
+        categories = ["Network" "Utility"];
+        startupNotify = true;
+      })
+    ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+
+      # The executable wrapper
+      cat > $out/bin/${pname} <<EOF
+      #!/bin/sh
+      exec ${pkgs.lib.getExe pkgs.chromium} --no-first-run --app=https://app.betaflight.com "\$@"
+      EOF
+      chmod +x $out/bin/${pname}
+
+      runHook postInstall
+    '';
+
+    meta.description = "A wrapper to open Betaflight Configurator web app in Chromium";
+  };
+in {
+  home.packages = [
     betaflight-configurator
-    express-lrs-configurator
-    inav-configurator
-    mission-planner
-    qgroundcontrol
+    pkgs.express-lrs-configurator
+    pkgs.inav-configurator
+    pkgs.mission-planner
+    pkgs.qgroundcontrol
   ];
 }
