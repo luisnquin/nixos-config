367c874c99e7c1ed08c6057794b57ed43a691a16
Luis Quiñones <lpaandres2020@gmail.com>
2025-10-31 11:57:45 -0500

    feat(studio/3d): add fstl

--- home/modules/programs/studio/3d.nix
+++ home/modules/programs/studio/3d.nix
@@ -4,6 +4,21 @@
     orca-slicer
     freecad-wayland
     inkscape # vector graphics, before 3d
+
+    (fstl.overrideAttrs (
+      old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.copyDesktopItems];
+        desktopItems = [
+          (pkgs.makeDesktopItem {
+            name = "fstl";
+            comment = "A fast STL file viewer";
+            exec = "${lib.getExe pkgs.fstl}";
+            desktopName = "FSTL";
+            keywords = ["3d"];
+          })
+        ];
+      }
+    ))
   ];
 
   services.flatpak.packages = [
