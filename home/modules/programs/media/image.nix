030aeb1ec8b88846d4671d85d9ef3e1f94c86268
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-07 23:12:21 -0500

    feat(media/image): use sxiv to handle gif files

--- home/modules/programs/media/image.nix
+++ home/modules/programs/media/image.nix
@@ -12,22 +12,41 @@
       })
     ];
   });
+
+  sxiv = pkgs.sxiv.overrideAttrs (old: {
+    nativeBuildInputs = [pkgs.copyDesktopItems];
+
+    desktopItems = [
+      (pkgs.makeDesktopItem {
+        name = "sxiv (gif)";
+        exec = "sxiv -a";
+        desktopName = "sxiv";
+        genericName = "Gif Viewer";
+      })
+    ];
+  });
 in {
   home.packages = [
     pkgs.imagemagick
     feh
+    sxiv
   ];
 
   xdg.mimeApps = let
-    associations = builtins.listToAttrs (map (mimeType: {
-        name = mimeType;
-        value = "feh.desktop";
-      })
-      [
-        "image/svg+xml"
-        "image/png"
-        "image/jpeg"
-      ]);
+    associations =
+      builtins.listToAttrs (map (mimeType: {
+          name = mimeType;
+          value = "feh.desktop";
+        })
+        [
+          "image/svg+xml"
+          "image/png"
+          "image/jpeg"
+        ])
+      // {
+        name = "image/gif";
+        value = "sxiv.desktop";
+      };
   in {
     associations.added = associations;
     defaultApplications = associations;
