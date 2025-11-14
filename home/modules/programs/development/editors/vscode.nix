78f1e8135c3e12043feb2024960cb40246acf489
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-13 21:27:24 -0500

    feat(vscode): force v1.106.0

--- home/modules/programs/development/editors/vscode.nix
+++ home/modules/programs/development/editors/vscode.nix
@@ -1,6 +1,34 @@
-{pkgs, ...}: {
+{
+  system,
+  pkgs,
+  ...
+}: {
   programs.vscode = {
     enable = true;
+
+    package = let
+      version = "1.106.0";
+      throwSystem = throw "Unsupported system: ${system}";
+      plat =
+        {
+          x86_64-linux = "linux-x64";
+          x86_64-darwin = "darwin";
+          aarch64-linux = "linux-arm64";
+          aarch64-darwin = "darwin-arm64";
+          armv7l-linux = "linux-armhf";
+        }
+    .${
+          system
+        } or throwSystem;
+    in
+      pkgs.vscode.overrideAttrs (oldAttrs: {
+        src = builtins.fetchTarball {
+          url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+          sha256 = "1s311sd1cbpg099wdac3di8f75y632xxw5mhjbyqhyb1mwv5cf3l";
+        };
+        inherit version;
+      });
+
     mutableExtensionsDir = true;
 
     profiles.default = {
