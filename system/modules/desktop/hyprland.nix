1ccbc02efc1148e18caefd1142e457938b246c8b
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-15 07:38:47 -0500

    feat(desktop/hyprland): some config for hyprland's xdg

--- system/modules/desktop/hyprland.nix
+++ system/modules/desktop/hyprland.nix
@@ -1,13 +1,26 @@
-{pkgs, ...}: {
+{
+  hmConfig,
+  pkgs,
+  ...
+}: {
   environment.pathsToLink = ["/libexec"];
 
   xdg.portal = {
-    xdgOpenUsePortal = true;
-    extraPortals = [pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk];
+    # xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      pkgs.xdg-desktop-portal-gtk
+      kdePackages.xdg-desktop-portal-kde
+    ];
 
     configPackages = [
-      pkgs.xdg-desktop-portal-hyprland
+      hmConfig.wayland.windowManager.hyprland.package
     ];
+
+    config.hyprland = {
+      default = ["hyprland" "gtk"];
+      "org.freedesktop.impl.portal.FileChooser" = "kde";
+      "org.freedesktop.impl.portal.Print" = "kde";
+    };
   };
 
   programs = {
