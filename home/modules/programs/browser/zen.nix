3286585fab746d81c11706dc9c9ad66e355ff70f
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-15 07:37:55 -0500

    feat(browser/zen): add refine github extension

--- home/modules/programs/browser/zen.nix
+++ home/modules/programs/browser/zen.nix
@@ -94,6 +94,7 @@
           id = "ublock-origin";
           pinned = true;
         };
+        "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = "refined-github-";
         "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
         "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = "return-youtube-dislikes";
         "{74145f27-f039-47ce-a470-a662b129930a}" = "clearurls";
