cbd08e96879533df9c466708312780e822428b45
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-17 11:02:38 -0500

    feat(dev/git): add glike utility

--- home/modules/programs/development/git/dots/shell.zsh
+++ home/modules/programs/development/git/dots/shell.zsh
@@ -1,60 +1,71 @@
 gbh() {
-    typeset -gA branch_color
-    branch_color=(
-        ['refactor/*']='\033[38;2;70;117;90m'
-        ['security/*']='\033[38;2;196;35;57m'
-        ['chore/*']='\033[38;2;237;40;158m'
-        ['style/*']='\033[38;2;56;93;245m'
-        ['perf/*']='\033[38;2;40;237;122m'
-        ['docs/*']='\033[38;2;89;87;222m'
-        ['dev/*']='\033[38;2;121;48;230m'
-        ['ci/*']='\033[38;2;240;222;22m'
-        ['feat/*']='\033[0;92m'
-        ['master']='\033[0;94m'
-        ['fix/*']='\033[0;93m'
-        ['main']='\033[0;94m'
-    )
+	typeset -gA branch_color
+	branch_color=(
+		['refactor/*']='\033[38;2;70;117;90m'
+		['security/*']='\033[38;2;196;35;57m'
+		['chore/*']='\033[38;2;237;40;158m'
+		['style/*']='\033[38;2;56;93;245m'
+		['perf/*']='\033[38;2;40;237;122m'
+		['docs/*']='\033[38;2;89;87;222m'
+		['dev/*']='\033[38;2;121;48;230m'
+		['ci/*']='\033[38;2;240;222;22m'
+		['feat/*']='\033[0;92m'
+		['master']='\033[0;94m'
+		['fix/*']='\033[0;93m'
+		['main']='\033[0;94m'
+	)
 
-    branch_keys=('feat/*' 'fix/*' 'refactor/*' 'ci/*' 'docs/*' 'dev/*' 'security/*' 'perf/*' 'chore/*' 'master' 'main')
+	branch_keys=('feat/*' 'fix/*' 'refactor/*' 'ci/*' 'docs/*' 'dev/*' 'security/*' 'perf/*' 'chore/*' 'master' 'main')
 
-    current_branch=$(git branch --show-current)
+	current_branch=$(git branch --show-current)
 
-    git for-each-ref --color=always --sort=-committerdate \
-        --format='%(HEAD) %(refname:short) %(color:reset)' refs/heads/ | while read -r branch; do
-        if [[ $branch == *"*"* ]]; then
-            branch="${branch#* }"
-        fi
+	git for-each-ref --color=always --sort=-committerdate \
+		--format='%(HEAD) %(refname:short) %(color:reset)' refs/heads/ | while read -r branch; do
+		if [[ $branch == *"*"* ]]; then
+			branch="${branch#* }"
+		fi
 
-        for pattern in "${branch_keys[@]}"; do
-            if [[ $branch =~ $pattern ]]; then
-                frags=($(echo "$branch" | tr '/' ' '))
+		for pattern in "${branch_keys[@]}"; do
+			if [[ $branch =~ $pattern ]]; then
+				frags=($(echo "$branch" | tr '/' ' '))
 
-                if [ ${#frags[@]} = 3 ]; then
-                    if [[ "$branch" =~ $current_branch ]]; then
-                        branch="${branch_color[$pattern]}${frags[1]}/\033[0m${frags[2]} \033[38;2;93;201;196m← current\033[0m"
-                    else
-                        branch="${branch_color[$pattern]}${frags[1]}/\033[0m${frags[2]}"
-                    fi
-                else
-                    if [[ "$branch" =~ $current_branch ]]; then
-                        branch="${branch_color[$pattern]}${frags[1]}\033[0m \033[38;2;93;201;196m← current\033[0m"
-                    else
-                        branch="${branch_color[$pattern]}${frags[1]}\033[0m"
-                    fi
-                fi
+				if [ ${#frags[@]} = 3 ]; then
+					if [[ "$branch" =~ $current_branch ]]; then
+						branch="${branch_color[$pattern]}${frags[1]}/\033[0m${frags[2]} \033[38;2;93;201;196m← current\033[0m"
+					else
+						branch="${branch_color[$pattern]}${frags[1]}/\033[0m${frags[2]}"
+					fi
+				else
+					if [[ "$branch" =~ $current_branch ]]; then
+						branch="${branch_color[$pattern]}${frags[1]}\033[0m \033[38;2;93;201;196m← current\033[0m"
+					else
+						branch="${branch_color[$pattern]}${frags[1]}\033[0m"
+					fi
+				fi
 
-                break
-            fi
-        done
+				break
+			fi
+		done
 
-        echo "$branch"
-    done
+		echo "$branch"
+	done
 }
 
 ghinit() {
-    pwd | grep -E '[a-z\d\-\_]+\.[a-z]+(/[a-z\d\-\_]+){2}$' >/dev/null || (echo 'current pwd must be in the following format: */<domain>/<owner>/<repo>' && return 1)
-    test -d .git && echo 'current directory is already a git repository' && return 1
+	pwd | grep -E '[a-z\d\-\_]+\.[a-z]+(/[a-z\d\-\_]+){2}$' >/dev/null || (echo 'current pwd must be in the following format: */<domain>/<owner>/<repo>' && return 1)
+	test -d .git && echo 'current directory is already a git repository' && return 1
 
-    git init
-    gh repo create --private --remote origin -s .
+	git init
+	gh repo create --private --remote origin -s .
+}
+
+glike() {
+	PATTERN="$1"
+
+	if [ -z "$PATTERN" ]; then
+		echo "Usage: glike <pattern>"
+		return 1
+	fi
+
+	git log --oneline | grep "$PATTERN" | head
 }
