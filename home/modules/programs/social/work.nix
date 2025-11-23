672542920a30200ebb50d71e9250d8dd584c679c
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-23 07:53:25 -0500

    feat(work): add cohesion + desktop entry to search it as "Notion"

--- home/modules/programs/social/work.nix
+++ home/modules/programs/social/work.nix
@@ -1,7 +1,26 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cohesionAppId = "io.github.brunofin.Cohesion";
+in {
   home.packages = with pkgs; [
     google-chat-linux
-    slack
     zoom-us
   ];
+
+  services.flatpak.packages = [
+    {
+      appId = cohesionAppId;
+      origin = "flathub";
+    }
+  ];
+
+  xdg.desktopEntries.notion = {
+    name = "Notion";
+    exec = "${lib.getExe pkgs.dex} ${config.home.homeDirectory}/.local/share/flatpak/exports/share/applications/${cohesionAppId}.desktop";
+    mimeType = ["x-scheme-handler/notion"];
+  };
 }
