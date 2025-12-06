dc711b824b0c642cec58adcb80008646e930947d
Luis Quiñones <lpaandres2020@gmail.com>
2025-12-05 23:05:29 -0500

    feat(home/shell): use shared stuff

--- home/modules/programs/shell/bat.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.bat = {
-    enable = true;
-    config = {
-      theme = "Nord";
-      italic-text = "always";
-    };
-  };
-}
--- home/modules/programs/shell/default.nix
+++ home/modules/programs/shell/default.nix
@@ -1,16 +1,20 @@
 {
   imports = [
-    ./btop
-    ./macchina
-    ./tmux
-    ./zsh
-
-    ./bat.nix
-    ./direnv.nix
-    ./fzf.nix
     ./man.nix
     ./nao.nix
     ./translate.nix
     ./tty.nix
   ];
+
+  shared = {
+    bat.enable = true;
+    btop.enable = true;
+    direnv.enable = true;
+    fzf.enable = true;
+    macchina.enable = true;
+    tmux.enable = true;
+    zsh.enable = true;
+  };
+
+  programs.zsh.completionInit = builtins.readFile ../../.../../../../tools/nyx/completions.zsh;
 }
