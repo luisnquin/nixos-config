0124c8671aae0594e5c9fe32ca8f0f784a0dd01f
Luis Quiñones <lpaandres2020@gmail.com>
2026-01-03 19:06:22 -0500

    refactor: for a fake hhl i should keep the vscode override in the overlays dir

--- home/modules/programs/development/editors/vscode.nix
+++ home/modules/programs/development/editors/vscode.nix
@@ -1,33 +1,6 @@
-{
-  system,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
-
-    package = let
-      version = "1.106.0";
-      throwSystem = throw "Unsupported system: ${system}";
-      plat =
-        {
-          x86_64-linux = "linux-x64";
-          x86_64-darwin = "darwin";
-          aarch64-linux = "linux-arm64";
-          aarch64-darwin = "darwin-arm64";
-          armv7l-linux = "linux-armhf";
-        }
-    .${
-          system
-        } or throwSystem;
-    in
-      pkgs.vscode.overrideAttrs (_oldAttrs: {
-        src = builtins.fetchTarball {
-          url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
-          sha256 = "1s311sd1cbpg099wdac3di8f75y632xxw5mhjbyqhyb1mwv5cf3l";
-        };
-        inherit version;
-      });
 
     mutableExtensionsDir = true;
 
--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -11,6 +11,30 @@
       });
     }
   )
+  (
+    _self: super: {
+      vscode = super.vscode.overrideAttrs (_old: let
+        version = "1.106.0";
+        throwSystem = throw "Unsupported system: ${system}";
+
+        platEquivs = {
+          x86_64-linux = "linux-x64";
+          x86_64-darwin = "darwin";
+          aarch64-linux = "linux-arm64";
+          aarch64-darwin = "darwin-arm64";
+          armv7l-linux = "linux-armhf";
+        };
+
+        plat = platEquivs.${system} or throwSystem;
+      in {
+        src = builtins.fetchTarball {
+          url = "https://update.code.visualstudio.com/${version}/${plat}/stable";
+          sha256 = "1s311sd1cbpg099wdac3di8f75y632xxw5mhjbyqhyb1mwv5cf3l";
+        };
+        inherit version;
+      });
+    }
+  )
   (_self: _super: {
     inherit (inputs.hyprland.packages.${system}) xdg-desktop-portal-hyprland;
 
