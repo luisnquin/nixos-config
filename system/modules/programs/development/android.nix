1afbec99e74e67e76182f5252cb0c9cabf35e756
Luis Quiñones <lpaandres2020@gmail.com>
2026-01-06 19:17:44 -0500

    chore(dev/android): add android-tools 'cause programs.adb option was deprecated

--- system/modules/programs/development/android.nix
+++ system/modules/programs/development/android.nix
@@ -1,8 +1,12 @@
-{user, ...}: {
-  programs = {
-    adb.enable = true;
-    nix-ld.enable = true; # I'm just a desperate shell
-  };
+{
+  pkgs,
+  user,
+  ...
+}: {
+  programs.nix-ld.enable = true; # I'm just a desperate shell
+
+  # previously "programs.adb.enable" but now systemd manages it
+  environment.systemPackages = [pkgs.android-tools];
 
   users.users.${user.alias}.extraGroups = ["adbusers"];
 }
