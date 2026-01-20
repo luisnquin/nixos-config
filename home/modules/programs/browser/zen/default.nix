073000a2458799e3d7fd2e595f8da5b36651af43
Luis Quiñones <lpaandres2020@gmail.com>
2026-01-19 20:15:34 -0500

    feat(browser/zen): new mods
    I know you think about me too
    
    I still beat you

--- home/modules/programs/browser/zen/default.nix
+++ home/modules/programs/browser/zen/default.nix
@@ -26,16 +26,19 @@
       };
 
       mods = [
-        "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
-        "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
-        "c6813222-6571-4ba6-8faf-58f3343324f6" # Disable Rounded Corners
         "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
-        "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
-        "cb15abdb-0514-4e09-8ce5-722cf1f4a20f" # Hide Extension Name
-        "803c7895-b39b-458e-84f8-a521f4d7a064" # Hide Inactive Workspaces
         "4ab93b88-151c-451b-a1b7-a1e0e28fa7f8" # No Sidebar Scrollbar
-        "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        "7190e4e9-bead-4b40-8f57-95d852ddc941" # Tab title fixes
+        "803c7895-b39b-458e-84f8-a521f4d7a064" # Hide Inactive Workspaces
+        "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+        "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
+        "c6813222-6571-4ba6-8faf-58f3343324f6" # Disable Rounded Corners
         "c8d9e6e6-e702-4e15-8972-3596e57cf398" # Zen Back Forward
+        "cb15abdb-0514-4e09-8ce5-722cf1f4a20f" # Hide Extension Name
+        "d8b79d4a-6cba-4495-9ff6-d6d30b0e94fe" # Better Active Tab
+        "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
+        "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e" # Quietify
       ];
 
       bookmarks = {
