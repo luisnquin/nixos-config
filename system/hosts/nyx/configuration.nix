c99390d6359b616bcfa780a65710a5bc8f7a1d26
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-03 11:24:14 -0500

    feat(hosts/nyx): set nixos.tags

--- system/hosts/nyx/configuration.nix
+++ system/hosts/nyx/configuration.nix
@@ -29,43 +29,45 @@
   i18n.defaultLocale = host.i18nLocale;
   location.provider = "geoclue2";
 
-  system.activationScripts = {
-    banner.text = ''
-      echo '    .              +   .                .   . .     .  .'
-      sleep 0.3
-      echo '                    .                    .       .     *'
-      echo '    .       *                        . . . .  .   .  + .'
-      sleep 0.2
-      echo '              (You Are Here)            .   .  +  . . .'
-      echo '  .                 |             .  .   .    .    . .'
-      echo '                    |           .     .     . +.    +  .'
-      sleep 0.5
-      echo '                    ↓            .       .   . .'
-      echo '          . .       .         .    * . . .  .  +   .'
-      echo '            +        ° <- (nyx)  *   .   .      +'
-      echo '<==[{                      .       . +  .+. .'
-      sleep 0.3
-      echo '    .                      .     . + .  . .     .      .'
-      echo '            .      .    .     . .   . . .        ! /'
-      echo '        *             .    . .  +    .  .       - O -  ←- (jyx)'
-      echo '            .     .    .  +   . .  *  .        . / |'
-      echo '                . + .  .  .  .. +  .'
-      echo '  .  +   .  .  .  *   .  *  . +..  .            *'
-      echo '  .      .   . .   .   .   . .  +   .    .            +'
-      echo
-
-      ${lib.getExe pkgs.genact} -s=5 --exit-after-time=1s \
-        -m bootlog \
-        -m cryptomining \
-        -m docker_image_rm \
-        -m download \
-        -m kernel_compile \
-        -m memdump \
-        -m simcity
-    '';
-  };
-
   system = {
+    activationScripts = {
+      banner.text = ''
+        echo '    .              +   .                .   . .     .  .'
+        sleep 0.3
+        echo '                    .                    .       .     *'
+        echo '    .       *                        . . . .  .   .  + .'
+        sleep 0.2
+        echo '              (You Are Here)            .   .  +  . . .'
+        echo '  .                 |             .  .   .    .    . .'
+        echo '                    |           .     .     . +.    +  .'
+        sleep 0.5
+        echo '                    ↓            .       .   . .'
+        echo '          . .       .         .    * . . .  .  +   .'
+        echo '            +        ° <- (nyx)  *   .   .      +'
+        echo '<==[{                      .       . +  .+. .'
+        sleep 0.3
+        echo '    .                      .     . + .  . .     .      .'
+        echo '            .      .    .     . .   . . .        ! /'
+        echo '        *             .    . .  +    .  .       - O -  ←- (jyx)'
+        echo '            .     .    .  +   . .  *  .        . / |'
+        echo '                . + .  .  .  .. +  .'
+        echo '  .  +   .  .  .  *   .  *  . +..  .            *'
+        echo '  .      .   . .   .   .   . .  +   .    .            +'
+        echo
+
+        ${lib.getExe pkgs.genact} -s=5 --exit-after-time=1s \
+          -m bootlog \
+          -m cryptomining \
+          -m docker_image_rm \
+          -m download \
+          -m kernel_compile \
+          -m memdump \
+          -m simcity
+      '';
+    };
+
+    nixos.tags = ["hyprland" "yay"];
+
     inherit (nix) stateVersion;
     autoUpgrade.enable = false;
   };
