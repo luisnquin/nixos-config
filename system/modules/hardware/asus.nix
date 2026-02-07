50dd9c4eda754e9f13b512b074f30c1189a81f3e
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-07 09:34:38 -0500

    feat(hardware/asus): add bat charget limit + aura config

--- system/modules/hardware/asus.nix
+++ system/modules/hardware/asus.nix
@@ -2,5 +2,54 @@
   services.asusd = {
     enable = true;
     enableUserService = true;
+
+    asusdConfig.text = ''
+      "bat_charge_limit": 80
+    '';
+
+    auraConfigs.tuf.text = ''
+      (
+        brightness: Med,
+        current_mode: Pulse,
+        builtins: {
+          Static: (
+            mode: Static,
+            zone: r#None,
+            colour1: (r: 200, g: 80, b: 30),
+            colour2: (r: 0, g: 0, b: 0),
+            speed: Med,
+            direction: Right,
+          ),
+          Breathe: (
+            mode: Breathe,
+            zone: r#None,
+            colour1: (r: 200, g: 80, b: 30),
+            colour2: (r: 30, g: 200, b: 80),
+            speed: Low,
+            direction: Right,
+          ),
+          Pulse: (
+            mode: Pulse,
+            zone: r#None,
+            colour1: (r: 200, g: 80, b: 30),
+            colour2: (r: 0, g: 0, b: 0),
+            speed: Low,                        # Low = transición más lenta y suave
+            direction: Right,
+          ),
+        },
+        multizone_on: false,
+        enabled: (
+          states: [
+            (
+              zone: Keyboard,
+              boot: true,
+              awake: true,
+              sleep: false,      # Se apaga al suspender
+              shutdown: false,
+            ),
+          ],
+        ),
+      )
+    '';
   };
 }
