d7fe5870a05814b46d9efd5f3c638c0839f415c3
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-07 09:46:50 -0500

    refactor(browser/zen): bookmarks in a separated file

--- home/modules/programs/browser/zen/default.nix
+++ home/modules/programs/browser/zen/default.nix
@@ -41,77 +41,7 @@
         "fd24f832-a2e6-4ce9-8b19-7aa888eb7f8e" # Quietify
       ];
 
-      bookmarks = {
-        force = true;
-        settings = let
-          gmailEntries = map (i: rec {
-            keyword = "l${toString i}";
-            name = keyword;
-            url = "https://mail.google.com/mail/u/${toString i}/#inbox";
-            tags = ["gmail"];
-          }) [0 1 2 3];
-        in
-          [
-            {
-              name = "encore";
-              tags = ["encore" "k9"];
-              keyword = "encore";
-              url = libx.base64.decode "aHR0cHM6Ly9hcHAuZW5jb3JlLmNsb3VkL2dhdGUtazktbXpuaQo=";
-            }
-            {
-              name = "kernel.org";
-              url = "https://www.kernel.org";
-            }
-            {
-              name = "orders";
-              url = "https://www.aliexpress.com/p/order/index.html";
-              keyword = "orders";
-            }
-            {
-              name = "cart";
-              url = "https://www.aliexpress.com/p/shoppingcart/index.html";
-              keyword = "cart";
-            }
-            {
-              name = "s3";
-              url = "https://sa-east-1.console.aws.amazon.com/s3/buckets?region=sa-east-1";
-              keyword = "s3";
-            }
-            {
-              name = "iam";
-              url = "https://us-east-1.console.aws.amazon.com/iam/home?region=sa-east-1";
-              keyword = "iam";
-            }
-            {
-              name = "firebase";
-              url = "https://console.firebase.google.com/u/0/";
-              keyword = "fire";
-            }
-            {
-              name = "novu";
-              url = "https://dashboard-v2.novu.co/env/dev_env_gQwrIEDTzbHL4coa";
-              keyword = "novu";
-            }
-            {
-              name = "supabase";
-              url = "https://supabase.com/dashboard/project/mjkvxcziwkwohxpuejak";
-              keyword = "supa";
-            }
-            {
-              name = "krear3d";
-              url = "https://www.tiendakrear3d.com/productos/filamentos/";
-              keyword = "3d";
-              tags = ["3d"];
-            }
-            {
-              name = "digitalz3d";
-              url = "http://www.digitalz3d.com/filamentos";
-              keyword = "3dd";
-              tags = ["3d"];
-            }
-          ]
-          ++ gmailEntries;
-      };
+      bookmarks = import ./bookmarks-config.nix {inherit libx;};
 
       search = import ./search-config.nix {inherit pkgs;};
 
