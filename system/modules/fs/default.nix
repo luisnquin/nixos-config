b0433557ee4b662049edd6e56f35d0953139ef81
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-07 08:12:15 -0500

    feat(system/fs): enable fstrim service
    from here on out, you're healthy

--- system/modules/fs/default.nix
+++ system/modules/fs/default.nix
@@ -9,5 +9,10 @@
     tmp.cleanOnBoot = true;
   };
 
-  services.udisks2.enable = true;
+  services = {
+    udisks2.enable = true;
+    # without it, the SSD's controller will not know where the free space is on the drive. The controller needs
+    # free space to do its garbage collection job. unexpected behavior may occur.
+    fstrim.enable = true;
+  };
 }
