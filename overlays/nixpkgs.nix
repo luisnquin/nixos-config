d313583d186bbbf78a7d9fe3ecac6ad03632b2f6
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-28 20:42:21 -0500

    feat(dev/vscode-like): antigravity @ v1.19.6

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -14,4 +14,14 @@
   (_self: _super: {
     inherit (inputs.hyprdysmorphic.packages.${system}) xdg-desktop-portal-hyprland hyprlauncher hyprland;
   })
+
+  (_self: super: {
+    antigravity = super.antigravity.overrideAttrs (oldAttrs: rec {
+      version = "1.19.6";
+      src = super.fetchurl {
+        url = "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/${version}-6514342219874304/linux-x64/Antigravity.tar.gz";
+        sha256 = "sha256-gFIsnWC8wEuxPUD6E2YB0YTcg/NruQZespzEVttMKeE=";
+      };
+    });
+  })
 ]
