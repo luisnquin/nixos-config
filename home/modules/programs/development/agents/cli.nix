bc9a1771992c6a217958d70272765fe677cde3e9
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-25 15:54:14 -0500

    feat(dev/agents): gemini settings

--- home/modules/programs/development/agents/cli.nix
+++ home/modules/programs/development/agents/cli.nix
@@ -3,6 +3,14 @@
     gemini-cli = {
       enable = true;
       defaultModel = "gemini-2.5-flash";
+      settings = {
+        theme = "Default";
+        vimMode = true;
+        preferredEditor = "nano";
+        autoAccept = false;
+        secureModeEnabled = false;
+        security.auth.selectedType = "oauth-personal";
+      };
     };
 
     claude-code.enable = true;
