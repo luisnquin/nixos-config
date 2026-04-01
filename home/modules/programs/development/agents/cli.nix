04a340b4ff89bb12358d6f1d15cfbebe24664882
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-31 20:23:54 -0500

    feat(dev/agents): mcp configuration for claude code

--- home/modules/programs/development/agents/cli.nix
+++ home/modules/programs/development/agents/cli.nix
@@ -1,4 +1,4 @@
-{
+{config, ...}: {
   programs = {
     gemini-cli = {
       enable = true;
@@ -15,6 +15,12 @@
 
     claude-code = {
       enable = true;
+      enableMcpIntegration = true;
+      mcpServers = builtins.removeAttrs config.programs.mcp.servers [
+        "expo"
+        "supabase"
+      ];
+
       memory.text = ''
         Do not include the Co-authored-by trailer in commits.
         Do not use emojis in development outputs.
