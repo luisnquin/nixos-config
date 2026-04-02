2d7605766aa2a46a6e7b9776631cc3aa918a6bf8
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-01 22:38:20 -0500

    chore(overlays): remove claude override

--- overlays/nixpkgs.nix
+++ overlays/nixpkgs.nix
@@ -17,24 +17,4 @@
       };
     });
   })
-  (_self: super: {
-    claude-code = super.buildNpmPackage (finalAttrs: {
-      pname = "claude-code";
-      version = "2.1.89";
-
-      src = super.fetchzip {
-        url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${finalAttrs.version}.tgz";
-        hash = "sha256-FoTm6KDr+8Dzhk4ibZUlU1QLPFdPm/OriUUWqAaFswg=";
-      };
-
-      npmDepsHash = "sha256-NI4F5bq0lEuMjLUdkGrml2aOzGbGkdyUckgfeVFEe8o=";
-
-      postPatch = ''
-        cp ${./claude-code-package-lock.json} package-lock.json
-      '';
-
-      dontNpmBuild = true;
-      env.AUTHORIZED = "1";
-    });
-  })
 ]
