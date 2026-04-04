47e99636cb8f0f5164036a16a8e0400872723625
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-04 11:07:42 -0500

    feat(file-manager): ranger setup improvements but in exchange I lose syntax highlighting
    that's "the exchange", you have read about it a long time ago and I was wondering if you
    still remember that

--- home/modules/file-manager.nix
+++ home/modules/file-manager.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = [
     pkgs.nautilus
   ];
@@ -6,11 +11,52 @@
   programs.ranger = {
     enable = true;
 
+    settings = {
+      use_preview_script = true;
+      preview_script = "${config.home.homeDirectory}/.config/ranger/scope.sh";
+
+      preview_images = true;
+      preview_images_method = "ueberzug";
+    };
+
     rifle = [
       {
         condition = "ext wav";
         command = "${pkgs.pulseaudio}/bin/paplay \"$@\"";
       }
+      {
+        condition = "ext ico";
+        command = "${lib.getExe pkgs.feh} \"$@\"";
+      }
     ];
+  };
+
+  xdg.configFile."ranger/scope.sh" = {
+    source = pkgs.writeShellScript "ranger-scope.sh" ''
+      call_bat () {
+        ${lib.getExe pkgs.bat} --color=always --paging=never --style=plain "$1"
+      }
+
+      FILE_PATH="''${1}"
+      PV_WIDTH="''${2}"
+      PV_HEIGHT="''${3}"
+      FILE_EXTENSION="''${FILE_PATH##*.}"
+      FILE_MIMETYPE=$(${lib.getExe pkgs.file} -b --mime-type "''${FILE_PATH}")
+
+      case "$FILE_MIMETYPE" in
+        text/*|application/json)
+          call_bat "$FILE_PATH"
+          exit 0
+          ;;
+      esac
+
+      if [ -z "$FILE_EXTENSION" ]; then
+        call_bat "$FILE_PATH"
+        exit 0
+      fi
+
+      exit 1
+    '';
+    executable = true;
   };
 }
