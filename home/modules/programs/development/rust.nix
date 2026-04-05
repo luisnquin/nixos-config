da8b7549fa77d2f314c9b194093e0cfc906bacb7
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-04 19:31:26 -0500

    feat(dev/rust): somer  useful cargo settings

--- home/modules/programs/development/rust.nix
+++ home/modules/programs/development/rust.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home = {
     packages = with pkgs; let
       cargo-plugins = [
@@ -19,9 +23,49 @@
 
   programs.cargo = {
     enable = true;
+    cargoHome = "${config.xdg.dataHome}/.cargo";
+
     settings = {
       net = {
         git-fetch-with-cli = true;
+      };
+
+      build = {
+        jobs = 4;
+        incremental = true;
+        target-dir = "target";
+      };
+
+      profile = {
+        dev = {
+          incremental = true;
+          codegen-units = 256;
+          debug = 1;
+        };
+
+        release = {
+          incremental = true;
+          codegen-units = 64;
+          lto = false;
+          debug = 0;
+          strip = "debuginfo";
+        };
+      };
+
+      future-incompat-report = {
+        frequency = "always";
+      };
+
+      cache = {
+        auto-clean-frequency = "1 day";
+      };
+
+      term = {
+        quiet = false;
+        verbose = false;
+        color = "auto";
+        hyperlinks = true;
+        unicode = true;
       };
     };
   };
