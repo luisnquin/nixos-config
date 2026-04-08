034b782a2b36072fa9208c9b724783546e27c6b1
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-07 19:39:40 -0500

    chore(agents/skills): update "with" predicate due to unexpected changes in upstream

--- home/modules/programs/development/agents/skills.nix
+++ home/modules/programs/development/agents/skills.nix
@@ -2,7 +2,7 @@
   programs.agents = {
     enable = true;
 
-    skills = with pkgs.agent-skills.skills-sh; [
+    skills = with pkgs.agent-skills; [
       (official.encoredev.skills {
         scopes = ["claude" "global"];
         plugins = [
