61509558e4d02ea25654886066b44ed28e82e043
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-07 20:46:28 -0500

    chore: remove dead secrets.nix file

--- secrets.nix
+++
@@ -1,8 +0,0 @@
-let
-  luisnquin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXW6vsDRgI/AiOdGnQOTyiz1uLFL0o66u0Ahcw9VWyd luis@quinones.pro";
-in {
-  "secrets/certs/rootCA.crt.age".publicKeys = [luisnquin];
-  "secrets/certs/rootCA.key.age".publicKeys = [luisnquin];
-  "secrets/certs/wildcard.crt.age".publicKeys = [luisnquin];
-  "secrets/certs/wildcard.key.age".publicKeys = [luisnquin];
-}
