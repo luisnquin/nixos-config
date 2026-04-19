3d36235040bbd751d05aaaf1697994912402898f
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-18 22:46:16 -0500

    feat(security): additional openssh constraints

--- system/modules/programs/security/protocols.nix
+++ system/modules/programs/security/protocols.nix
@@ -29,36 +29,49 @@
       HostName dyx.local
   '';
 
-  services.openssh = {
-    enable = true;
-    banner = libx.base64.decode "SXQncyB0cnVlLCB5b3UgY2FuIG5ldmVyIGVhdCBhIHBldCB5b3UgbmFtZQ==";
-    ports = [
-      357
-    ];
-    openFirewall = true;
+  services = {
+    openssh = {
+      enable = true;
+      banner = libx.base64.decode "SXQncyB0cnVlLCB5b3UgY2FuIG5ldmVyIGVhdCBhIHBldCB5b3UgbmFtZQ==";
+      ports = [
+        357
+      ];
+      openFirewall = true;
 
-    hostKeys = [
-      {
-        bits = 4096;
-        path = "/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-      }
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/home/${user.alias}/.ssh/id_ed25519";
-        type = "ed25519";
-      }
-    ];
+      hostKeys = [
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/home/${user.alias}/.ssh/id_ed25519";
+          type = "ed25519";
+        }
+      ];
 
-    # https://github.com/NixOS/nixpkgs/issues/234683
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
-      AllowUsers = ["luisnquin"];
+      # https://github.com/NixOS/nixpkgs/issues/234683
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = ["luisnquin"];
+      };
+
+      extraConfig = ''
+        MaxAuthTries 3
+        PerSourcePenalties crash:3600s authfail:3600s max:86400s
+      '';
+    };
+
+    endlessh = {
+      enable = true;
+      port = 357;
+      openFirewall = true;
     };
   };
 }
