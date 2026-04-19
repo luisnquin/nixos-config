9cd72060b8032860f29d5737f1d4538eb2470a22
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-18 23:08:30 -0500

    chore(security/ssh): drop redundant hostKeys defaults

--- system/modules/programs/security/ssh.nix
+++ system/modules/programs/security/ssh.nix
@@ -28,18 +28,6 @@
       ];
       openFirewall = true;
 
-      hostKeys = [
-        {
-          bits = 4096;
-          path = "/etc/ssh/ssh_host_rsa_key";
-          type = "rsa";
-        }
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
-
       settings = {
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
