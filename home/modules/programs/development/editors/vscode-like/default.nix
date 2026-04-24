ccd5213f86ac153aa7f0d14b3f8a3c1c9af784d7
Luis Quiñones <lpaandres2020@gmail.com>
2026-04-24 18:03:52 -0500

    fix(dev/vscode-like): update hash

--- home/modules/programs/development/editors/vscode-like/default.nix
+++ home/modules/programs/development/editors/vscode-like/default.nix
@@ -2,7 +2,7 @@
   imports = let
     hmFork = builtins.fetchTarball {
       url = "https://github.com/sei40kr/home-manager/archive/vscode-fork-modules.tar.gz";
-      sha256 = "0jrd01ydyqq4w1qflzyw21a0n9qbgafy9ya6q4v02ygdcqhl92yz";
+      sha256 = "0s43cs3883w12glndkfjsqrsy8hc05hg8yl92hvd2c2kxpmyqlih";
     };
   in [
     (import "${hmFork}/modules/programs/cursor.nix")
