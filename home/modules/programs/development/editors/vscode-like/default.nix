d4297670fdbc6e1b4bb25479171df8c0f9dcbe3f
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-04 14:53:40 -0500

    chore(dev/vscode-like): sei40kr/home-manager/archive/vscode-fork-modules is no longer necessary

--- home/modules/programs/development/editors/vscode-like/default.nix
+++ home/modules/programs/development/editors/vscode-like/default.nix
@@ -1,12 +1,5 @@
 {
-  imports = let
-    hmFork = builtins.fetchTarball {
-      url = "https://github.com/sei40kr/home-manager/archive/vscode-fork-modules.tar.gz";
-      sha256 = "0s43cs3883w12glndkfjsqrsy8hc05hg8yl92hvd2c2kxpmyqlih";
-    };
-  in [
-    (import "${hmFork}/modules/programs/cursor.nix")
-    (import "${hmFork}/modules/programs/antigravity.nix")
+  imports = [
     ./antigravity.nix
     ./cursor.nix
   ];
