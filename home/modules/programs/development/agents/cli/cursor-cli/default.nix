419fbb8916860038b6e8420111f2c1519735e884
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-05 17:25:05 -0500

    feat(agents/cursor-cli): add global rules

--- home/modules/programs/development/agents/cli/cursor-cli/default.nix
+++ home/modules/programs/development/agents/cli/cursor-cli/default.nix
@@ -1,8 +1,22 @@
-{pkgs, ...}: {
+{
+  mkAgentKit,
+  pkgs,
+  ...
+}: let
+  kit = mkAgentKit {};
+in {
   programs.cursor-agent = {
     enable = true;
     package = pkgs.cursor-cli;
     enableMcpIntegration = true;
+    rules = {
+      "global.mdc" = ''
+        ---
+        alwaysApply: true
+        ---
+        ${kit.memories}
+      '';
+    };
     settings = {
       attribution = {
         attributeCommitsToAgent = false;
