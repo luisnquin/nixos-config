452f0ec7d718b7bec4f24981340dcf2c3ae37d87
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-05 00:47:32 -0500

    feat(security/gnupg): use pinentry curses

--- system/modules/programs/security/gnupg.nix
+++ system/modules/programs/security/gnupg.nix
@@ -1,7 +1,8 @@
-{
+{pkgs, ...}: {
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-curses;
     settings = {
       # this is your vector attack
       default-cache-ttl = 60 * 15;
