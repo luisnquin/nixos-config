617eff8ccd494feb2a2c6891ade3f4080bce274c
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-05 10:06:25 -0500

    feat(security/keyring): pam integrates with greetd

--- system/modules/programs/security/keyring.nix
+++ system/modules/programs/security/keyring.nix
@@ -4,5 +4,9 @@
   ];
 
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
+
+  security.pam.services = {
+    login.enableGnomeKeyring = true;
+    greetd.enableGnomeKeyring = true;
+  };
 }
