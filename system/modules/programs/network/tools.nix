c78ea3a16c6ca454db816162be45f501a5cb12de
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-10 00:38:29 -0500

    feat(network/tools): force lsyncd to use pinentry gui

--- system/modules/programs/network/tools.nix
+++ system/modules/programs/network/tools.nix
@@ -12,7 +12,17 @@
 
   services = with pkgs; [
     cloudflared # https://developers.cloudflare.com/ssl/edge-certificates/additional-options/total-tls/error-messages/#active-domains
-    lsyncd
+    (pkgs.symlinkJoin {
+      name = "lsyncd-wrapper";
+      paths = [pkgs.lsyncd];
+
+      buildInputs = [pkgs.makeWrapper];
+
+      postBuild = ''
+        wrapProgram "$out/bin/lsyncd" \
+          --set PINENTRY_USER_DATA gui
+      '';
+    })
   ];
 in {
   environment.systemPackages = tools ++ base ++ services;
