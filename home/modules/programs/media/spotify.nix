5aee93cd4c54add37ec90e3bdf885ae680e81008
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-12 18:50:21 -0500

    feat(media/spotify): surrender?

--- home/modules/programs/media/spotify.nix
+++ home/modules/programs/media/spotify.nix
@@ -1,35 +1,5 @@
-{
-  nixosConfig,
-  pkgs,
-  ...
-}: {
-  services = {
-    go-librespot = {
-      enable = true;
-      settings = {
-        log_level = "info";
-        device_name = nixosConfig.networking.hostName;
-        device_type = "speaker";
-        audio_backend = "pulseaudio";
-
-        zeroconf_enabled = false;
-
-        credentials = {
-          type = "interactive";
-        };
-
-        bitrate = 160;
-        initial_volume = 100;
-        ignore_last_volume = true;
-        disable_autoplay = false;
-        mpris_enabled = false;
-      };
-    };
-
-    playerctld.enable = true;
-  };
-
+{pkgs, ...}: {
   home.packages = [
-    pkgs.spotify-qt
+    pkgs.spotify
   ];
 }
