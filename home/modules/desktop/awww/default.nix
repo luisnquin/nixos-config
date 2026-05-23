d9fbf25263bcdf8240dbfd186c6fdcbc49236bda
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-23 08:38:08 -0500

    fix(desktop/awww): pin wallpapers in hypr-wallpapers store derivation

--- home/modules/desktop/awww/default.nix
+++ home/modules/desktop/awww/default.nix
@@ -1,17 +1,38 @@
 {
   pkgs,
   lib,
-  libx,
   config,
   ...
 }: let
-  wallpaperFiles = libx.fs.getFilesInDirectory ./wallpapers;
+  cfg = config.services.awww;
   inherit (pkgs.scripts) awww-switcher;
-  wallpaperArgs = builtins.concatStringsSep " " (map builtins.toString (wallpaperFiles ++ [./background.gif]));
+
+  wallpapersPkg = pkgs.runCommand "hypr-wallpapers" {
+    preferLocalBuild = true;
+  } ''
+    mkdir -p $out
+    cp -r ${./wallpapers}/. $out/
+    cp ${./background.gif} $out/
+  '';
+
+  wallpaperNames = lib.naturalSort (
+    lib.attrNames (lib.filterAttrs (_: t: t == "regular") (builtins.readDir ./wallpapers))
+  );
+
+  wallpaperFiles = map (name: "${wallpapersPkg}/${name}") wallpaperNames;
+  backgroundFile = "${wallpapersPkg}/background.gif";
+  defaultWallpaper = lib.head wallpaperFiles;
+
+  wallpaperArgs = lib.concatStringsSep " " (map lib.escapeShellArg (wallpaperFiles ++ [backgroundFile]));
+  awwwImg = "${lib.getExe' cfg.package "awww"} img ${lib.escapeShellArg defaultWallpaper}";
 in {
   services.awww.enable = true;
 
   wayland.windowManager.hyprland.extraConfig = lib.mkIf config.wayland.windowManager.hyprland.enable ''
+    hl.on("hyprland.start", function()
+      hl.exec_cmd("${awwwImg}")
+    end)
+
     hl.bind("SUPER + L", hl.dsp.exec_cmd("${awww-switcher}/bin/cli ${wallpaperArgs}"))
   '';
 }
