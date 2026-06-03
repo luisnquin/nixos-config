10b12a69a3e153d0e7c0cbc19d2e3ca60669d46c
Luis Quiñones <lpaandres2020@gmail.com>
2026-06-03 18:55:40 -0500

    chore(dev/node): rm things I don't use

--- home/modules/programs/development/node.nix
+++ home/modules/programs/development/node.nix
@@ -9,10 +9,7 @@
 in {
   home = {
     packages = with pkgs; [
-      npm-check-updates # (ncu) Find newer versions of package dependencies and check outdated npm packages locally or globally.
       npkill # remove node_modules from child directories
-      nest-cli
-      husky
       biome
     ];
 
