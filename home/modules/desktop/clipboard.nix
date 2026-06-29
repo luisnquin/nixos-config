bf116b2d387e331b8b87cb192c81218a1d4d0d32
Luis Quiñones <lpaandres2020@gmail.com>
2026-06-29 13:07:37 -0500

    feat(desktop/clipboard): only load necessary fonts

--- home/modules/desktop/clipboard.nix
+++ home/modules/desktop/clipboard.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
-  programs.cliphizt.enable = true;
-
-  home.packages = [
-    pkgs.cliplenz
-  ];
+  programs = {
+    cliphizt.enable = true;
+    cliplenz = {
+      enable = true;
+      fonts = with pkgs; [
+        cascadia-code
+        dejavu_fonts
+      ];
+    };
+  };
 }
