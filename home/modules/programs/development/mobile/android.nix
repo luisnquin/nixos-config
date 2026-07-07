dc817e4c7e08d87c78f36accfb8db00a070ffeec
Luis Quiñones <lpaandres2020@gmail.com>
2026-07-07 11:26:17 -0500

    refactor(dev/mobile): split android module wiring

---
+++ home/modules/programs/development/mobile/android.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  ANDROID_HOME = "${config.home.homeDirectory}/.android/sdk";
+  ANDROID_SDK_ROOT = ANDROID_HOME;
+in {
+  programs.zsh.initContent = builtins.readFile (builtins.path {
+    name = "mobile-zshrc";
+    path = ./mobile.zsh;
+  });
+
+  systemd.user.slices.android-emulator = {
+    Unit.Description = "Android emulator resource control";
+    Slice = {
+      IOWriteBandwidthMax = "/ 150M";
+      MemoryHigh = "12G";
+    };
+  };
+
+  home = {
+    packages = with pkgs; [
+      android-studio
+      android-tools
+      (import ./android-cli.nix {inherit pkgs ANDROID_HOME ANDROID_SDK_ROOT;})
+
+      scrcpy
+
+      kotlin-language-server
+      kotlin-native
+      kotlin
+
+      sdkmanager
+    ];
+
+    file = {
+      ".android/advancedFeatures.ini" = {
+        text = ''
+          QuickbootFileBacked = off
+        '';
+        force = true;
+      };
+
+      ".gradle/gradle.properties" = {
+        text = ''
+          org.gradle.jvmargs=-Xmx14g -XX:MaxMetaspaceSize=512m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8
+          org.gradle.parallel=true
+          org.gradle.configureondemand=true
+          org.gradle.daemon=false
+        '';
+        force = true;
+      };
+    };
+
+    sessionVariables = {
+      inherit ANDROID_HOME ANDROID_SDK_ROOT;
+    };
+  };
+}
---
+++ home/modules/programs/development/mobile/avds.nix
@@ -0,0 +1,24 @@
+{
+  android.avds.galaxy-s26-plus = {
+    displayName = "Galaxy S26+";
+    api = "36";
+
+    systemImage = "google_apis_playstore";
+    arch = "x86_64";
+
+    manufacturer = "Samsung";
+    deviceName = "Galaxy S26 Plus";
+
+    width = 1080;
+    height = 2340;
+    density = 425;
+
+    ramSize = 8192;
+
+    skin = {
+      enable = true;
+      name = "galaxy-s26-plus";
+      source = ./skins/Galaxy_S26_Plus;
+    };
+  };
+}
