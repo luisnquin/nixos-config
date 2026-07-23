07462fe1eed0a04c45c5417ca70fb635277a98c9
Luis Quiñones <lpaandres2020@gmail.com>
2026-07-23 15:20:32 -0500

    chore: do not allow numtide cache layer

--- flake.nix
+++ flake.nix
@@ -145,15 +145,6 @@
     };
   };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.numtide.com"
-    ];
-    extra-trusted-public-keys = [
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-    ];
-  };
-
   outputs = inputs @ {
     home-manager,
     nixpkgs,
