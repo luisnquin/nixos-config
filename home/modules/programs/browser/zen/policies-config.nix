a8dfd5c05780c3547fa283398097485c582bc80d
Luis Quiñones <lpaandres2020@gmail.com>
2026-08-03 10:53:02 -0500

    feat(browser/zen): update policies->extensions

--- home/modules/programs/browser/zen/policies-config.nix
+++ home/modules/programs/browser/zen/policies-config.nix
@@ -53,7 +53,6 @@ in {
       pinned = true;
     };
     # about:debugging#/runtime/this-firefox
-    "{0814291e-c531-4741-a8e7-9a3e8f62bf71}" = "remove-youtube-tracking";
     "{3579f63b-d8ee-424f-bbb6-6d0ce3285e6a}" = "chameleon-ext";
     "{4590d8b8-3569-46e3-a571-cabfbaeab2c1}" = "no-youtube-shorts";
     "{74145f27-f039-47ce-a470-a662b129930a}" = "clearurls";
@@ -61,13 +60,14 @@ in {
     "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
     "{861a3982-bb3b-49c6-bc17-4f50de104da1}" = "custom-user-agent-revived";
     "{a4c4eda4-fb84-4a84-b4a1-f7c1cbf2a1ad}" = "refined-github-";
-    "{ef9e884b-b6d8-4544-b0de-82c46c5e95de}" = "sponsorblock";
     "{fef652df-dd80-450e-b64a-567abeb3aa4b}" = "youtube-cards";
     "@searchengineadremover" = "searchengineadremover";
     "firefox-extension@steamdb.info" = "steam-database";
     "github-no-more@ihatereality.space" = "github-no-more";
     "github-repository-size@pranavmangal" = "gh-repo-size";
     "jid1-BoFifL9Vbdl2zQ@jetpack" = "decentraleyes";
+    "remove.youtube.tracking@moreo.app" = "remove-youtube-tracking";
+    "sponsorBlocker@ajay.app" = "sponsorblock";
   };
   "3rdparty".Extensions."uBlock0@raymondhill.net".toOverwrite = {
     filterLists = [
