8d4f7cab80ddcaade5fa4debd050c4dcffad158a
Luis Quiñones <lpaandres2020@gmail.com>
2026-08-11 11:15:38 -0500

    feat(dev/android): add mtkclient in udev packages list

--- system/modules/programs/development/android.nix
+++ system/modules/programs/development/android.nix
@@ -8,5 +8,7 @@
   # previously "programs.adb.enable" but now systemd manages it
   environment.systemPackages = [pkgs.android-tools];
 
+  services.udev.packages = [pkgs.mtkclient];
+
   users.users.${user.alias}.extraGroups = ["adbusers"];
 }
