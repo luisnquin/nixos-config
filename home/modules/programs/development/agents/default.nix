52e384d253a3990d3731e2b1eb836c9756914b9d
Luis Quiñones <lpaandres2020@gmail.com>
2026-09-04 12:55:39 -0500

    flake-update(llm-agents): flake @ latest git

--- home/modules/programs/development/agents/default.nix
+++ home/modules/programs/development/agents/default.nix
@@ -1,13 +1,12 @@
 {
   nixosConfig,
-  inputs,
   config,
   pkgs,
   libx,
   lib,
   ...
 }: {
-  _module.args = import ./lib.nix {inherit nixosConfig inputs pkgs lib libx config;};
+  _module.args = import ./lib.nix {inherit nixosConfig pkgs lib libx config;};
 
   imports = [
     ./apps
