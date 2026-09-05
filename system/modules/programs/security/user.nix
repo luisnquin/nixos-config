ff2d52bbd30929cfb2f2ded85fde8f6df7565ee3
Luis Quiñones <lpaandres2020@gmail.com>
2026-09-04 22:00:36 -0500

    feat(security/user): refresh pubkey

--- system/modules/programs/security/user.nix
+++ system/modules/programs/security/user.nix
@@ -27,8 +27,7 @@
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNOYm8dmSXKjgaBQDWCnSvcsGyiJILX3Vwejmkm150+"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpV17zqf4dGsuaddSslVpHV5APCsEQSXPAnuBSZk5zY"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3ELYeyuf9nzd+M6Y2ksJUl0B7L24iVFIOLCCS8Rb7L hotline-emulator"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPidNFv02W5+F9mlSjIkMLKBef9qVxK+yHoOI/qQ23i hotline"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOy0naPj/yYMQtfKt/geDwWyS16IujRV3UbC4P2xQalE"
         ];
       };
 
