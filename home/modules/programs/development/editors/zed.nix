b5270745b7b71b438c2e80cd0127b93269bba794
Luis Quiñones <lpaandres2020@gmail.com>
2026-09-16 16:08:04 -0500

    feat(dev/editors): disable zed telemetry

--- home/modules/programs/development/editors/zed.nix
+++ home/modules/programs/development/editors/zed.nix
@@ -2,5 +2,14 @@
   programs.zed-editor = {
     enable = true;
     extensions = ["nix" "toml" "rust"];
+
+    userSettings = {
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+
+      auto_update = false;
+    };
   };
 }
